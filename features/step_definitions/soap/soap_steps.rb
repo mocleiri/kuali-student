061b33a4001b836bd80cd16de583a0446f335a33
miloskarhanek <miloskarhanek@kuali.org>
2010-01-21 11:00:38 -0500

    New commit of light version if cucumber testing
    git-svn-id: https://svn.kuali.org/repos/student/deploymentlab/UI/ks-cuke-testing/trunk@7472 0ac02d80-fb3f-11dc-a574-c57244a850eb

---
+++ features/step_definitions/soap/soap_steps.rb
@@ -0,0 +1,21 @@
+Given /^(?:|I )am connected to the "([^\"]*)" service$/ do |service_name|
+  @client = Savon::Client.new soap_path_to(service_name)
+end
+
+When /^(?:|I )call the "([^\"]*)" action with params "([^\"]*)"$/ do |action_name, params|
+  @response = @client.instance_eval (action_name + build_action_params(params))
+end
+
+Then /^response parameter "([^\"]*)" for the action "([^\"]*)" should contain "([^\"]*)"$/ do |response_parameter, action_name, value|
+  puts @response.to_hash[action_name.to_sym][response_parameter.to_sym].should == value
+end
+
+
+Then /^show me the output in the console$/ do
+  puts @response.to_hash.inspect
+end
+
+
+def build_action_params(params)
+  "{|soap| soap.body={#{params}}}"
+end
