332f68ed45ab5f60bb5910f05b0794dd31f2e547
nwright <nwright@kuali.org>
2010-12-08 14:51:58 -0500

    git-svn-id: https://svn.kuali.org/repos/student/tools/ks-loader@18137 0ac02d80-fb3f-11dc-a574-c57244a850eb

--- src/main/resources/ks-loader-organization-loader-example.cmd
+++ src/main/resources/ks-loader-organization-loader-example.cmd
@@ -1,5 +1,5 @@
 @echo off
 echo organization loader
 pause
-java -jar ks-loader.jar organization "AccreditingBodies.xls" "http://localhost:9393/ks-embedded-dev"
+java -jar ks-loader.jar organization "Organizations for BSCI program.xls" "http://localhost:9393/ks-embedded-dev"
 pause
