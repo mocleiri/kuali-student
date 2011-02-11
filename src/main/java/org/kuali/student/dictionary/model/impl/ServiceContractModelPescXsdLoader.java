0d9d8389f35859557924b98ad42f9f93182a22bc
nwright <nwright@kuali.org>
2011-02-11 10:40:28 -0500

    got pesc model loader working
    git-svn-id: https://svn.kuali.org/repos/student/tools/maven-dictionary-generator/trunk@19682 0ac02d80-fb3f-11dc-a574-c57244a850eb

--- src/main/java/org/kuali/student/dictionary/model/impl/ServiceContractModelPescXsdLoader.java
+++ src/main/java/org/kuali/student/dictionary/model/impl/ServiceContractModelPescXsdLoader.java
@@ -15,23 +15,27 @@
  */
 package org.kuali.student.dictionary.model.impl;
 
+import com.sun.xml.xsom.XSAnnotation;
+import com.sun.xml.xsom.XSComplexType;
+import com.sun.xml.xsom.XSContentType;
+import com.sun.xml.xsom.XSElementDecl;
+import com.sun.xml.xsom.XSModelGroup;
+import com.sun.xml.xsom.XSParticle;
 import com.sun.xml.xsom.XSSchema;
 import com.sun.xml.xsom.XSSchemaSet;
+import com.sun.xml.xsom.XSTerm;
+import com.sun.xml.xsom.XSType;
 import com.sun.xml.xsom.parser.XSOMParser;
 import java.io.File;
 import java.io.IOException;
 import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.HashMap;
+import java.util.LinkedHashMap;
 import java.util.List;
 import java.util.Map;
-import java.util.logging.Level;
-import java.util.logging.Logger;
 import org.kuali.student.dictionary.model.MessageStructure;
 import org.kuali.student.dictionary.model.Service;
 import org.kuali.student.dictionary.model.ServiceMethod;
 import org.kuali.student.dictionary.model.ServiceContractModel;
-import org.kuali.student.dictionary.model.ServiceMethodParameter;
 import org.kuali.student.dictionary.model.ServiceMethodReturnValue;
 import org.kuali.student.dictionary.model.XmlType;
 import org.xml.sax.SAXException;
@@ -45,6 +49,10 @@ public class ServiceContractModelPescXsdLoader implements
 {
 
  private String xsdFileName;
+ private List<Service> services = null;
+ private List<ServiceMethod> serviceMethods = null;
+ private Map<String, XmlType> xmlTypeMap = null;
+ private List<MessageStructure> messageStructures;
 
  public ServiceContractModelPescXsdLoader (String xsdFileName)
  {
@@ -54,28 +62,76 @@ public class ServiceContractModelPescXsdLoader implements
  @Override
  public List<ServiceMethod> getServiceMethods ()
  {
-  List<ServiceMethod> list = new ArrayList ();
-  return list;
+  if (this.serviceMethods == null)
+  {
+   this.parse ();
+  }
+  return this.serviceMethods;
  }
 
  @Override
  public List<String> getSourceNames ()
  {
-  return Arrays.asList (this.xsdFileName);
+  List<String> list = new ArrayList ();
+  list.add (this.xsdFileName);
+  return list;
  }
 
  @Override
  public List<Service> getServices ()
  {
-  List<Service> list = new ArrayList ();
-  return list;
+  if (services == null)
+  {
+   this.parse ();
+  }
+  return services;
  }
 
  @Override
  public List<XmlType> getXmlTypes ()
  {
-  Map<String, XmlType> map = new HashMap ();
+  if (xmlTypeMap == null)
+  {
+   this.parse ();
+  }
+  return new ArrayList (xmlTypeMap.values ());
+ }
+
+ @Override
+ public List<MessageStructure> getMessageStructures ()
+ {
+  if (messageStructures == null)
+  {
+   this.parse ();
+  }
+  return this.messageStructures;
+ }
+
+ private void parse ()
+ {
+  System.out.println ("ServiceContractModelQDoxLoader: Starting parse");
+  services = new ArrayList ();
+  Service service = new Service ();
+  services.add (service);
+  service.setKey ("Pesc");
+  service.setName ("PescService");
+  service.setComments ("Derived from pesc CoreMain");
+  serviceMethods = new ArrayList ();
+  ServiceMethod method = new ServiceMethod ();
+  serviceMethods.add (method);
+  method.setName ("dummy");
+  method.setDescription ("Dummy method so validation won't fail");
+  method.setService ("Pesc");
+  method.setParameters (new ArrayList ());
+  ServiceMethodReturnValue rv = new ServiceMethodReturnValue ();
+  rv.setType ("void");
+  rv.setDescription ("returns nothing");
+  method.setReturnValue (rv);
+  xmlTypeMap = new LinkedHashMap ();
+  messageStructures = new ArrayList ();
+
   XSOMParser parser = new XSOMParser ();
+  parser.setAnnotationParser (new XsdAnnotationParserFactory ());
   try
   {
    parser.parse (new File (this.xsdFileName));
@@ -89,7 +145,6 @@ public class ServiceContractModelPescXsdLoader implements
    throw new IllegalArgumentException (ex);
   }
 
-
   XSSchemaSet schemaSet;
   try
   {
@@ -102,58 +157,112 @@ public class ServiceContractModelPescXsdLoader implements
 
   for (XSSchema schema : schemaSet.getSchemas ())
   {
-//   ServiceMethodReturnValue rv = method.getReturnValue ();
-//   if (rv != null)
-//   {
-//    XmlType xmlType = map.get (rv.getType ());
-//    if (xmlType == null)
-//    {
-//     xmlType = new XmlType ();
-//     map.put (rv.getType (), xmlType);
-//     xmlType.setName (rv.getType ());
-//     xmlType.setDesc ("???");
-//     xmlType.setComments ("???");
-//     xmlType.setExamples ("???");
-//     xmlType.setService (method.getService ());
-//    }
-//    else
-//    {
-//     if ( ! xmlType.getService ().contains (method.getService ()))
-//     {
-//      xmlType.setService (xmlType.getService () + ", " + method.getService ());
-//     }
-//    }
-//   }
-//   for (ServiceMethodParameter parm : method.getParameters ())
-//   {
-//    XmlType xmlType = map.get (parm.getType ());
-//    if (xmlType == null)
-//    {
-//     xmlType = new XmlType ();
-//     map.put (parm.getType (), xmlType);
-//     xmlType.setName (parm.getType ());
-//     xmlType.setDesc ("???");
-//     xmlType.setComments ("???");
-//     xmlType.setExamples ("???");
-//     xmlType.setService (method.getService ());
-//    }
-//    else
-//    {
-//     if ( ! xmlType.getService ().contains (method.getService ()))
-//     {
-//      xmlType.setService (xmlType.getService () + ", " + method.getService ());
-//     }
-//    }
-//   }
+   System.out.println ("Schema Namespace=" + schema.getTargetNamespace ());
+   for (XSComplexType ct : schema.getComplexTypes ().values ())
+   {
+    System.out.println ("ComplexType =" + ct.getName ());
+    addComplexType (ct);
+   }
   }
-  return new ArrayList (map.values ());
  }
 
- @Override
- public List<MessageStructure> getMessageStructures ()
+ private void addComplexType (XSComplexType complexType)
  {
-  List<MessageStructure> list = new ArrayList ();
+  XmlType xmlType = xmlTypeMap.get (complexType.getName ());
+  if (xmlType != null)
+  {
+   System.out.println ("Already processed complex Type="
+                       + complexType.getName ());
+   return;
+  }
+  xmlType = new XmlType ();
+  xmlTypeMap.put (complexType.getName (), xmlType);
+  xmlType.setName (complexType.getName ());
+  xmlType.setDesc (calcMissing (calcDesc (complexType.getAnnotation ())));
+  xmlType.setComments ("???");
+  xmlType.setExamples ("???");
+  xmlType.setService ("Pesc");
 
-  return list;
+  XSContentType contentType = complexType.getContentType ();
+  XSParticle particle = contentType.asParticle ();
+  if (particle != null)
+  {
+   XSTerm term = particle.getTerm ();
+   if (term.isModelGroup ())
+   {
+    XSModelGroup xsModelGroup = term.asModelGroup ();
+    XSParticle[] particles = xsModelGroup.getChildren ();
+    for (XSParticle p : particles)
+    {
+     XSTerm pterm = p.getTerm ();
+     if (pterm.isElementDecl ())
+     { //xs:element inside complex type
+      XSElementDecl element = pterm.asElementDecl ();
+      addMessageStructure (xmlType.getName (), element);
+     }
+    }
+   }
+  }
+ }
+
+ private String calcMissing (String str)
+ {
+  if (str == null)
+  {
+   return "???";
+  }
+  if (str.trim ().isEmpty ())
+  {
+   return "???";
+  }
+  return str;
+ }
+
+ private String calcDesc (XSAnnotation annotation)
+ {
+  if (annotation == null)
+  {
+   return null;
+  }
+  if (annotation.getAnnotation () == null)
+  {
+   return null;
+  }
+  return annotation.getAnnotation ().toString ();
+ }
+
+ private void addMessageStructure (String xmlObject, XSElementDecl element)
+ {
+  MessageStructure ms = new MessageStructure ();
+  this.messageStructures.add (ms);
+  ms.setXmlObject (xmlObject);
+  ms.setName (element.getName ());
+  ms.setId (xmlObject + "." + ms.getName ());
+  ms.setType (calcType (element));
+  ms.setDescription (calcMissing (calcDesc (element.getAnnotation ())));
+  System.out.println ("Element " + ms.getId () + " " + ms.getType ());
+  ms.setRequired (calcRequired (element));
+  ms.setCardinality (calcCardinality (element));
+ }
+
+ private String calcType (XSElementDecl element)
+ {
+  return calcType (element.getType ());
+ }
+
+ private String calcType (XSType xsType)
+ {
+  String type = xsType.getName ();
+  return type;
+ }
+
+ private String calcRequired (XSElementDecl element)
+ {
+  return "No";
+ }
+
+ private String calcCardinality (XSElementDecl element)
+ {
+  return "One";
  }
 }
