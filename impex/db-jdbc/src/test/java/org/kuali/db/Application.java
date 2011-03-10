8fb4fc63837b3331f6ff9acc069a01c91a093fe4
jcaddel <jcaddel@kuali.org>
2011-03-10 09:22:03 -0500

    git-svn-id: https://svn.kuali.org/repos/student/deploymentlab/trunk@20301 0ac02d80-fb3f-11dc-a574-c57244a850eb

--- impex/db-jdbc/src/test/java/org/kuali/db/Application.java
+++ impex/db-jdbc/src/test/java/org/kuali/db/Application.java
@@ -1,9 +1,6 @@
 package org.kuali.db;
 
-import java.util.Vector;
-
-import org.kuali.db.jdbc.SQLExecutor;
-import org.kuali.db.jdbc.Transaction;
+import org.kuali.db.jdbc.ConnectionHandler;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
 
@@ -11,12 +8,15 @@ public class Application {
 
 	public static void main(final String[] args) {
 		try {
-			ApplicationContext context = new ClassPathXmlApplicationContext("org/kuali/db/jdbc.xml");
-			SQLExecutor executor = (SQLExecutor) context.getBean("jdbc.init.sql.executor");
-			Vector<Transaction> transactions = executor.getTransactions();
-			for (Transaction t : transactions) {
-				System.out.println(t.getSqlCommand());
-			}
+			ApplicationContext context = new ClassPathXmlApplicationContext("org/kuali/db/jdbc-context.xml");
+			ConnectionHandler handler = (ConnectionHandler) context.getBean("jdbc.dba.connectionHandler");
+			System.out.println(handler.getDriver());
+			/**
+			 * SQLExecutor executor = (SQLExecutor)
+			 * context.getBean("jdbc.init.sql.executor"); Vector<Transaction>
+			 * transactions = executor.getTransactions(); for (Transaction t :
+			 * transactions) { System.out.println(t.getSqlCommand()); }
+			 */
 		} catch (Throwable t) {
 			t.printStackTrace();
 		}
