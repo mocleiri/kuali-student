44bd8886dc3a7c2156d7a3b07118ca34a525b6f7
jcaddel <jcaddel@kuali.org>
2011-03-09 19:28:18 -0500

    git-svn-id: https://svn.kuali.org/repos/student/deploymentlab/trunk@20291 0ac02d80-fb3f-11dc-a574-c57244a850eb

--- impex/db-jdbc/src/test/java/org/kuali/db/Application.java
+++ impex/db-jdbc/src/test/java/org/kuali/db/Application.java
@@ -1,5 +1,9 @@
 package org.kuali.db;
 
+import java.util.Vector;
+
+import org.kuali.db.jdbc.SQLExecutor;
+import org.kuali.db.jdbc.Transaction;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
 
@@ -8,7 +12,11 @@ public class Application {
 	public static void main(final String[] args) {
 		try {
 			ApplicationContext context = new ClassPathXmlApplicationContext("org/kuali/db/jdbc.xml");
-			System.out.println(context.getBean("jdbc.sql.drop"));
+			SQLExecutor executor = (SQLExecutor) context.getBean("jdbc.init.sql.executor");
+			Vector<Transaction> transactions = executor.getTransactions();
+			for (Transaction t : transactions) {
+				System.out.println(t.getSqlCommand());
+			}
 		} catch (Throwable t) {
 			t.printStackTrace();
 		}
