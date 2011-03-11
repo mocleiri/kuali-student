16fc10ec2d2bbf079748e4908bc41dbe57619172
jcaddel <jcaddel@kuali.org>
2011-03-11 16:34:17 -0500

    git-svn-id: https://svn.kuali.org/repos/student/deploymentlab/trunk@20382 0ac02d80-fb3f-11dc-a574-c57244a850eb

--- impex/db-impex/src/test/java/org/kuali/db/impex/ApplicationTest.java
+++ impex/db-impex/src/test/java/org/kuali/db/impex/ApplicationTest.java
@@ -1,8 +1,9 @@
-package org.kuali.db.jdbc;
+package org.kuali.db.impex;
 
 import junit.framework.Assert;
 
 import org.junit.Test;
+import org.kuali.db.jdbc.Credentials;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
 
@@ -10,8 +11,9 @@ public class ApplicationTest {
 
 	@Test
 	public void appTest() {
-		ApplicationContext context = new ClassPathXmlApplicationContext("jdbc-test-context.xml");
+		ApplicationContext context = new ClassPathXmlApplicationContext("applicationContext.xml");
 		Credentials c = (Credentials) context.getBean("org.kuali.jdbc.dba.credentials");
 		Assert.assertNotNull(c);
 	}
+
 }
