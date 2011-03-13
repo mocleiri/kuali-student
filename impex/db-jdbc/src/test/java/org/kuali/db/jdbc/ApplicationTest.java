0d2efbd626693b55a633983da29777c40ab9af31
jcaddel <jcaddel@kuali.org>
2011-03-13 10:08:59 -0400

    git-svn-id: https://svn.kuali.org/repos/student/deploymentlab/trunk@20405 0ac02d80-fb3f-11dc-a574-c57244a850eb

--- impex/db-jdbc/src/test/java/org/kuali/db/jdbc/ApplicationTest.java
+++ impex/db-jdbc/src/test/java/org/kuali/db/jdbc/ApplicationTest.java
@@ -1,6 +1,5 @@
 package org.kuali.db.jdbc;
 
-
 import org.junit.Test;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
@@ -10,7 +9,14 @@ public class ApplicationTest {
 	@Test
 	public void appTest() {
 		ApplicationContext context = new ClassPathXmlApplicationContext("dba-context.xml");
-		String fooBean = (String) context.getBean("foo.bean");
-		System.out.println(fooBean);
+		showContext(context);
+	}
+
+	protected void showContext(ApplicationContext context) {
+		String[] names = context.getBeanDefinitionNames();
+		for (String name : names) {
+			Object bean = context.getBean(name);
+			System.out.println(name + "=" + bean);
+		}
 	}
 }
