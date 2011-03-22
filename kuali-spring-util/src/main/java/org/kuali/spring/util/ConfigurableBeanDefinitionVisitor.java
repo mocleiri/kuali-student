b102afb383e54f6dbbd900efaed90827adaf22b9
jcaddel <jcaddel@kuali.org>
2011-03-22 11:31:33 -0400

    git-svn-id: https://svn.kuali.org/repos/student/deploymentlab/trunk@20634 0ac02d80-fb3f-11dc-a574-c57244a850eb

--- kuali-spring-util/src/main/java/org/kuali/spring/util/ConfigurableBeanDefinitionVisitor.java
+++ kuali-spring-util/src/main/java/org/kuali/spring/util/ConfigurableBeanDefinitionVisitor.java
@@ -2,30 +2,13 @@ package org.kuali.spring.util;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-import org.springframework.beans.MutablePropertyValues;
-import org.springframework.beans.PropertyValue;
 import org.springframework.beans.factory.config.BeanDefinitionVisitor;
-import org.springframework.util.ObjectUtils;
 import org.springframework.util.StringValueResolver;
 
 public class ConfigurableBeanDefinitionVisitor extends BeanDefinitionVisitor {
 	final Logger logger = LoggerFactory.getLogger(ConfigurableBeanDefinitionVisitor.class);
 
 	StringValueResolver stringValueResolver;
-	PropertiesLoggerSupport loggerSupport;
-
-	@Override
-	protected void visitPropertyValues(MutablePropertyValues pvs) {
-		PropertyValue[] pvArray = pvs.getPropertyValues();
-		for (PropertyValue pv : pvArray) {
-			Object newVal = resolveValue(pv.getValue());
-			if (!ObjectUtils.nullSafeEquals(newVal, pv.getValue())) {
-				logger.info("Resolved '" + pv.getName() + "' [" + pv.getValue() + "]->["
-						+ loggerSupport.getPropertyValue(pv.getValue() + "", newVal + "") + "]");
-				pvs.add(pv.getName(), newVal);
-			}
-		}
-	}
 
 	@Override
 	protected String resolveStringValue(String strVal) {
@@ -35,7 +18,8 @@ public class ConfigurableBeanDefinitionVisitor extends BeanDefinitionVisitor {
 		String resolvedValue = this.stringValueResolver.resolveStringValue(strVal);
 		boolean equal = strVal.equals(resolvedValue);
 		if (!equal) {
-			logger.debug("Resolved " + strVal + "->" + loggerSupport.getPropertyValue(strVal, resolvedValue));
+			// logger.info("Resolved " + strVal + "->" + loggerSupport.getPropertyValue(strVal, resolvedValue));
+			// TODO Do "something" so the application is aware a bean property has been changed
 		}
 		// Return original String if not modified.
 		return (equal ? strVal : resolvedValue);
@@ -47,14 +31,6 @@ public class ConfigurableBeanDefinitionVisitor extends BeanDefinitionVisitor {
 
 	public void setStringValueResolver(StringValueResolver stringValueResolver) {
 		this.stringValueResolver = stringValueResolver;
-	}
-
-	public PropertiesLoggerSupport getLoggerSupport() {
-		return loggerSupport;
-	}
-
-	public void setLoggerSupport(PropertiesLoggerSupport loggerSupport) {
-		this.loggerSupport = loggerSupport;
 	}
 
 }
