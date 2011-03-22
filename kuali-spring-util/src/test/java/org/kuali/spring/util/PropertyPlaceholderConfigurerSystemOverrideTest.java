b11abdc383eeef0e1dc7107890231e2e3ab53aa1
jcaddel <jcaddel@kuali.org>
2011-03-22 18:35:15 -0400

    git-svn-id: https://svn.kuali.org/repos/student/deploymentlab/trunk@20656 0ac02d80-fb3f-11dc-a574-c57244a850eb

--- kuali-spring-util/src/test/java/org/kuali/spring/util/PropertyPlaceholderConfigurerSystemOverrideTest.java
+++ kuali-spring-util/src/test/java/org/kuali/spring/util/PropertyPlaceholderConfigurerSystemOverrideTest.java
@@ -3,11 +3,14 @@ package org.kuali.spring.util;
 import java.util.Properties;
 
 import org.junit.AfterClass;
+import org.junit.Assert;
 import org.junit.BeforeClass;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.ApplicationContext;
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 
@@ -16,27 +19,31 @@ import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 public class PropertyPlaceholderConfigurerSystemOverrideTest {
 	static final Logger logger = LoggerFactory.getLogger(PropertyPlaceholderConfigurerSystemOverrideTest.class);
 	static String key = "cat";
-	static String val = "cheetah";
+	static String cheetah = "cheetah";
 	static Properties systemProperties = System.getProperties();
+
+	@Autowired
+	private ApplicationContext applicationContext;
 
 	@BeforeClass
 	public static void oneTimeSetUp() {
-		logger.info("Setting system property {}={}", key, val);
-		System.setProperty(key, val);
+		logger.info("Setting system property {}={}", key, cheetah);
+		systemProperties.setProperty(key, cheetah);
 	}
-	
-    @AfterClass
-    public static void oneTimeTearDown() {
-		logger.info("Removing system property {}={}", key, val);
+
+	@AfterClass
+	public static void oneTimeTearDown() {
+		logger.info("Removing system property {}={}", key, cheetah);
 		systemProperties.remove(key);
-    }
-
-
+	}
 
 	@Test
 	public void systemPropertyOverrideTest() {
-		// Nothing to do. This test exists just to make sure Spring can process and load
-		// PropertyPlaceholderConfigurerSystemOverrideTest-context.xml
+		ResolvePropertiesFirstPlaceholderConfigurer configurer = (ResolvePropertiesFirstPlaceholderConfigurer) applicationContext
+				.getBean("kuali.spring.property.configurer.test");
+		Properties properties = configurer.getProperties();
+		String cat = properties.getProperty("cat");
+		Assert.assertEquals(cat, cheetah);
 	}
 
 }
