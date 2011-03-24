3a1dc931ee35d16663e2df736c1febc0f2909ea4
jcaddel <jcaddel@kuali.org>
2011-03-24 12:45:13 -0400

    git-svn-id: https://svn.kuali.org/repos/student/deploymentlab/trunk@20714 0ac02d80-fb3f-11dc-a574-c57244a850eb

---
+++ kuali-spring-util/src/main/java/org/kuali/spring/util/PropertiesLoader.java
@@ -0,0 +1,181 @@
+package org.kuali.spring.util;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.util.Properties;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.core.io.Resource;
+import org.springframework.core.io.support.PropertiesLoaderSupport;
+import org.springframework.util.DefaultPropertiesPersister;
+import org.springframework.util.ObjectUtils;
+import org.springframework.util.PropertiesPersister;
+
+public class PropertiesLoader {
+	final Logger logger = LoggerFactory.getLogger(PropertiesLoader.class);
+	public static final boolean DEFAULT_IGNORE_RESOURCE_NOT_FOUND = false;
+
+	PropertiesPersister persister = new DefaultPropertiesPersister();
+	PropertiesLoggerSupport loggerSupport = new PropertiesLoggerSupport();
+
+	boolean ignoreResourceNotFound = DEFAULT_IGNORE_RESOURCE_NOT_FOUND;
+	String fileEncoding;
+
+	public PropertiesLoader() {
+		this(DEFAULT_IGNORE_RESOURCE_NOT_FOUND, null);
+	}
+
+	public PropertiesLoader(boolean ignoreResourceNotFound, String fileEncoding) {
+		super();
+		setIgnoreResourceNotFound(ignoreResourceNotFound);
+		setFileEncoding(fileEncoding);
+	}
+
+	protected Properties loadProperties(Resource location, InputStream is) throws IOException {
+		Properties properties = new Properties();
+		if (isXMLFile(location)) {
+			this.persister.loadFromXml(properties, is);
+			return properties;
+		}
+
+		// It is not an xml file
+		if (this.fileEncoding == null) {
+			this.persister.load(properties, is);
+		} else {
+			this.persister.load(properties, new InputStreamReader(is, this.fileEncoding));
+		}
+		return properties;
+	}
+
+	protected Properties getProperties(Resource location) throws IOException {
+		logger.info("Loading properties from {}", location);
+		InputStream is = null;
+		try {
+			is = location.getInputStream();
+			return loadProperties(location, is);
+		} catch (IOException e) {
+			handleIOException(location, e);
+		} finally {
+			nullSafeClose(is);
+		}
+		return new Properties();
+	}
+
+	protected boolean isXMLFile(Resource location) {
+		String filename = null;
+		try {
+			filename = location.getFilename();
+		} catch (IllegalStateException ex) {
+			// resource is not file-based. See SPR-7552.
+			return false;
+		}
+		// May not have thrown an exception, but might still be null
+		if (filename == null) {
+			return false;
+		}
+		// It's an XML file
+		if (filename.endsWith(PropertiesLoaderSupport.XML_FILE_EXTENSION)) {
+			return true;
+		} else {
+			return false;
+		}
+	}
+
+	protected void handleIOException(Resource location, IOException e) throws IOException {
+		if (!this.ignoreResourceNotFound) {
+			throw e;
+		}
+		logger.warn("Could not load properties from {}: {}", location, e.getMessage());
+	}
+
+	protected void nullSafeClose(InputStream is) throws IOException {
+		if (is == null) {
+			return;
+		}
+		is.close();
+	}
+
+	public void loadProperties(Properties properties, Resource[] locations) throws IOException {
+		if (locations == null || locations.length == 0) {
+			logger.info("No property locations to load from");
+			return;
+		}
+		for (Resource location : locations) {
+			Properties newProps = getProperties(location);
+			mergeProperties(properties, newProps, true, PropertiesSource.RESOURCE.toString());
+		}
+	}
+
+	/**
+	 * Merge the property under 'key' from newProps into currentProps. If override is false and the property already
+	 * exists do not update currentProps
+	 * 
+	 * @param currentProps
+	 * @param newProps
+	 * @param key
+	 * @param override
+	 * @param src
+	 */
+	protected void mergeProperty(Properties currentProps, Properties newProps, String key, boolean override, String src) {
+		// Extract the new value
+		String newValue = newProps.getProperty(key);
+
+		// Ignore values that are null
+		if (newValue == null) {
+			return;
+		}
+
+		// Extract the existing property
+		String currentValue = currentProps.getProperty(key);
+
+		// There is no existing property
+		if (currentValue == null) {
+			logger.debug("Adding " + src + " property {}=[{}]", key, newValue);
+			currentProps.setProperty(key, newValue);
+			return;
+		}
+
+		// Values are the same, nothing to do
+		if (ObjectUtils.nullSafeEquals(newValue, currentValue)) {
+			return;
+		}
+
+		// There is an existing property, but the new property wins
+		if (override) {
+			logger.info(src + " property override for '" + key + "' [{}]->[{}]", currentValue, newValue);
+			currentProps.setProperty(key, newValue);
+		}
+	}
+
+	public void mergeProperties(Properties currentProps, Properties newProps, boolean override, String source) {
+		for (String key : newProps.stringPropertyNames()) {
+			mergeProperty(currentProps, newProps, key, override, source);
+		}
+	}
+
+	public boolean isIgnoreResourceNotFound() {
+		return ignoreResourceNotFound;
+	}
+
+	public void setIgnoreResourceNotFound(boolean ignoreResourceNotFound) {
+		this.ignoreResourceNotFound = ignoreResourceNotFound;
+	}
+
+	public PropertiesPersister getPersister() {
+		return persister;
+	}
+
+	public void setPersister(PropertiesPersister propertiesPersister) {
+		this.persister = propertiesPersister;
+	}
+
+	public String getFileEncoding() {
+		return fileEncoding;
+	}
+
+	public void setFileEncoding(String fileEncoding) {
+		this.fileEncoding = fileEncoding;
+	}
+}
