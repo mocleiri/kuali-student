c15e87077057e8e6b9b4e3f0ffcf1edbd7f2633f
jcaddel <jcaddel@kuali.org>
2011-04-07 08:42:36 -0400

    [maven-release-plugin]  copy for tag kuali-79
    git-svn-id: https://svn.kuali.org/repos/student/deploymentlab/tags/kuali-79@20999 0ac02d80-fb3f-11dc-a574-c57244a850eb

--- kuali-spring-util/src/main/java/org/kuali/spring/util/SystemPropertiesMode.java
+++
@@ -1,7 +0,0 @@
-package org.kuali.spring.util;
-
-public enum SystemPropertiesMode {
-	SYSTEM_PROPERTIES_MODE_NEVER, // Never use system properties
-	SYSTEM_PROPERTIES_MODE_FALLBACK, // Only use the system property if there is no regular property
-	SYSTEM_PROPERTIES_MODE_OVERRIDE; // Always use the system property if there is one
-}
