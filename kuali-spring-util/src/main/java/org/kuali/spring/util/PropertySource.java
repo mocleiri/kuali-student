49ee50bab97d2c6f443c7d1ba55f25c05db64755
jcaddel <jcaddel@kuali.org>
2011-04-15 15:33:33 -0400

    [maven-release-plugin]  copy for tag multimodule-1.0.1
    git-svn-id: https://svn.kuali.org/repos/student/deploymentlab/tags/multimodule-1.0.1@21476 0ac02d80-fb3f-11dc-a574-c57244a850eb

--- kuali-spring-util/src/main/java/org/kuali/spring/util/PropertySource.java
+++
@@ -1,9 +0,0 @@
-package org.kuali.spring.util;
-
-public enum PropertySource {
-	SYSTEM, // A system property
-	ENVIRONMENT, // An environment property
-	RESOURCE, // A property loaded from a Resource
-	LOCAL, // A property from a properties object injected into the factory bean
-	OTHER; // Some place besides one of the above
-}
