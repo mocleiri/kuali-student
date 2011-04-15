cf91aea574b9c8fb5a3e9f14149ef814a281a605
jcaddel <jcaddel@kuali.org>
2011-04-15 12:05:20 -0400

    [maven-release-plugin]  copy for tag maven-kuali-common-1.0.18
    git-svn-id: https://svn.kuali.org/repos/student/deploymentlab/tags/maven-kuali-common-1.0.18@21422 0ac02d80-fb3f-11dc-a574-c57244a850eb

--- kuali-spring-util/src/main/java/org/kuali/spring/util/PropertyMergeType.java
+++
@@ -1,9 +0,0 @@
-package org.kuali.spring.util;
-
-public enum PropertyMergeType {
-	NULL_NEW_VALUE, // New value was null, nothing changed
-	IDENTICAL_VALUES, // New value is the same as the old value, nothing changed
-	EXISTING_PROPERTY_WINS, // Existing property won out over the new property
-	ADD, // No existing property, property was added
-	OVERRIDE; // Existing property was overridden by the new property
-}
