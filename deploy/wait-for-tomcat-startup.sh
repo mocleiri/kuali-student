6ac2d404ee611cb32bfd1ecb3a6f16fb7e578f34
jcaddel <jcaddel@kuali.org>
2011-06-25 08:15:42 -0400

    git-svn-id: https://svn.kuali.org/repos/student/deploymentlab/trunk@22434 0ac02d80-fb3f-11dc-a574-c57244a850eb

--- deploy/wait-for-tomcat-startup.sh
+++ deploy/wait-for-tomcat-startup.sh
@@ -1,8 +1,19 @@
 #!/bin/bash
 
+URL=$1
+
+if [ "$URL" = "" ]
+then
+  echo URL is empty
+  echo ------------------------------------------------------------------------------------------
+  echo Usage: wait-for-tomcat-startup.sh http://dev.ks.kuali.org
+  echo ------------------------------------------------------------------------------------------
+  exit 1
+fi
+
 counter=50
 echo --------- Determining Tomcat status - `date`
-until [ "`curl --silent --connect-timeout 3 --max-time 3 -I http://localhost | grep 'Coyote'`" != "" ];
+until [ "`curl --silent --connect-timeout 3 --max-time 3 -I $URL | grep 'Coyote'`" != "" ];
 do
   echo --- $counter --- Waiting for Tomcat to start - `date`
   sleep 3
