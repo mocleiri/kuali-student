fdd097213fd4ce87382b269893ba819e30c46473
alubbers <alubbers@kuali.org>
2011-08-26 19:11:43 -0400

    Updated liquibase files with changes to data structure from rice-m7 upgrade
    git-svn-id: https://svn.kuali.org/repos/student/sandbox/ks-db-gen/trunk@23658 0ac02d80-fb3f-11dc-a574-c57244a850eb

--- liquibase/src/main/resources/xmlConvert.rb
+++ liquibase/src/main/resources/xmlConvert.rb
@@ -1,7 +1,11 @@
+# Converts impex xml data files into liquibase changesets
+
+
 require 'xmlsimple'
 
 @indent = "\t"
-@impexDirName = '/home/andy/src/kualiWorkspace/ks-1.3/ks-cfg-dbs/ks-embedded-db/src/main/impex'
+@ksHomeDir = '/home/andy/src/kualiWorkspace/ks-1.3'
+@impexDirName = @ksHomeDir + '/ks-cfg-dbs/ks-embedded-db/src/main/impex'
 @dataHeader = '<databaseChangeLog xmlns="http://www.liquibase.org/xml/ns/dbchangelog" xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance" xsi:schemaLocation="http://www.liquibase.org/xml/ns/dbchangelog http://www.liquibase.org/xml/ns/dbchangelog/dbchangelog-2.0.xsd">'
 @dataFooter = '</databaseChangeLog>'
 
@@ -14,6 +18,9 @@ def writeInsertTags(inFileName, outFile)
 			outFile.puts (@indent * 2) + '<insert tableName="' + tableName + '">'
 			row.each_key do |colName|
 				colValue = row[colName]
+				colValue.gsub!('&', '&amp;')
+				colValue.gsub!('<', '&lt;')
+				colValue.gsub!('"', '&quot;')
 				outFile.puts (@indent * 3) + '<column name="' + colName+ '" value="' + colValue + '"/>'
 			end
 			outFile.puts (@indent * 2) + '</insert>'
@@ -76,15 +83,18 @@ impexDir.each do |dirEntry|
 	if dirEntry.include? '.xml' and dirEntry != 'schema.xml'
 		tableName = dirEntry.sub('.xml', '')
 		absoluteFile = impexDir.path + '/' + dirEntry
+		if @tableModules[tableName] == nil
+			puts tableName
+		end
 		outputPath = @tableModules[tableName] + '/' + dirEntry
 		
 		outputFile = File.open(outputPath, 'w')
 		outputFile.puts @dataHeader
-		outputFile.puts @indent + '<changeLog id="' + tableName + '-initial-data" author="alubbers">'
+		outputFile.puts @indent + '<changeSet id="' + tableName + '-initial-data" author="alubbers">'
 		
 		writeInsertTags(absoluteFile, outputFile)
 		
-		outputFile.puts @indent + '</changeLog>'
+		outputFile.puts @indent + '</changeSet>'
 		outputFile.puts@dataFooter
 		outputFile.close
 	end
