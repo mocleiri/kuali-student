84444113f3972e48ea18d50e927d55f23876aa7b
jcaddel <jcaddel@kuali.org>
2011-11-09 22:37:02 -0500

    git-svn-id: https://svn.kuali.org/repos/student/deploymentlab/trunk@24926 0ac02d80-fb3f-11dc-a574-c57244a850eb

---
+++ maven-cloudfront-plugin/src/main/java/org/kuali/maven/mojo/s3/Depth.java
@@ -0,0 +1,18 @@
+package org.kuali.maven.mojo.s3;
+
+public class Depth {
+    int depth;
+
+    public void increment() {
+        depth++;
+    }
+
+    public void decrement() {
+        depth--;
+    }
+
+    public int getDepth() {
+        return depth;
+    }
+
+}
