12775b03f81ad75aa710e5288d425b06941387c3
paulheald <paulheald@kuali.org>
2011-12-20 13:55:31 -0500

    Small updates.
    git-svn-id: https://svn.kuali.org/repos/student/contrib/ksa/trunk@25654 0ac02d80-fb3f-11dc-a574-c57244a850eb

--- ksa-core/src/main/java/com/sigmasys/kuali/ksa/rm/Transaction.java
+++ ksa-core/src/main/java/com/sigmasys/kuali/ksa/rm/Transaction.java
@@ -29,20 +29,23 @@ public abstract class Transaction {
 	boolean internalTransaction; 
 	/** This is the amount of the transaction that has been allocated. For example, if a $1000 payment is put towards a $2000 charge, the $1000 will have a $1000 allocation amount, and the $2000 charge will have a $1000 allocation amount. The PA module is responsible for allocating charges to payments. */
 	BigDecimal allocatedAmount; 
-	/** This value is set to a value (up to the maximum of localAmount) and is the value of the transaction that has been allocated, and may not be reallocated by the payment application routine. Most commonly, the entire transaction will be locked, by setting the lockedAllocationAmount to the localValue. */
+	/** This value is set to a value (up to the maximum of localAmount) and is the value of the transaction that has been allocated, and may not be reallocated by the payment application routine. Most commonly, the entire transaction will be locked, by setting the lockedAllocationAmount to the localValue. 
+	 * Schools will also likely lock their allocations of transactions before the current period of time (for example, if an allocation was made with a payment in a pervious year, it will not
+	 * be allowed to be deallocated by the system.
+	 * */
 	BigDecimal lockedAllocationAmount; 
 	/** This is the code for the transaction, and it defines the type of the transaction. The transaction code for charges are responsible for indicating the priority of the charge, and the general ledger accounts that the charge relates to. The code for payments also indicate priority, as well as restrictions that are placed on the payment, including processing times, refund rule, and the charges to which they may be applied. This functionality is still being determined. */
 	String transctionType;
 	/** The responsible entity is the entity that created the transaction. It is assumed the identity will be derived from KIM. */
 	String responsibleEntity; 
-	/** The text for the transaction that will be displayed as a summary of the transaction on statements. For example "Bookstore Purchase" */
+	/** The text for the transaction that will be displayed as a summary of the transaction on statements. For example "Bookstore Purchase"  If this string is null during instantiation, the default text will
+	 * be populated from the transaction code.*/
 	String statementText; 
 	/** A reference to an external document that gives more granular information about the transaction. This information will be different depending on the type of transaction. The information will be available when a user "drills down" into a transaction. It will contain information relating to the charge or payment, such as the contact details for the department who generated a charge, and other information as made available by the system. For example, a charge from the bookstore may list the items purchased. The document referenced will be an XML document, allowing flexible creation of different document types by the institutions themselves. Some basic document types will be defined by the project. */
 	String documentReference; 
-	/** If this value is set, it points to the most recent memo line that refers to this transaction. For example, if the system locks the transaction and enters a memo line in the account memo, the CSR would be able to pull up the transaction and see the memo line directly, instead of having to search for it.*/
-	
-	
-	
+	/** If this value is non null, it points to the most recent memo line that refers to this transaction. For example, if the system locks the transaction and enters a memo line in the account memo, the CSR would be able to pull up the transaction and see the memo line directly, instead of having to search for it.*/
+	String memoReference;
+		
 	
 	/** This will allocate the value of amount on the transaction. A check will be made to ensure that the allocated amount
 	 *  is equal to or less than the localAmount, less any lockedAllocationAmount. The expectation is that this method will only
@@ -57,7 +60,7 @@ public abstract class Transaction {
 	public void allocateAmount (BigDecimal amount) {
 	}
 	
-	/* This will allocate a locked amount on the transaction. A check will be made to ensure that the lockedAmount and the allocateAmount don't exceed the ledgerAmount
+	/** This will allocate a locked amount on the transaction. A check will be made to ensure that the lockedAmount and the allocateAmount don't exceed the ledgerAmount
 	 * of the transaction. Setting an amount as locked prevents the payment application system from reallocating the balance elsewhere. /*
 	 *  
 	 */	
@@ -65,8 +68,9 @@ public abstract class Transaction {
 		
 	}
 	
-	/*This method can set the internal switch on or off for a transaction. I cannot think of a time when an internal transaction would
-	 * not also be locked (tbd) so we should ensure that if a transaction is being set to internal, its lockedAllocationAmount == ledgerAmount
+	/** This method can set the internal switch on or off for a transaction. I cannot think of a time when an internal transaction would
+	 * not also be locked (tbd) so we should ensure that if a transaction is being set to internal, its lockedAllocationAmount == ledgerAmount. 
+	 * Internal transactions are not generally displayed to the user, and are only displayed to the CSR when asked for. 
 	 * 
 	 * 
 	 */
@@ -75,13 +79,33 @@ public abstract class Transaction {
 		
 	}
 	
-	
-	
+	/**  If a document reference is altered, then a new one can be rereferenced. The documents associated wtih each transaction give more details about the transaction, and are 
+	 * for information for both the student and the CSR. For example, if the bookstore wanted to, it could lists the books it had sold in a transaction so the student would be
+	 * able to drill down and see what books made up a line on their statement.
+	 * 
+	 */
 	public void setDocumentReference (String newDocumentReference){
 		
 	}
 	
+	/** If a memos can be generated in a number of ways. If a memo is generated against a transaction, it is placed in the main memo account, and also the memoReference is
+	 * set to point to that memo. This allows the CSR to see the most recent memo associated with a certain transaction. Certain transaction instantiations will generate a memo
+	 * as soon as they are created.
+	 * 
+	 */
+	public void generateTransactionMemo (String memoText){
+		
+	}
 
+	/**
+	 * If the reverse method is called, the system will generate a negative transaction for the type of the original transaction. A memo transaction will be generated, and
+	 * the transactions will be locked together. Subject to user customization, the transactions may be marked as hidden. (likely that credits will not be hidden, debits will.)
+	 * 
+	 */
+	public void reverse(){
+		
+	}
+	
 }
 	
 
