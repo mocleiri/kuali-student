80c79fbcc06779be4dfe1b3e124e45f10e5034dc
mivanov <mivanov@kuali.org>
2012-01-26 18:38:55 -0500

    Added a composite PK
    git-svn-id: https://svn.kuali.org/repos/student/contrib/ksa/trunk@26442 0ac02d80-fb3f-11dc-a574-c57244a850eb

---
+++ ksa-core/src/main/java/com/sigmasys/kuali/ksa/model/TransactionTypeId.java
@@ -0,0 +1,82 @@
+package com.sigmasys.kuali.ksa.model;
+
+import java.io.Serializable;
+
+import javax.persistence.Column;
+
+public class TransactionTypeId implements Serializable {
+	
+	    private String id;    
+	    private String subCode;
+	    
+	    
+	    public TransactionTypeId() {
+	    }
+	    
+	    public TransactionTypeId(String id, String subCode) {
+	        setId(id);
+	        setSubCode(subCode);
+	    }
+	    
+	    @Column(name = "ID")
+	    public String getId() {
+	        return id;
+	    }
+	    public void setId(String id) {
+	        this.id = id;
+	    }
+	    
+	    @Column(name = "SUBCODE")
+	    public String getSubCode() {
+	        return subCode;
+	    }
+	    public void setSubCode(String subCode) {
+	        this.subCode = subCode;
+	    }
+	    
+	    @Override
+	    public int hashCode() {
+	        final int prime = 31;
+	        int result = 1;
+	        result = prime * result + ((id == null) ? 0 : id.hashCode());
+	        result = prime * result + ((subCode == null) ? 0 : subCode.hashCode());
+	        return result;
+	    }
+	    
+	    @Override
+	    public boolean equals(Object obj) {
+	        
+	    	if (this == obj) {
+	            return true;
+	    	}    
+	        
+	        if (obj == null) {
+	            return false;
+	        }    
+	        
+	        if (getClass() != obj.getClass()) {
+	            return false;
+	        }    
+	        
+	        TransactionTypeId other = (TransactionTypeId) obj;
+	        
+	        if (getId() == null) {
+	            if (other.getId() != null) {
+	                return false;
+	            }    
+	        } else if (!getId().equals(other.getId())) {
+	            return false;
+	        }    
+	        
+	        if (getSubCode() == null) {
+	            if (other.getSubCode() != null) {
+	                return false;
+	            }    
+	        } else if (!getSubCode().equals(other.getSubCode())) {
+	            return false;
+	        }    
+	        
+	        return true;
+	    }
+
+}
