ecac81a16c8681e7b8254e146482dc5fc295b391
mivanov <mivanov@kuali.org>
2012-01-26 19:09:02 -0500

    Added JPA annotations
    git-svn-id: https://svn.kuali.org/repos/student/contrib/ksa/trunk@26444 0ac02d80-fb3f-11dc-a574-c57244a850eb

--- ksa-core/src/main/java/com/sigmasys/kuali/ksa/model/Charge.java
+++ ksa-core/src/main/java/com/sigmasys/kuali/ksa/model/Charge.java
@@ -1,5 +1,10 @@
 package com.sigmasys.kuali.ksa.model;
 
+import javax.persistence.DiscriminatorValue;
+import javax.persistence.Entity;
+
+@Entity
+@DiscriminatorValue("C")
 public class Charge extends Debit {
 
     // TODO: Do we need Charge if it is the only one Credit transaction type?
