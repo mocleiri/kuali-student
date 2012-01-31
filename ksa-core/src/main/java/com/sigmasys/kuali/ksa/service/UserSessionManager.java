e1dcd71cec728c54754056432b8687fe8820ad9a
mivanov <mivanov@kuali.org>
2012-01-30 19:17:47 -0500

    git-svn-id: https://svn.kuali.org/repos/student/contrib/ksa/trunk@26518 0ac02d80-fb3f-11dc-a574-c57244a850eb

---
+++ ksa-core/src/main/java/com/sigmasys/kuali/ksa/service/UserSessionManager.java
@@ -0,0 +1,44 @@
+package com.sigmasys.kuali.ksa.service;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+public interface UserSessionManager {
+
+
+    /**
+     * Creates a new HTTP session
+     *
+     * @param request    the HTTP request
+     * @param response   the HTTP response
+     * @param user       the User instance
+     * @param userGroups user groups
+     */
+    void createSession(HttpServletRequest request, HttpServletResponse response, String userName);
+
+    /**
+     * Destroys the current HTTP session
+     *
+     * @param request the HTTP request
+     */
+    void destroySession(HttpServletRequest request);
+
+    /**
+     * Checks if the user session is valid.
+     *
+     * @param request  the HTTP request
+     * @param response the HTTP response
+     * @return true if the session is valid.
+     */
+    boolean isSessionValid(HttpServletRequest request, HttpServletResponse response);
+
+    /**
+     * Returns the user ID without domain name. This method should be called after session creation and validation.
+     *
+     * @param request HttpServletRequest
+     * @return the user ID
+     */
+    String getUserId(HttpServletRequest request);
+
+}
+
