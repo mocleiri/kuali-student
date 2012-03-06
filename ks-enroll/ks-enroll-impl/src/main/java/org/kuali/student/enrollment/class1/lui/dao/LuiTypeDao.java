4400abafd43add9f676d2962030a7cda2b2d0437
jcaddel <jcaddel@kuali.org>
2012-03-06 17:27:19 -0500

    git-svn-id: https://svn.kuali.org/repos/student/sandbox/ks-1.3-core-slice-demo/modules/ks-pom/trunk@27833 0ac02d80-fb3f-11dc-a574-c57244a850eb

--- ks-enroll/ks-enroll-impl/src/main/java/org/kuali/student/enrollment/class1/lui/dao/LuiTypeDao.java
+++
@@ -1,8 +0,0 @@
-package org.kuali.student.enrollment.class1.lui.dao;
-
-import org.kuali.student.enrollment.class1.lui.model.LuiTypeEntity;
-import org.kuali.student.enrollment.dao.GenericEntityDao;
-
-public class LuiTypeDao extends GenericEntityDao<LuiTypeEntity>{
-
-}
