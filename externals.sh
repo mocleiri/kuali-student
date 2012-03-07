afdca0f58b1d36ec7267638bc33592552606e37a
jcaddel <jcaddel@kuali.org>
2012-03-07 15:02:44 -0500

    git-svn-id: https://svn.kuali.org/repos/student/sandbox/ks-1.3-core-slice-demo/modules/ks-pom/trunk@28004 0ac02d80-fb3f-11dc-a574-c57244a850eb

--- externals.sh
+++ externals.sh
@@ -1,13 +1,1 @@
-URL=http://svn.kuali.org/repos/student/sandbox/ks-1.3-core-slice-demo/modules
-
-EXT1="ks-api $URL/ks-api/trunk"
-EXT2="ks-core $URL/ks-core/trunk"
-EXT3="ks-lum $URL/ks-lum/trunk"
-EXT4="ks-enroll $URL/ks-enroll/trunk"
-
-ARGS="'$EXT1' '$EXT2' '$EXT3' '$EXT4'"
-ARGS="'$EXT1'"
-
-echo $ARGS
-
-svn propset svn:externals $ARGS .
+svn propset svn:externals -F svn.externals .
