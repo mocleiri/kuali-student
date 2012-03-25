ceff8af83d88fb0d51477e23a1aecd9e9b15f193
mivanov <mivanov@kuali.org>
2012-03-25 14:31:20 -0400

    KRAD experiments....
    git-svn-id: https://svn.kuali.org/repos/student/contrib/ksa/trunk@28939 0ac02d80-fb3f-11dc-a574-c57244a850eb

--- ksa-core/src/main/java/com/sigmasys/kuali/ksa/config/KsaConfigurer.java
+++ ksa-core/src/main/java/com/sigmasys/kuali/ksa/config/KsaConfigurer.java
@@ -5,8 +5,13 @@ import org.kuali.rice.core.api.config.module.RunMode;
 import org.kuali.rice.core.framework.config.module.ModuleConfigurer;
 import org.kuali.rice.core.framework.config.module.WebModuleConfiguration;
 import org.kuali.rice.core.framework.persistence.ojb.RiceDataSourceConnectionFactory;
+import org.kuali.rice.krad.service.DataDictionaryService;
+import org.kuali.rice.krad.service.KRADServiceLocatorWeb;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.ApplicationContextAware;
+import org.springframework.context.ApplicationEvent;
+import org.springframework.context.event.ContextRefreshedEvent;
+import org.springframework.context.event.SmartApplicationListener;
 
 import java.util.Arrays;
 import java.util.List;
@@ -18,10 +23,12 @@ import java.util.List;
  *         Date: 3/22/12
  *         Time: 12:17 AM
  */
-public class KsaConfigurer extends ModuleConfigurer implements ApplicationContextAware {
+public class KsaConfigurer extends ModuleConfigurer implements ApplicationContextAware, SmartApplicationListener {
 
     private static final String KSA_BEANS_PATH = "classpath:/META-INF/";
     private static final String KSA_BEANS_FILE = KSA_BEANS_PATH + "ksa-beans.xml";
+
+    private List<String> dataDictionaryPackages;
 
     public KsaConfigurer() {
         super(Constants.MODULE_NAME);
@@ -59,5 +66,42 @@ public class KsaConfigurer extends ModuleConfigurer implements ApplicationContex
     public void setApplicationContext(ApplicationContext applicationContext) {
         // Required by Rice OJB
         RiceDataSourceConnectionFactory.addBeanFactory(applicationContext);
+    }
+
+    @Override
+    public boolean supportsEventType(Class<? extends ApplicationEvent> aClass) {
+        return true;
+    }
+
+    @Override
+    public boolean supportsSourceType(Class<?> aClass) {
+        return true;
+    }
+
+    @Override
+    public int getOrder() {
+        // return a lower value which will give the data dictionary indexing higher precedence since DD indexing should
+        // be started as soon as it can be
+        return -500;
+    }
+
+    @Override
+    public void onApplicationEvent(ApplicationEvent applicationEvent) {
+        if (applicationEvent instanceof ContextRefreshedEvent) {
+            DataDictionaryService dataDictionaryService = KRADServiceLocatorWeb.getDataDictionaryService();
+            try {
+                dataDictionaryService.addDataDictionaryLocations(getDataDictionaryPackages());
+            } catch (Exception e) {
+                throw new RuntimeException("Cannot add KSA packages to data dictionary locations: " + e.getMessage(), e);
+            }
+        }
+    }
+
+    public List<String> getDataDictionaryPackages() {
+        return dataDictionaryPackages;
+    }
+
+    public void setDataDictionaryPackages(List<String> dataDictionaryPackages) {
+        this.dataDictionaryPackages = dataDictionaryPackages;
     }
 }
