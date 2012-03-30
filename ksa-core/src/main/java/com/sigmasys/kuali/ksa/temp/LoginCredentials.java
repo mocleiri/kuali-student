f45c46a91d46081d6d1030cf6f4eb70ccba3fd40
mivanov <mivanov@kuali.org>
2012-03-30 11:11:36 -0400

    refactoring
    git-svn-id: https://svn.kuali.org/repos/student/contrib/ksa/trunk@29181 0ac02d80-fb3f-11dc-a574-c57244a850eb

--- ksa-core/src/main/java/com/sigmasys/kuali/ksa/temp/LoginCredentials.java
+++ ksa-core/src/main/java/com/sigmasys/kuali/ksa/temp/LoginCredentials.java
@@ -9,20 +9,15 @@ package com.sigmasys.kuali.ksa.temp;
  */
 public class LoginCredentials {
 
-    private static String defaultUserName;
-    private static String defaultPswd;
+    private static final String defaultUserName = "sigma";
+    private static final String defaultPswd = "sigma";
 
     public static Login login(Login bean) {
-        defaultUserName = "sigma";
-        defaultPswd = "sigma";
+
         String username = bean.getUserName();
         String userpswd = bean.getPswd();
 
-        if (defaultUserName.equals(username) && defaultPswd.equals(userpswd)) {
-            bean.setValid(true);
-        } else {
-            bean.setValid(false);
-        }
+        bean.setValid(defaultUserName.equals(username) && defaultPswd.equals(userpswd));
 
         return bean;
 
