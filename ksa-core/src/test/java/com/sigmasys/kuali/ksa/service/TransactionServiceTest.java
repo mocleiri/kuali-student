67246bc56a4095a2b702f9e8a29748be30a68dd1
mivanov <mivanov@kuali.org>
2012-04-07 15:18:02 -0400

    added a few transaction tests
    git-svn-id: https://svn.kuali.org/repos/student/contrib/ksa/trunk@29537 0ac02d80-fb3f-11dc-a574-c57244a850eb

--- ksa-core/src/test/java/com/sigmasys/kuali/ksa/service/TransactionServiceTest.java
+++ ksa-core/src/test/java/com/sigmasys/kuali/ksa/service/TransactionServiceTest.java
@@ -1,23 +1,57 @@
 package com.sigmasys.kuali.ksa.service;
 
 
+import com.sigmasys.kuali.ksa.model.Transaction;
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 
 import org.springframework.util.Assert;
 
+import java.util.List;
+
 @RunWith(SpringJUnit4ClassRunner.class)
-@ContextConfiguration(locations = { ServiceTestSuite.TEST_KSA_CONTEXT })
+@ContextConfiguration(locations = {ServiceTestSuite.TEST_KSA_CONTEXT})
 public class TransactionServiceTest extends AbstractServiceTest {
 
-	@Test
-	public void getTransaction() throws Exception {
-		// TODO - write a transaction test #1
-		Assert.notNull("transaction");
-		Assert.isTrue(1 >= 0);
-		Assert.isTrue("Transaction".equalsIgnoreCase("transaction"));
-	}
+
+    @Autowired
+    private TransactionService transactionService;
+
+    @Test
+    public void getTransactions() throws Exception {
+
+        List<Transaction> transactions = transactionService.getTransactions();
+
+        Assert.notNull(transactions);
+
+        // Add more assertions when we have some test data
+    }
+
+    @Test
+    public void getTransaction() throws Exception {
+
+        Transaction transaction = transactionService.getTransaction(7777777L);
+
+        // Check that the entity does not exist
+        Assert.isNull(transaction);
+
+        // Add more assertions when we have some test data
+    }
+
+    @Test
+    public void getTransactionByUserId() throws Exception {
+
+        List<Transaction> transactions = transactionService.getTransactions("dukakis");
+
+        Assert.notNull(transactions);
+
+        Assert.isTrue(transactions.isEmpty());
+
+        // Add more assertions when we have some test data
+    }
+
 
 }
