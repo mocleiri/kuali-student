8a589a6e66e48811646ca11e6e53bc2ec991e014
tbornholtz <tbornholtz@kuali.org>
2012-04-07 21:41:47 -0400

    First cut of Account Service (from the Process Diagrams KSA-RM
    git-svn-id: https://svn.kuali.org/repos/student/contrib/ksa/trunk@29540 0ac02d80-fb3f-11dc-a574-c57244a850eb

---
+++ ksa-core/src/main/java/com/sigmasys/kuali/ksa/service/AccountService.java
@@ -0,0 +1,106 @@
+package com.sigmasys.kuali.ksa.service;
+
+import com.sigmasys.kuali.ksa.model.Account;
+import com.sigmasys.kuali.ksa.model.Deferment;
+import com.sigmasys.kuali.ksa.model.Transaction;
+
+import javax.jws.WebService;
+import javax.jws.soap.SOAPBinding;
+import java.math.BigDecimal;
+import java.util.List;
+
+/**
+ * Account service declares business operations on Student Accounts and related
+ * objects
+ * <p/>
+ *
+ * @author Tim Bornholtz
+ */
+@WebService(name = "AccountService", targetNamespace = "http://ksa.kuali.org/wsdl/account")
+@SOAPBinding(style = SOAPBinding.Style.DOCUMENT, use = SOAPBinding.Use.LITERAL, parameterStyle = SOAPBinding.ParameterStyle.WRAPPED)
+public interface AccountService {
+    /**
+     *     This process creates a temporary subset of the account as if the account were being administered
+     *     as a balance forward account. This permits aging the account in a way that is not affected by the
+     *     payment application methodology. This temporary array is passed to the ageDebt() method.
+     * @param ignoreDeferment
+     */
+    void rebalance(Boolean ignoreDeferment);
+
+    /**
+     *
+     * @param ignoreDeferment
+     */
+    void ageDebt(Boolean ignoreDeferment);
+
+    /**
+     *
+     * @param ignoreDeferment
+     * @return
+     */
+    BigDecimal getDueBalance(Boolean ignoreDeferment);
+
+    /**
+     *
+     * @param ignoreDeferment
+     * @return
+     */
+    BigDecimal getOutstantingBalance(Boolean ignoreDeferment);
+
+    /**
+     *
+     * @return
+     */
+    BigDecimal getUnallocatedBalance();
+
+    /**
+     *
+     * @return
+     */
+    BigDecimal getDeferredAmount();
+
+    /**
+     * moving a transaction from a pre-effective state to an effective state.
+     *
+    */
+    void makeEffective();
+
+    /**
+     * Simple Balance Forward Only
+     *
+     * Notes:
+     * This is a simple, balance-forward payment application routine.
+     * “Allocate” requires both the transaction allocationAmount to be updated, as well as updating/instantiating
+     * the Allocation objects.
+     * Deferments, when created, are allocated to their charge using lockedAllocationAmount.
+     * A more complex, rule-based payment application system, that takes into account payment
+     * divisions, priority codes, etc. will be developed to supplement this algorithm.
+     */
+
+    void paymentApplication();
+
+    void createAllocation (Transaction transaction1, Transaction transaction2, BigDecimal allocationAmount);
+
+    void createLockedAllocation (Transaction transaction1, Transaction transaction2, BigDecimal allocationAmount);
+
+    /**
+     *     This method is used to verify that an account exists before a transaction or other operations are
+     *     performed on the account. There is an initial inquiry into the KSA store. If no account exist, then there is
+     *     an inquiry into KIM. If KIM also returns no result, then false is returned. If a KIM account does exist, then
+     *     a KSA account is created, using the KIM information as a template.
+     *
+     * @param accountIdentifer
+     * @return
+     */
+    Boolean doesAccountExist(String accountIdentifer);
+
+    /**
+     *  Check ONLY if the account exists in the KSA system. Return either true or false
+     */
+    Boolean doesKsaAccountExist(String accountIdentifier);
+
+    Account instantiateKsaAccount();
+    
+    Account instantiateKsaAccount(String accountIdentifier);
+
+}
---
+++ ksa-core/src/main/java/com/sigmasys/kuali/ksa/service/impl/AccountServiceImpl.java
@@ -0,0 +1,92 @@
+package com.sigmasys.kuali.ksa.service.impl;
+
+import com.sigmasys.kuali.ksa.model.Account;
+import com.sigmasys.kuali.ksa.model.Transaction;
+import com.sigmasys.kuali.ksa.service.AccountService;
+import com.sigmasys.kuali.ksa.service.CurrencyService;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.math.BigDecimal;
+
+/**
+ * Currency service JPA implementation.
+ * <p/>
+ *
+ * @author Tim Bornholtz
+ */
+@Service("accountService")
+@Transactional(readOnly = true)
+@SuppressWarnings("unchecked")
+public class AccountServiceImpl extends GenericPersistenceService implements AccountService {
+
+    @Override
+    public void rebalance(Boolean ignoreDeferment) {
+        //To change body of implemented methods use File | Settings | File Templates.
+    }
+
+    @Override
+    public void ageDebt(Boolean ignoreDeferment) {
+        //To change body of implemented methods use File | Settings | File Templates.
+    }
+
+    @Override
+    public BigDecimal getDueBalance(Boolean ignoreDeferment) {
+        return null;  //To change body of implemented methods use File | Settings | File Templates.
+    }
+
+    @Override
+    public BigDecimal getOutstantingBalance(Boolean ignoreDeferment) {
+        return null;  //To change body of implemented methods use File | Settings | File Templates.
+    }
+
+    @Override
+    public BigDecimal getUnallocatedBalance() {
+        return null;  //To change body of implemented methods use File | Settings | File Templates.
+    }
+
+    @Override
+    public BigDecimal getDeferredAmount() {
+        return null;  //To change body of implemented methods use File | Settings | File Templates.
+    }
+
+    @Override
+    public void makeEffective() {
+        //To change body of implemented methods use File | Settings | File Templates.
+    }
+
+    @Override
+    public void paymentApplication() {
+        //To change body of implemented methods use File | Settings | File Templates.
+    }
+
+    @Override
+    public void createAllocation(Transaction transaction1, Transaction transaction2, BigDecimal allocationAmount) {
+        //To change body of implemented methods use File | Settings | File Templates.
+    }
+
+    @Override
+    public void createLockedAllocation(Transaction transaction1, Transaction transaction2, BigDecimal allocationAmount) {
+        //To change body of implemented methods use File | Settings | File Templates.
+    }
+
+    @Override
+    public Boolean doesAccountExist(String accountIdentifer) {
+        return null;  //To change body of implemented methods use File | Settings | File Templates.
+    }
+
+    @Override
+    public Boolean doesKsaAccountExist(String accountIdentifier) {
+        return null;  //To change body of implemented methods use File | Settings | File Templates.
+    }
+
+    @Override
+    public Account instantiateKsaAccount() {
+        return null;  //To change body of implemented methods use File | Settings | File Templates.
+    }
+
+    @Override
+    public Account instantiateKsaAccount(String accountIdentifier) {
+        return null;  //To change body of implemented methods use File | Settings | File Templates.
+    }
+}
