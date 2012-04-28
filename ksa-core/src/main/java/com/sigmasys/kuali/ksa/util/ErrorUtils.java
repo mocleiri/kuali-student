26e6eee45306c932bdff6e5f3af7707becabfce0
mivanov <mivanov@kuali.org>
2012-04-27 22:15:02 -0400

    added ErrorUtils
    git-svn-id: https://svn.kuali.org/repos/student/contrib/ksa/trunk@30392 0ac02d80-fb3f-11dc-a574-c57244a850eb

---
+++ ksa-core/src/main/java/com/sigmasys/kuali/ksa/util/ErrorUtils.java
@@ -0,0 +1,44 @@
+package com.sigmasys.kuali.ksa.util;
+
+import org.apache.commons.logging.Log;
+import org.apache.commons.logging.LogFactory;
+import java.sql.SQLException;
+
+public class ErrorUtils {
+
+    private static final Log logger = LogFactory.getLog(ErrorUtils.class);
+
+    public static String getMessage(Throwable t) {
+        return retrieveErrorMessages(t);
+    }
+
+    private static String retrieveErrorMessages(Throwable t) {
+        int depth = 20;
+        StringBuilder buffer = new StringBuilder();
+        for (Throwable error = t; error != null && (depth--) > 0; error = error.getCause()) {
+            addMessage(buffer, error.getMessage());
+            if (error instanceof SQLException) {
+                SQLException sqle = (SQLException) error;
+                while ((sqle = sqle.getNextException()) != null) {
+                    addMessage(buffer, sqle.getMessage());
+                }
+            }
+        }
+        String message = buffer.toString();
+        logger.error("ErrorInterceptor: " + message, t);
+        return (t != null && message.trim().isEmpty()) ? t.toString() : message;
+    }
+
+    private static void addMessage(StringBuilder buffer, String message) {
+        if (message != null && !message.isEmpty()) {
+            if (buffer.toString().contains(message)) {
+                buffer.append(message);
+                if (!message.trim().endsWith(".")) {
+                    buffer.append(".");
+                }
+                buffer.append(" ");
+            }
+        }
+    }
+
+}
