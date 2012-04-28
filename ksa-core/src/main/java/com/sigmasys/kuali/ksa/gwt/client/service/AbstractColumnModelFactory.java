65a18f8325d222924c3cda59f365c8d89d1b84c6
mivanov <mivanov@kuali.org>
2012-04-28 11:26:32 -0400

    Added Account column meta-data
    git-svn-id: https://svn.kuali.org/repos/student/contrib/ksa/trunk@30401 0ac02d80-fb3f-11dc-a574-c57244a850eb

--- ksa-core/src/main/java/com/sigmasys/kuali/ksa/gwt/client/service/AbstractColumnModelFactory.java
+++ ksa-core/src/main/java/com/sigmasys/kuali/ksa/gwt/client/service/AbstractColumnModelFactory.java
@@ -14,28 +14,6 @@ import java.util.List;
  * @author Michael Ivanov
  */
 public abstract class AbstractColumnModelFactory<M extends BaseModel> implements ColumnModelFactory<M> {
-    
-    public static final String LABEL_GENERATION_DATE = "Generation Date";
-    public static final String LABEL_GENERATION_DATE_SHORT = "Gen. Date";
-    public static final String LABEL_ID = "ID";
-    public static final String LABEL_START_DATE = "Start Date";
-    public static final String LABEL_END_DATE = "End Date";
-    public static final String LABEL_FIRM_ID = "Firm ID";
-    public static final String LABEL_TYPE = "Type";
-    public static final String LABEL_SUBTYPE = "Subtype";
-    public static final String LABEL_ISSUE = "Issue Sym";
-    public static final String LABEL_LABEL = "Label";
-    public static final String LABEL_MARKET_CLASS = "Mkt Class";
-    public static final String LABEL_MARKET_CENTER = "Mkt Center";
-    public static final String LABEL_START_TIME = "Start Time";
-    public static final String LABEL_END_TIME = "End Time";
-    public static final String LABEL_DISPOSITION = "Disposition";
-    public static final String LABEL_COMMENT = "Comment";
-    public static final String LABEL_PATTERN_NAME = "Pattern Name";
-    public static final String LABEL_VERSION = "Version";
-    public static final String LABEL_SURV_PERIOD = "Surv. Period";
-    public static final String LABEL_DURATION = "Duration";
-    public static final String LABEL_ASSIGNEE = "Assignee";
 
     // To make it list panel aware - subclasses may want to override it if necessary
     public void setListPanel(ListPanel<M> listPanel) {
