60bfe7779790aa67d8b1efc2e5bc3efb0b9cc284
dmulderink <dmulderink@kuali.org>
2012-05-15 18:24:59 -0400

    refactor transaction details panel
    git-svn-id: https://svn.kuali.org/repos/student/contrib/ksa/trunk@31154 0ac02d80-fb3f-11dc-a574-c57244a850eb

--- ksa-core/src/main/java/com/sigmasys/kuali/ksa/gwt/client/view/TransactionDetailsPanel.java
+++ ksa-core/src/main/java/com/sigmasys/kuali/ksa/gwt/client/view/TransactionDetailsPanel.java
@@ -7,10 +7,7 @@ import com.extjs.gxt.ui.client.event.Events;
 import com.extjs.gxt.ui.client.event.Listener;
 import com.extjs.gxt.ui.client.store.ListStore;
 import com.extjs.gxt.ui.client.util.Margins;
-import com.extjs.gxt.ui.client.widget.Dialog;
-import com.extjs.gxt.ui.client.widget.Info;
-import com.extjs.gxt.ui.client.widget.LayoutContainer;
-import com.extjs.gxt.ui.client.widget.VerticalPanel;
+import com.extjs.gxt.ui.client.widget.*;
 import com.extjs.gxt.ui.client.widget.button.Button;
 import com.extjs.gxt.ui.client.widget.form.CheckBox;
 import com.extjs.gxt.ui.client.widget.grid.Grid;
@@ -32,9 +29,30 @@ import com.sigmasys.kuali.ksa.gwt.client.view.widget.WidgetFactory;
  */
 public class TransactionDetailsPanel extends AbstractDetailsPanel<TransactionModel> {
 
-   protected final LayoutContainer statusPanel;
+   // Common fields
+
+   protected final Text accountId = WidgetFactory.createStyledText("", "text-link");
+
+   // Charge - aka Debit type
+   protected final Text isDefered = WidgetFactory.createStyledText("", "text-link");
+
+   // Payment - aka Credit type
+   protected final Text isRefundable = WidgetFactory.createStyledText("", "text-link");
+   protected final Text refundRule = WidgetFactory.createStyledText("", "text-link");
+   protected final Text clearDate = WidgetFactory.createStyledText("", "text-link");
+
+   // Deferment - aka a deferred debit
+   protected final Text expireDate = WidgetFactory.createStyledText("", "text-link");
+
+   protected final LayoutContainer commonPanel;
+   protected final LayoutContainer debitPanel;
+   protected final LayoutContainer creditPanel;
+   protected final LayoutContainer specialPanel;
+   //protected final LayoutContainer statusPanel;
 
    protected final Button addChargeButton;
+   protected final Button makePaymentButton;
+   protected final Button ageDebtButton;
 
    public TransactionDetailsPanel() {
 
@@ -59,25 +77,95 @@ public class TransactionDetailsPanel extends AbstractDetailsPanel<TransactionMod
          }
       });
 
+      makePaymentButton = new Button("Make Payment");
+      makePaymentButton.addListener(Events.OnClick, new Listener<BaseEvent>() {
+         public void handleEvent(BaseEvent be) {
+            // TODO
+            createYesNoDialog("Make Payment", "Are you sure you want to make a payment?", null).show();
+         }
+      });
 
+      ageDebtButton = new Button("Age Transactions");
+      ageDebtButton.addListener(Events.OnClick, new Listener<BaseEvent>() {
+         public void handleEvent(BaseEvent be) {
+            Command command = new Command() {
+               @Override
+               public void execute() {
+                  ageDebt();
+               }
+            };
+            createYesNoDialog("Age Transactions", "Are you sure you want to age transactions?", command).show();
+         }
+      });
+
+
+      // -----
       TableLayout layout = new TableLayout(1);
       layout.setCellPadding(CELL_PADDING);
       layout.setCellSpacing(CELL_SPACING);
 
-      statusPanel = new LayoutContainer(layout);
-      statusPanel.add(addChargeButton);
+      commonPanel = new LayoutContainer(layout);
+      commonPanel.add(WidgetFactory.createText("Account ID"));
+      commonPanel.add(accountId);
+
+      // -----
+      layout = new TableLayout(2);
+      layout.setCellPadding(CELL_PADDING);
+      layout.setCellSpacing(CELL_SPACING);
+
+      debitPanel = new LayoutContainer(layout);
+      debitPanel.add(WidgetFactory.createText("Deferred"));
+      debitPanel.add(addChargeButton);
 
       TableData td = new TableData();
       td.setColspan(1);
+      debitPanel.add(isDefered);
+
+      // -----
+      layout = new TableLayout(4);
+      layout.setCellPadding(CELL_PADDING);
+      layout.setCellSpacing(CELL_SPACING);
+
+      creditPanel = new LayoutContainer(layout);
+      creditPanel.add(WidgetFactory.createText("Refundable"));
+      creditPanel.add(WidgetFactory.createText("Refund Rule"));
+      creditPanel.add(WidgetFactory.createText("Clear Date"));
+      creditPanel.add(makePaymentButton);
+
+      creditPanel.add(isRefundable);
+      creditPanel.add(refundRule);
+
+      td = new TableData();
+      td.setColspan(2);
+      creditPanel.add(clearDate);
+
+      // -----
+      // deferment and special information
+      layout = new TableLayout(1);
+      layout.setCellPadding(CELL_PADDING);
+      layout.setCellSpacing(CELL_SPACING);
+
+      specialPanel = new LayoutContainer(layout);
+      specialPanel.add(WidgetFactory.createText("Expiration Date"));
+
+      td = new TableData();
+      td.setColspan(1);
+      specialPanel.add(expireDate);
 
       panel.add(WidgetFactory.createTextBold("&nbsp;Extended Transaction Information"));
-      panel.add(statusPanel);
-
+      panel.add(commonPanel);
+      panel.add(WidgetFactory.createTextBold("&nbsp;Charges"));
+      panel.add(debitPanel);
+      panel.add(WidgetFactory.createTextBold("&nbsp;Payments"));
+      panel.add(creditPanel);
+      panel.add(WidgetFactory.createTextBold("&nbsp;Deferments"));
+      panel.add(specialPanel);
       add(panel);
 
    }
 
-/*   private void ageDebt() {
+   private void ageDebt() {
+/*
       final AccountModel model = ageDebtButton.getData("model");
       if (model != null && model.getId() != null) {
          try {
@@ -119,8 +207,9 @@ public class TransactionDetailsPanel extends AbstractDetailsPanel<TransactionMod
             AccountDetailsPanel.this.unmask();
          }
       }
-   }
 */
+   }
+
    @Override
    public void updateView(TransactionModel model) {
 
