ffdac6d7dfa4e93047ee1bed5cfa29759eccb7b8
mivanov <mivanov@kuali.org>
2012-06-11 18:22:29 -0400

    XLIFF initial commit
    git-svn-id: https://svn.kuali.org/repos/student/contrib/ksa/trunk@32240 0ac02d80-fb3f-11dc-a574-c57244a850eb

---
+++ ksa-core/src/main/java/com/sigmasys/kuali/ksa/service/xliff/TransUnit.java
@@ -0,0 +1,74 @@
+package com.sigmasys.kuali.ksa.service.xliff;
+
+import java.io.Serializable;
+
+/**
+ * This class encapsulates an &lt;trans-unit&gt; tag content.
+ *
+ * @author Michael Ivanov
+ */
+public class TransUnit implements Serializable {
+
+    private String id;
+    private String source;
+    private String target;
+
+    /**
+     * Setter for id property.
+     *
+     * @param id ID according to XLIFF specs
+     */
+    public void setId(String id) {
+        this.id = id;
+    }
+
+
+    /**
+     * Getter for id property.
+     *
+     * @return ID
+     */
+    public String getId() {
+        return id;
+    }
+
+
+    /**
+     * Setter for source property.
+     *
+     * @param param source according to XLIFF specs
+     */
+    public void setSource(String param) {
+        this.source = param;
+    }
+
+
+    /**
+     * Getter for source property.
+     *
+     * @return source content
+     */
+    public String getSource() {
+        return source;
+    }
+
+
+    /**
+     * Setter for target property.
+     *
+     * @param param target
+     */
+    public void setTarget(String param) {
+        this.target = param;
+    }
+
+
+    /**
+     * Getter for target property.
+     *
+     * @return target according to XLIFF specs
+     */
+    public String getTarget() {
+        return target;
+    }
+}
---
+++ ksa-core/src/main/java/com/sigmasys/kuali/ksa/service/xliff/XliffParser.java
@@ -0,0 +1,128 @@
+package com.sigmasys.kuali.ksa.service.xliff;
+
+import java.io.StringReader;
+
+import java.util.HashMap;
+import java.util.Map;
+
+import javax.xml.stream.XMLInputFactory;
+import javax.xml.stream.XMLStreamException;
+import javax.xml.stream.XMLStreamReader;
+import javax.xml.stream.events.XMLEvent;
+
+import org.apache.commons.logging.Log;
+import org.apache.commons.logging.LogFactory;
+
+import org.codehaus.stax2.XMLInputFactory2;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+
+/**
+ * Implementation for the &lt;xliff/&gt; tag for the XLIFF 1.2 OASIS specification.
+ * An xliff tag contains the whole translation for one language.
+ *
+ * @author Michael Ivanov
+ */
+@Service("xliffParser")
+@Transactional(readOnly = true)
+public class XliffParser {
+
+    private static final Log logger = LogFactory.getLog(XliffParser.class);
+
+    private final XMLInputFactory2 xmlInputFactory;
+
+    private XliffParser() {
+        xmlInputFactory = (XMLInputFactory2) XMLInputFactory.newInstance();
+        xmlInputFactory.setProperty(XMLInputFactory.IS_REPLACING_ENTITY_REFERENCES, Boolean.FALSE);
+        xmlInputFactory.setProperty(XMLInputFactory.IS_SUPPORTING_EXTERNAL_ENTITIES, Boolean.FALSE);
+        xmlInputFactory.setProperty(XMLInputFactory.IS_COALESCING, Boolean.FALSE);
+        xmlInputFactory.configureForSpeed();
+    }
+
+
+    /**
+     * Parses the xliff file.
+     */
+    public Map<String, TransUnit> parseXliff(String content) {
+
+        try {
+            Map<String, TransUnit> transUnits = new HashMap<String, TransUnit>();
+            XMLStreamReader streamReader = xmlInputFactory.createXMLStreamReader(new StringReader(content));
+            while (streamReader.next() != XMLEvent.END_DOCUMENT) {
+                int eventType = streamReader.getEventType();
+                switch (eventType) {
+                    case XMLEvent.START_ELEMENT:
+                        String currentElement = streamReader.getName().toString();
+                        if ("trans-unit".equals(currentElement)) {
+                            TransUnit transUnit = getTransUnit(streamReader, eventType);
+                            if (transUnit.getId() != null) {
+                                transUnits.put(transUnit.getId(), transUnit);
+                            }
+                        }
+                }
+            }
+            return transUnits;
+        } catch (Exception ex) {
+            String errMsg = "Error while loading dictionary from XLIFF file: " + ex.getMessage();
+            logger.error(errMsg, ex);
+            throw new RuntimeException(errMsg, ex);
+        }
+    }
+
+
+    /**
+     * Retrieve one &lt;trans-unit/&gt; contents.
+     *
+     * @param streamReader XML Stream reader
+     * @param eventType    Type of event where parsing stopped in previous step
+     * @return Trans unit read
+     */
+    private TransUnit getTransUnit(XMLStreamReader streamReader, int eventType) {
+
+        String transUnitId = null;
+        for (int i = 0; i < streamReader.getAttributeCount(); i++) {
+            String attrName = streamReader.getAttributeName(i).toString();
+            String attrVal = streamReader.getAttributeValue(i);
+            if ("id".equals(attrName)) {
+                transUnitId = attrVal;
+            }
+        }
+
+        try {
+
+            TransUnit transUnit = new TransUnit();
+            transUnit.setId(transUnitId);
+
+            // Read until </trans-unit>
+            String currentElement = "";
+            while (!("trans-unit".equals(currentElement) && eventType == XMLEvent.END_ELEMENT)) {
+                streamReader.next();
+                eventType = streamReader.getEventType();
+                switch (eventType) {
+                    case XMLEvent.START_ELEMENT:
+                        currentElement = streamReader.getName().toString();
+                        break;
+                    case XMLEvent.END_ELEMENT:
+                        currentElement = streamReader.getName().toString();
+                        break;
+                    case XMLEvent.CHARACTERS:
+                        String text = streamReader.getText();
+                        if ("source".equals(currentElement)) {
+                            transUnit.setSource(text);
+                        }
+                        if ("target".equals(currentElement)) {
+                            transUnit.setTarget(text);
+                        }
+                }
+            }
+
+            return transUnit;
+
+        } catch (XMLStreamException ex) {
+            logger.error(ex.getMessage(), ex);
+            throw new RuntimeException(ex.getMessage(), ex);
+        }
+    }
+
+}
