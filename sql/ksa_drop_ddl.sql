700fef8dbc0017005beb1cbbc22012a324c6bc1c
mivanov <mivanov@kuali.org>
2012-06-14 18:57:02 -0400

    Added 2 more tables to drop
    git-svn-id: https://svn.kuali.org/repos/student/contrib/ksa/trunk@32449 0ac02d80-fb3f-11dc-a574-c57244a850eb

--- sql/ksa_drop_ddl.sql
+++ sql/ksa_drop_ddl.sql
@@ -2,6 +2,7 @@
 -- This Oracle script drops database objects in KSA schema.
 -- It needs to be run by the schema owner
 
+drop table KSA.KSSA_CONFIG cascade constraints;
 drop table KSA.KSSA_ACNT cascade constraints;
 drop table KSA.KSSA_ACNT_PROTECTED_INFO cascade constraints;
 drop table KSA.KSSA_ACNT_STATUS_TYPE cascade constraints;
@@ -26,4 +27,5 @@ drop table KSA.KSSA_TAX_TYPE cascade constraints;
 drop table KSA.KSSA_TRANSACTION cascade constraints;
 drop table KSA.KSSA_TRANSACTION_TYPE cascade constraints;
 drop table KSA.KSSA_TRANSACTION_TYPE_TAG cascade constraints;
+drop table KSA.KSSA_LANGUAGE cascade constraints;
 drop table KSA.KSSA_SEQUENCE_TABLE cascade constraints;
