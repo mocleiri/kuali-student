668a894236815e0856d63f53eda2720be7b0bd00
mivanov <mivanov@kuali.org>
2012-06-30 23:24:39 -0400

    Fixed compilation error
    git-svn-id: https://svn.kuali.org/repos/student/contrib/ksa/trunk@33062 0ac02d80-fb3f-11dc-a574-c57244a850eb

--- ksa-core/src/main/java/com/sigmasys/kuali/ksa/transform/XmlToObj.java
+++ ksa-core/src/main/java/com/sigmasys/kuali/ksa/transform/XmlToObj.java
@@ -3,7 +3,7 @@ package com.sigmasys.kuali.ksa.transform;
 import com.sigmasys.kuali.ksa.model.Transaction;
 import com.sigmasys.kuali.ksa.service.AccountService;
 import com.sigmasys.kuali.ksa.service.TransactionService;
-import com.sigmasys.kuali.ksa.util.XMLGregorianCalendarConversionUtil;
+//import com.sigmasys.kuali.ksa.util.XMLGregorianCalendarConversionUtil;
 import org.springframework.beans.factory.annotation.Autowired;
 
 import javax.xml.bind.JAXBContext;
@@ -98,7 +98,7 @@ public class XmlToObj {
                         // add the accepted or failed transactions to the appropriate response list
                         // add one for each accepted or failed transactions to the appropriate number scalar
 
-                        Date effectiveDate = XMLGregorianCalendarConversionUtil.asDate(trans.getEffectiveDate());
+                        Date effectiveDate =  null; //XMLGregorianCalendarConversionUtil.asDate(trans.getEffectiveDate());
                         persistTransaction(trans.getTransactionType().toString(),
                               trans.getAccount().toString(), effectiveDate, trans.getAmount());
 
@@ -161,7 +161,7 @@ public class XmlToObj {
                   meetsRequiredInputs = verifyRequiredValues(trans);
                   if (meetsRequiredInputs) {
 
-                     Date effectiveDate = XMLGregorianCalendarConversionUtil.asDate(trans.getEffectiveDate());
+                     Date effectiveDate = null; //XMLGregorianCalendarConversionUtil.asDate(trans.getEffectiveDate());
                      persistTransaction(trans.getTransactionType().toString(),
                            trans.getAccount().toString(), effectiveDate, trans.getAmount());
 
@@ -256,7 +256,7 @@ public class XmlToObj {
 
             if (transaction != null) {
                retIdValue = transaction.getId();
-               xmlGCLedgerDate = XMLGregorianCalendarConversionUtil.asXMLGregorianCalendar(transaction.getLedgerDate());
+               xmlGCLedgerDate = null; //XMLGregorianCalendarConversionUtil.asXMLGregorianCalendar(transaction.getLedgerDate());
             }
          }
       }
