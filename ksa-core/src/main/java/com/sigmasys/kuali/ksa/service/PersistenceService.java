d0c8ec0bfdf77892770985063756b922edfbfa91
mivanov <mivanov@kuali.org>
2012-07-19 23:52:58 -0400

    Worked on transaction import validation, implemented isRefundRuleValid() and isTransactionTypeAllowed() methods, fixed issues with AccessControl Spring's injection, added PersistenceInterface, exposed TransactionImportService as a web service on KSB
    git-svn-id: https://svn.kuali.org/repos/student/contrib/ksa/trunk@33812 0ac02d80-fb3f-11dc-a574-c57244a850eb

---
+++ ksa-core/src/main/java/com/sigmasys/kuali/ksa/service/PersistenceService.java
@@ -0,0 +1,76 @@
+package com.sigmasys.kuali.ksa.service;
+
+import com.sigmasys.kuali.ksa.model.*;
+import com.sigmasys.kuali.ksa.model.search.SearchCriteria;
+import com.sigmasys.kuali.ksa.service.aop.AopProxy;
+
+import java.io.Serializable;
+import java.util.List;
+
+/**
+ * Persistence Service interface
+ * <p/>
+ * <p/>
+ *
+ * @author Michael Ivanov
+ */
+public interface PersistenceService extends AopProxy {
+
+
+    /**
+     * Returns Identifiable entity by ID
+     *
+     * @param id Entity ID
+     * @return Identifiable instance
+     */
+    <T extends Identifiable> T getEntity(Serializable id, Class<T> entityClass);
+
+
+    /**
+     * Removes Identifiable entity by ID
+     *
+     * @param id Entity ID
+     * @return Identifiable instance
+     */
+    <T extends Identifiable> boolean deleteEntity(Serializable id, Class<T> entityClass);
+
+    /**
+     * Returns the list of all Identifiable entities for the given class with the default search criteria.
+     *
+     * @param entityClass Entity Class
+     * @return List of Identifiable objects
+     */
+    <T extends Identifiable> List<T> getEntities(Class<T> entityClass);
+
+
+    /**
+     * Returns the list of all Identifiable entities for the given class with the default search criteria.
+     *
+     * @param entityClass Entity Class
+     * @param orderBy     array of fields used in "order by" clause, can be null
+     * @return List of Identifiable objects
+     */
+    <T extends Identifiable> List<T> getEntities(Class<T> entityClass, Pair<String, SortOrder>... orderBy);
+
+    /**
+     * Returns the list of all Identifiable entities for the given class.
+     *
+     * @param entityClass    Entity Class
+     * @param searchCriteria Search Criteria, can be null
+     * @param orderBy        optional array of fields used in "order by" clause, can be null
+     * @return List of Identifiable objects
+     */
+    <T extends Identifiable> List<T> getEntities(Class<T> entityClass, SearchCriteria searchCriteria,
+                                                 Pair<String, SortOrder>... orderBy);
+
+
+    /**
+     * Persists the Identifiable entity in the database.
+     * Creates a new entity when ID is null and updates the existing one otherwise.
+     *
+     * @param entity Identifiable instance
+     * @return Entity ID
+     */
+    <T extends Serializable> T persistEntity(Identifiable entity);
+
+}
