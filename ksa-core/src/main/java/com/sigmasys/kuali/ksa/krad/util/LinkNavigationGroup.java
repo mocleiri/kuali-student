bcbae770c91cbdf7dba08020cb3c98dce419acf1
mivanov <mivanov@kuali.org>
2012-09-18 11:25:16 -0400

    Created a custom NavigationGroup for navigation in KRAD allowing text links in the navigation menu
    git-svn-id: https://svn.kuali.org/repos/student/contrib/ksa/trunk@36325 0ac02d80-fb3f-11dc-a574-c57244a850eb

---
+++ ksa-core/src/main/java/com/sigmasys/kuali/ksa/krad/util/LinkNavigationGroup.java
@@ -0,0 +1,27 @@
+package com.sigmasys.kuali.ksa.krad.util;
+
+import org.kuali.rice.krad.uif.component.Component;
+import org.kuali.rice.krad.uif.container.NavigationGroup;
+import org.kuali.rice.krad.uif.element.Link;
+
+import java.util.HashSet;
+import java.util.Set;
+
+/**
+ * The custom navigation group. It allows to use text links in a navigation widget with KRAD.
+ *
+ * @author Michael Ivanov
+ */
+public class LinkNavigationGroup extends NavigationGroup {
+
+    @Override
+    public Set<Class<? extends Component>> getSupportedComponents() {
+        Set<Class<? extends Component>> components = super.getSupportedComponents();
+        if ( components == null ) {
+            components = new HashSet<Class<? extends Component>>();
+        }
+        components.add(Link.class);
+        return components;
+    }
+
+}
