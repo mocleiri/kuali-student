f3ebe319729710f17efa7a05171da5c006fcf1ae
tbornholtz <tbornholtz@kuali.org>
2012-09-26 18:14:39 -0400

    Initial implementation of the get method
    git-svn-id: https://svn.kuali.org/repos/student/contrib/ksa/trunk@36876 0ac02d80-fb3f-11dc-a574-c57244a850eb

--- ksa-core/src/main/java/com/sigmasys/kuali/ksa/krad/controller/KsaStudentAccountsController.java
+++ ksa-core/src/main/java/com/sigmasys/kuali/ksa/krad/controller/KsaStudentAccountsController.java
@@ -3,6 +3,8 @@ package com.sigmasys.kuali.ksa.krad.controller;
 import com.sigmasys.kuali.ksa.krad.form.KsaStudentAccountsForm;
 import com.sigmasys.kuali.ksa.krad.util.PersonPostal;
 import com.sigmasys.kuali.ksa.model.*;
+import org.apache.commons.logging.Log;
+import org.apache.commons.logging.LogFactory;
 import org.springframework.stereotype.Controller;
 import org.springframework.transaction.annotation.Transactional;
 import org.springframework.validation.BindingResult;
@@ -13,6 +15,7 @@ import org.springframework.web.servlet.ModelAndView;
 
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import java.math.BigDecimal;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -23,6 +26,7 @@ import java.util.List;
 @RequestMapping(value = "/ksaStudentAccountsVw")
 public class KsaStudentAccountsController extends GenericSearchController {
 
+    private static final Log logger = LogFactory.getLog(KsaStudentAccountsController.class);
 
     /**
      * @see org.kuali.rice.krad.web.controller.UifControllerBase#createInitialForm(javax.servlet.http.HttpServletRequest)
@@ -45,29 +49,48 @@ public class KsaStudentAccountsController extends GenericSearchController {
 
         // do get stuff...
         String pageId = request.getParameter("pageId");
-/*
 
-        if (pageId != null && pageId.compareTo("") == 0) {
-            String id = request.getParameter("id");
-            if (id == null || id.isEmpty()) {
-                throw new IllegalArgumentException("'id' request parameter must be specified");
-            }
-
-            String compositePersonName = createCompositeDefaultPersonName(id);
-            form.setSelectedAccountCompositePersonName(compositePersonName);
-
-              // charges by ID
-            List<Charge> charges = transactionService.getCharges(id);
-
-            // payments by ID
-            List<Payment> payments = transactionService.getPayments(id);
-
-            form.setChargeList(charges);
-
-            form.setPaymentList(payments);*//*
-
+        String id = request.getParameter("id");
+        if (id == null || id.isEmpty()) {
+            throw new IllegalArgumentException("'id' request parameter must be specified");
         }
-*/
+
+        //String compositePersonName = createCompositeDefaultPersonName(id);
+        //form.setSelectedAccountCompositePersonName(compositePersonName);
+
+        // All transactions
+        List<Transaction> transactions = transactionService.getTransactions(id);
+
+        logger.info("TJB Transaction Count: " + transactions.size());
+        List<Transaction> rollups = new ArrayList<Transaction>();
+        List<Transaction> ungrouped = new ArrayList<Transaction>();
+
+        form.setRollUpCredit(new BigDecimal(0));
+        form.setRollUpDebit(new BigDecimal(0));
+        form.setUnGroupedCredit(new BigDecimal(0));
+        form.setUnGroupedDebit(new BigDecimal(0));
+
+        for(Transaction t : transactions){
+            if(t.getRollup() != null){
+                rollups.add(t);
+                if(TransactionTypeValue.CHARGE.equals(t.getTransactionTypeValue())){
+                    form.setRollUpCredit(form.getRollUpCredit().add(t.getAmount()));
+                } else {
+                    form.setRollUpDebit(form.getRollUpDebit().add(t.getAmount()));
+                }
+            } else {
+                ungrouped.add(t);
+                if(TransactionTypeValue.CHARGE.equals(t.getTransactionTypeValue())){
+                    form.setUnGroupedCredit(form.getUnGroupedCredit().add(t.getAmount()));
+                } else {
+                    form.setUnGroupedDebit(form.getUnGroupedDebit().add(t.getAmount()));
+                }
+            }
+        }
+
+        form.setRollUpList(rollups);
+        form.setUnGroupedList(ungrouped);
+
         return getUIFModelAndView(form);
     }
 
@@ -148,6 +171,58 @@ public class KsaStudentAccountsController extends GenericSearchController {
     public ModelAndView refresh(@ModelAttribute("KualiForm") KsaStudentAccountsForm form, BindingResult result,
                                 HttpServletRequest request, HttpServletResponse response) {
         // do refresh stuff...
+        return getUIFModelAndView(form);
+    }
+
+    /**
+     * User searches on a (last) name. The result set is iterated over to create the composite PersonName
+     * and composite address using default records, eventfully creating a browse list that can be displayed
+     * and selected from for further processing as desired.
+     *
+     * @param form
+     * @param result
+     * @param request
+     * @param response
+     * @return
+     */
+    @RequestMapping(method = RequestMethod.POST, params = "methodToCall=searchByType")
+    public ModelAndView searchByName(@ModelAttribute("KualiForm") KsaStudentAccountsForm form, BindingResult result,
+                                     HttpServletRequest request, HttpServletResponse response) {
+
+        // we do not have a query by name or partial name via last name or contains yet
+        // if no result set from getting full accounts than the List is empty
+        // otherwise the lit contains records and a compsite person name and postal address
+
+        String personSearchByAccount = form.getBioSearchByAccount();
+
+        // query for all accounts
+
+        List<Account> accountSearchList = accountService.getFullAccounts();
+
+        // create a a list of Account objects for display requirements
+
+        List<Account> accountList = new ArrayList<Account>();
+
+        // if we have a result set of Accounts from the query
+
+        for (Account account : accountSearchList) {
+
+            PersonName personName = account.getDefaultPersonName();
+
+            if (personName != null && personName.getLastName() != null &&
+                    personName.getLastName().contains(personSearchByAccount)) {
+
+                // add each account copy to a list
+
+                accountList.add(account.getCopy());
+            }
+        }
+
+        // set the account list derived from the full search list
+
+        form.setAccountBrowseList(accountList);
+
+        // do a search by name returning account info
         return getUIFModelAndView(form);
     }
 
