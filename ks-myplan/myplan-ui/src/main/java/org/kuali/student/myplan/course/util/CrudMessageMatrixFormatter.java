98f2f576e6eae34ace356ea86cde6e0162029916
kmuthusw <kmuthusw@kuali.org>
2012-09-28 16:49:54 -0400

    MyPlan-958
    git-svn-id: https://svn.kuali.org/repos/student/contrib/myplan/branches/ks-myplan-1.0.1@37196 0ac02d80-fb3f-11dc-a574-c57244a850eb

--- ks-myplan/myplan-ui/src/main/java/org/kuali/student/myplan/course/util/CrudMessageMatrixFormatter.java
+++ ks-myplan/myplan-ui/src/main/java/org/kuali/student/myplan/course/util/CrudMessageMatrixFormatter.java
@@ -240,27 +240,12 @@ public class CrudMessageMatrixFormatter extends PropertyEditorSupport {
 
             }
         }
-        /*When savedItemId and savedItemDateCreated are not null then populating message
-            *"Saved to Your Bookmark List on 8/15/2012" or
-            *"Had saved to Your Bookmark List on 8/15/2012"*/
+         /*When savedItemId and savedItemDateCreated are not null then populating message
+            *"Bookmarked on 8/15/2012"*/
         if (courseDetails.getSavedItemId() != null && courseDetails.getSavedItemDateCreated() != null) {
-            /*When planned List or backup list are equal to null then show message "Saved to Your Bookmark List on 8/15/2012"*/
-            if (courseDetails.getPlannedList() == null && courseDetails.getBackupList() == null) {
-                String message = "";
-                if (UserSessionHelper.isAdviser()) {
-                    String user = UserSessionHelper.getStudentName();
-                    message = "Saved to " + user + "'s. ";
-                }
-                sb = sb.append("<dd>").append(message)
-                        .append("<a href=lookup?methodToCall=search&viewId=SavedCoursesDetail-LookupView>").append("Bookmarked ").append("</a>").append(" on ").append(courseDetails.getSavedItemDateCreated());
-            }
-            /*When planned List or backup list are not null then show message "Had saved to Your Courses List on 8/15/2012"*/
-            if (courseDetails.getPlannedList() != null || courseDetails.getBackupList() != null) {
-                sb = sb.append("<dd>")
-                        .append("<a href=lookup?methodToCall=search&viewId=SavedCoursesDetail-LookupView>").append("Bookmarked ").append("</a>").append(" on ").append(courseDetails.getSavedItemDateCreated());
-            }
-        }
+            sb = sb.append("<dd>").append("<a href=lookup?methodToCall=search&viewId=SavedCoursesDetail-LookupView>").append("Bookmarked ").append("</a>").append(" on ").append(courseDetails.getSavedItemDateCreated());
 
+        }
 
         return sb.toString();
     }
