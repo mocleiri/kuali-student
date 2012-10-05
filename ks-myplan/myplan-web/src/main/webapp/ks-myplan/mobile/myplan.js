e8763b93c1f64c75d803e70b552fd308b579a31d
garettg <garettg@kuali.org>
2012-10-05 19:10:11 -0400

    Updates to Mobile myplan
    git-svn-id: https://svn.kuali.org/repos/student/contrib/myplan/branches/mobile@38077 0ac02d80-fb3f-11dc-a574-c57244a850eb

---
+++ ks-myplan/myplan-web/src/main/webapp/ks-myplan/mobile/myplan.js
@@ -0,0 +1,4 @@
+jQuery(document).ready(function () {
+    jQuery("head").append('<meta name="viewport" content="width=device-width, initial-scale=1">');
+    //jQuery("#Uif-Application").attr("data-role","page");
+});
