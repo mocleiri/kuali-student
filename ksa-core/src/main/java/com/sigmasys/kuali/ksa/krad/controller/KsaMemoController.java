596ae651c2759c744de3340f2a04b9d2dd00a156
dmulderink <dmulderink@kuali.org>
2012-10-05 15:55:22 -0400

    Memo, QuickVw and StudentAccounts view changes / additions.
    git-svn-id: https://svn.kuali.org/repos/student/contrib/ksa/trunk@38037 0ac02d80-fb3f-11dc-a574-c57244a850eb

--- ksa-core/src/main/java/com/sigmasys/kuali/ksa/krad/controller/KsaMemoController.java
+++ ksa-core/src/main/java/com/sigmasys/kuali/ksa/krad/controller/KsaMemoController.java
@@ -2,9 +2,15 @@ package com.sigmasys.kuali.ksa.krad.controller;
 
 import com.sigmasys.kuali.ksa.krad.form.KsaMemoForm;
 import com.sigmasys.kuali.ksa.krad.form.KsaStudentAccountsForm;
+import com.sigmasys.kuali.ksa.krad.util.AlertsFlagsMemos;
 import com.sigmasys.kuali.ksa.model.Account;
+import com.sigmasys.kuali.ksa.model.Information;
+import com.sigmasys.kuali.ksa.model.InformationTypeValue;
+import com.sigmasys.kuali.ksa.model.Memo;
+import com.sigmasys.kuali.ksa.service.InformationService;
 import org.apache.commons.logging.Log;
 import org.apache.commons.logging.LogFactory;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.transaction.annotation.Transactional;
 import org.springframework.validation.BindingResult;
@@ -15,6 +21,8 @@ import org.springframework.web.servlet.ModelAndView;
 
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import java.util.Date;
+import java.util.List;
 
 /**
  * Created by: dmulderink on 10/4/12 at 7:53 AM
@@ -24,6 +32,9 @@ import javax.servlet.http.HttpServletResponse;
 public class KsaMemoController extends GenericSearchController {
 
    private static final Log logger = LogFactory.getLog(KsaStudentAccountsController.class);
+
+   @Autowired
+   private InformationService informationService;
 
    /**
     * @see org.kuali.rice.krad.web.controller.UifControllerBase#createInitialForm(javax.servlet.http.HttpServletRequest)
@@ -64,8 +75,30 @@ public class KsaMemoController extends GenericSearchController {
    @RequestMapping(method = RequestMethod.GET, params = "methodToCall=get")
    public ModelAndView get(@ModelAttribute("KualiForm") KsaMemoForm form, BindingResult result,
                            HttpServletRequest request, HttpServletResponse response) {
-
       // do get stuff...
+
+      String pageId = request.getParameter("pageId");
+      // example user 1
+      String userId = request.getParameter("userId");
+      // a table selection record index
+      String transUserId = request.getParameter("id");
+
+      if (pageId != null && pageId.compareTo("MemosPage") == 0) {
+         if (userId == null || userId.isEmpty()) {
+            throw new IllegalArgumentException("'userId' request parameter must be specified");
+         }
+
+         List<Memo> memos = informationService.getMemos(userId);
+
+         AlertsFlagsMemos afm = new AlertsFlagsMemos();
+         // Alerts
+         for (Memo memo : memos) {
+
+            memo.setCompositeInfo(afm.CreateCompositeMemo(memo));
+         }
+
+         form.setMemos(memos);
+      }
 
       return getUIFModelAndView(form);
    }
@@ -102,7 +135,6 @@ public class KsaMemoController extends GenericSearchController {
    public ModelAndView submit(@ModelAttribute("KualiForm") KsaMemoForm form, BindingResult result,
                               HttpServletRequest request, HttpServletResponse response) {
       // do submit stuff...
-
 
       return getUIFModelAndView(form);
    }
@@ -165,6 +197,56 @@ public class KsaMemoController extends GenericSearchController {
     */
    @RequestMapping(method= RequestMethod.POST, params="methodToCall=insertMemo")
    public ModelAndView insertMemo(@ModelAttribute("KualiForm") KsaMemoForm form, BindingResult result,
+                               HttpServletRequest request, HttpServletResponse response) {
+      // do insert stuff...
+
+      // TODO validate the field entries before inserting
+
+      // the account should be satisfied by the link that got us into this page
+      Account account = form.getAccount();
+
+      String infoType = InformationTypeValue.MEMO.name();
+
+      InformationTypeValue informationType = Enum.valueOf(InformationTypeValue.class, infoType);
+
+      Memo memo = form.getMemo();
+      Information info =  memo;
+      memo.setAccount(account);
+      memo.setAccountId(account.getId());
+      memo.setCreationDate(new Date());
+      memo.setLastUpdate(new Date());
+      //info.setCreatorId();
+      //info.setCreatorId();
+      Long infoId = informationService.persistInformation(memo);
+
+      return getUIFModelAndView(form);
+   }
+
+   /**
+    *
+    * @param form
+    * @param result
+    * @param request
+    * @param response
+    * @return
+    */
+   @RequestMapping(method= RequestMethod.POST, params="methodToCall=updateMemo")
+   public ModelAndView updateMemo(@ModelAttribute("KualiForm") KsaMemoForm form, BindingResult result,
+                               HttpServletRequest request, HttpServletResponse response) {
+      // do refresh stuff...
+      return getUIFModelAndView(form);
+   }
+
+   /**
+    *
+    * @param form
+    * @param result
+    * @param request
+    * @param response
+    * @return
+    */
+   @RequestMapping(method= RequestMethod.POST, params="methodToCall=followUpMemo")
+   public ModelAndView followUpMemo(@ModelAttribute("KualiForm") KsaMemoForm form, BindingResult result,
                                HttpServletRequest request, HttpServletResponse response) {
       // do refresh stuff...
       return getUIFModelAndView(form);
