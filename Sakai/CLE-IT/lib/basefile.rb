67e254a3c5680011b75a2cf1db7c997e4f78b6fb
chuckv <chuckv@kuali.org>
2012-10-05 14:47:48 -0400

    not related to this project, being moved to its own repo git side
    git-svn-id: https://svn.kuali.org/repos/student/test/functional-automation/sambal/trunk@38020 0ac02d80-fb3f-11dc-a574-c57244a850eb

--- Sakai/CLE-IT/lib/basefile.rb
+++
@@ -1,7 +0,0 @@
-$: << File.dirname(__FILE__)+'/cle'
-
-require 'authenticate'
-require 'webservices'
-
-# Autoloader
-Dir["#{File.dirname(__FILE__)}/cle/*.rb"].each {|f| require f; puts "*** loading #{f}" }
