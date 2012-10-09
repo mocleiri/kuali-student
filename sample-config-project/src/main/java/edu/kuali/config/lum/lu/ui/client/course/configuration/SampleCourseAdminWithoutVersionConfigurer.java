861ab4d27ae1eb22fc7241be37f7a8c2f1a41fa3
nwright <nwright@kuali.org>
2012-10-09 09:50:55 -0400

    KSENROLL-3357 Upgrade Sample and Training Projects to work with 1.2.2-M4
    git-svn-id: https://svn.kuali.org/repos/student/sandbox/examples@38290 0ac02d80-fb3f-11dc-a574-c57244a850eb

--- sample-config-project/src/main/java/edu/kuali/config/lum/lu/ui/client/course/configuration/SampleCourseAdminWithoutVersionConfigurer.java
+++ sample-config-project/src/main/java/edu/kuali/config/lum/lu/ui/client/course/configuration/SampleCourseAdminWithoutVersionConfigurer.java
@@ -1,18 +1,3 @@
-/**
- * Copyright 2010 The Kuali Foundation Licensed under the
- * Educational Community License, Version 2.0 (the "License"); you may
- * not use this file except in compliance with the License. You may
- * obtain a copy of the License at
- *
- * http://www.osedu.org/licenses/ECL-2.0
- *
- * Unless required by applicable law or agreed to in writing,
- * software distributed under the License is distributed on an "AS IS"
- * BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express
- * or implied. See the License for the specific language governing
- * permissions and limitations under the License.
- */
-
 /*
  * Copyright 2009 The Kuali Foundation Licensed under the
  * Educational Community License, Version 2.0 (the "License"); you may
@@ -33,7 +18,8 @@ import org.kuali.student.common.ui.client.configurable.mvc.FieldDescriptor;
 import org.kuali.student.common.ui.client.configurable.mvc.sections.Section;
 import org.kuali.student.common.ui.client.widgets.KSCharCount;
 import org.kuali.student.lum.common.client.lu.LUUIConstants;
-import org.kuali.student.lum.lu.ui.course.client.configuration.CourseProposalConfigurer;
+import org.kuali.student.lum.lu.ui.course.client.configuration.CourseAdminWithoutVersionConfigurer;
+import org.kuali.student.lum.lu.ui.course.client.configuration.CourseProposalConfigurer.KeyListModelWigetBinding;
 
 
 /**
@@ -41,13 +27,11 @@ import org.kuali.student.lum.lu.ui.course.client.configuration.CourseProposalCon
  *
  * @author Kuali Student Team
  */
-public class SampleCourseProposalConfigurer extends CourseProposalConfigurer {
+public class SampleCourseAdminWithoutVersionConfigurer extends CourseAdminWithoutVersionConfigurer {
    
 	@Override
 	public Section generateCourseInfoSection(Section section) {
-
-        addField(section, PROPOSAL_TITLE_PATH, generateMessageInfo(LUUIConstants.PROPOSAL_TITLE_LABEL_KEY));
-        addField(section, COURSE + "/" + TRANSCRIPT_TITLE, generateMessageInfo(LUUIConstants.SHORT_TITLE_LABEL_KEY), new KSCharCount(getMetaData(COURSE + "/" + TRANSCRIPT_TITLE)));
+        addField(section, COURSE + "/" + TRANSCRIPT_TITLE, generateMessageInfo(LUUIConstants.SHORT_TITLE_LABEL_KEY));
         addField(section, COURSE + "/" + COURSE_TITLE, generateMessageInfo(LUUIConstants.COURSE_TITLE_LABEL_KEY));
         section.addSection(generateCourseNumberSection());
     	FieldDescriptor instructorsFd = addField(section, COURSE + "/" + INSTRUCTORS, generateMessageInfo(LUUIConstants.INSTRUCTORS_LABEL_KEY));
