d522ad1527586b4a9b3ee60d7ab2a829444fb9d3
dmulderink <dmulderink@kuali.org>
2012-10-12 17:45:14 -0400

    Added file upload support. Needs to be changed to account import for rules
    git-svn-id: https://svn.kuali.org/repos/student/contrib/ksa/trunk@38564 0ac02d80-fb3f-11dc-a574-c57244a850eb

--- ksa-core/src/main/java/com/sigmasys/kuali/ksa/krad/controller/KsaFeeAssessmentController.java
+++ ksa-core/src/main/java/com/sigmasys/kuali/ksa/krad/controller/KsaFeeAssessmentController.java
@@ -1,14 +1,18 @@
 package com.sigmasys.kuali.ksa.krad.controller;
 
 import com.sigmasys.kuali.ksa.krad.form.KsaFeeAssessmentForm;
+import com.sigmasys.kuali.ksa.service.TransactionImportService;
+import com.sigmasys.kuali.ksa.util.CommonUtils;
 import org.apache.commons.logging.Log;
 import org.apache.commons.logging.LogFactory;
+import org.kuali.rice.kns.exception.FileUploadLimitExceededException;
 import org.springframework.stereotype.Controller;
 import org.springframework.transaction.annotation.Transactional;
 import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.multipart.MultipartFile;
 import org.springframework.web.servlet.ModelAndView;
 
 import javax.servlet.http.HttpServletRequest;
@@ -22,6 +26,8 @@ import javax.servlet.http.HttpServletResponse;
 public class KsaFeeAssessmentController extends GenericSearchController {
 
    private static final Log logger = LogFactory.getLog(KsaFeeAssessmentController.class);
+
+   private TransactionImportService transactionImportService;
 
    /**
     * @see org.kuali.rice.krad.web.controller.UifControllerBase#createInitialForm(javax.servlet.http.HttpServletRequest)
@@ -82,6 +88,49 @@ public class KsaFeeAssessmentController extends GenericSearchController {
    public ModelAndView submit(@ModelAttribute("KualiForm") KsaFeeAssessmentForm form, BindingResult result,
                               HttpServletRequest request, HttpServletResponse response) {
       // do submit stuff...
+      // org.springframework.web.multipart.MaxUploadSizeExceededException:
+      // Maximum upload size of 500000 bytes exceeded; nested exception is
+      // org.apache.commons.fileupload.FileUploadBase$SizeLimitExceededException:
+      // the request was rejected because its size (992410) exceeds the configured maximum (500000)
+      String processMsg = "";
+      try {
+         MultipartFile uploadXMLFile = form.getUploadXMLFile();
+         String contentType = uploadXMLFile.getContentType();
+         if (contentType.endsWith("xml")) {
+
+            try {
+
+               String xmlContent = CommonUtils.getStreamAsString(uploadXMLFile.getInputStream());
+
+               processMsg = "Processing Transaction(s)";
+               form.setUploadProcessState(processMsg);
+               String processResponse = transactionImportService.processTransactions(xmlContent);
+
+               logger.info("Response: \n" + processResponse);
+               String begValue = "<batch-status>";
+               String endValue = "</batch-status>";
+               int begIndex = processResponse.indexOf(begValue) + begValue.length();
+               int endIndex = processResponse.indexOf(endValue);
+               String batchStatus = processResponse.substring(begIndex, endIndex);
+               processMsg = "Transaction(s) Processing " + batchStatus;
+
+               form.setUploadProcessState(processMsg);
+            } catch (Exception exp) {
+               String expMsg = exp.getMessage();
+               logger.error(expMsg);
+               processMsg = "Transaction(s) Processing Failed";
+               form.setUploadProcessState(processMsg);
+            }
+         } else {
+            processMsg = "Only XML files are allowed. Please try again.";
+            form.setUploadProcessState(processMsg);
+            logger.error(processMsg);
+         }
+      } catch (FileUploadLimitExceededException exp) {
+         String expMsg = exp.getMessage();
+         logger.error(expMsg);
+         form.setUploadProcessState("File size limit exceeded");
+      }
 
 
       return getUIFModelAndView(form);
