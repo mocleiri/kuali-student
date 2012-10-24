14787640f30c18d891fd260cf2070416ee934eca
mivanov <mivanov@kuali.org>
2012-10-24 18:32:58 -0400

    added 2 examples of the XML request
    git-svn-id: https://svn.kuali.org/repos/student/contrib/bsinas/trunk@39021 0ac02d80-fb3f-11dc-a574-c57244a850eb

--- src/main/java/com/sigmasys/bsinas/util/ErrorUtils.java
+++ src/main/java/com/sigmasys/bsinas/util/ErrorUtils.java
@@ -3,7 +3,6 @@ package com.sigmasys.bsinas.util;
 import org.apache.commons.logging.Log;
 import org.apache.commons.logging.LogFactory;
 
-import javax.xml.bind.JAXBException;
 import java.sql.SQLException;
 
 public class ErrorUtils {
@@ -26,11 +25,6 @@ public class ErrorUtils {
                 SQLException sqle = (SQLException) error;
                 while ((sqle = sqle.getNextException()) != null) {
                     addMessage(buffer, sqle.getMessage());
-                }
-            } else if (error instanceof JAXBException) {
-                JAXBException jaxbe = (JAXBException) error;
-                if (jaxbe.getLinkedException() != null) {
-                    addMessage(buffer, jaxbe.getMessage());
                 }
             }
         }
