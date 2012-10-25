419a2c8acd147b7ab3797c401953528699fc18c1
cmuller <cmuller@kuali.org>
2012-10-25 17:33:34 -0400

    KSENROLL-3205: Added comments, javadoc, removed unnecessary code.
    git-svn-id: https://svn.kuali.org/repos/student/enrollment/ks-api/branches/M5@39079 0ac02d80-fb3f-11dc-a574-c57244a850eb

--- ks-common-api/src/main/java/org/kuali/student/r2/common/criteria/CriteriaLookupService.java
+++ ks-common-api/src/main/java/org/kuali/student/r2/common/criteria/CriteriaLookupService.java
@@ -3,6 +3,8 @@ package org.kuali.student.r2.common.criteria;
 import org.kuali.rice.core.api.criteria.GenericQueryResults;
 import org.kuali.rice.core.api.criteria.QueryByCriteria;
 
+import java.util.List;
+
 public interface CriteriaLookupService {
 
     /**
@@ -27,12 +29,50 @@ public interface CriteriaLookupService {
      */
     <T> GenericQueryResults<T> lookup(final Class<T> queryClass, final QueryByCriteria criteria, final LookupCustomizer<T> customizer);
 
+    /**
+     * Looks up the id of a type based on a query criteria.
+     *
+     * @param queryClass the class to lookup
+     * @param criteria the criteria to lookup against. cannot be null.
+     * @param <T> the type that is being looked up.
+     * @return the results. will never be null.
+     * @throws IllegalArgumentException if the criteria is null
+     */
     <T> GenericQueryResults<String> lookupIds(final Class<T> queryClass, final QueryByCriteria criteria);
 
+    /**
+     * Looks up the id of a type based on a query criteria.
+     *
+     * @param queryClass the class to lookup
+     * @param criteria the criteria to lookup against. cannot be null.
+     * @param <T> the type that is being looked up.
+     * @return the results. will never be null.
+     * @throws IllegalArgumentException if the criteria is null
+     */
     <T> GenericQueryResults<String> lookupIds(final Class<T> queryClass, final QueryByCriteria criteria, final LookupCustomizer<T> customizer);
 
-    <T> GenericQueryResults<T> genericLookup(final Class<T> queryClass, final QueryByCriteria criteria, String field);
+    /**
+     * Looks up the requested field of a type based on a query criteria.
+     *
+     * @param queryClass the class to lookup
+     * @param criteria the criteria to lookup against. cannot be null.
+     * @param fields the field of the query class to lookup
+     * @param <T> the type that is being looked up.
+     * @return the results. will never be null.
+     * @throws IllegalArgumentException if the criteria is null
+     */
+    <T> GenericQueryResults<List<String>> genericLookup(final Class<T> queryClass, final QueryByCriteria criteria, List<String> fields);
 
-    <T> GenericQueryResults<T> genericLookup(final Class<T> queryClass, final QueryByCriteria criteria, final LookupCustomizer<T> customizer, String field);
+    /**
+     * Looks up the requested field of a type based on a query criteria.
+     *
+     * @param queryClass the class to lookup
+     * @param criteria the criteria to lookup against. cannot be null.
+     * @param fields the field of the query class to lookup
+     * @param <T> the type that is being looked up.
+     * @return the results. will never be null.
+     * @throws IllegalArgumentException if the criteria is null
+     */
+    <T> GenericQueryResults<List<String>> genericLookup(final Class<T> queryClass, final QueryByCriteria criteria, final LookupCustomizer<T> customizer, List<String> fields);
 
 }
