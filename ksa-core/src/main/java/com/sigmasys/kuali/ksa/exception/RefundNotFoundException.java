6f6d494aa03cbb2510bdddaaa91168c843800ca0
godunovs <godunovs@kuali.org>
2012-11-04 01:27:37 -0500

    Added a "refundId" attribute for more informative content.
    git-svn-id: https://svn.kuali.org/repos/student/contrib/ksa/trunk@39471 0ac02d80-fb3f-11dc-a574-c57244a850eb

--- ksa-core/src/main/java/com/sigmasys/kuali/ksa/exception/RefundNotFoundException.java
+++ ksa-core/src/main/java/com/sigmasys/kuali/ksa/exception/RefundNotFoundException.java
@@ -2,13 +2,34 @@ package com.sigmasys.kuali.ksa.exception;
 
 /**
  * This exception is thrown when a refund is not found.
+ * A <code>Refund</code> identifier of an object that was attempted to be located
+ * is stored in this object optionally as well.
+ * 
  *
  * @author Michael Ivanov
+ * @author Sergey Godunov
  *         Date: 8/7/12
  */
 public class RefundNotFoundException extends GenericException {
 
-    public RefundNotFoundException(String message) {
+	private static final long serialVersionUID = 1L;
+	
+	/**
+	 * ID of a <code>Refund</code> that was looked up, but not found.
+	 */
+	private Long refundId;
+	
+    public RefundNotFoundException(Long refundId, String message) {
         super(message);
+        
+        this.refundId = refundId;
+    }
+    
+    /**
+     * Returns the ID of a <code>Refund</code> object that was not found.
+     * @return ID of the not found <code>Refund</code>.
+     */
+    public Long getRefundId() {
+    	return refundId;
     }
 }
