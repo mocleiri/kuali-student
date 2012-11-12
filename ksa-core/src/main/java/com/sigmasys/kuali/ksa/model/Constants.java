11c838a37681fea4a252cac5e1e3ca04eae894b1
godunovs <godunovs@kuali.org>
2012-11-11 23:04:19 -0500

    Added Refund type method constants.
    git-svn-id: https://svn.kuali.org/repos/student/contrib/ksa/trunk@39768 0ac02d80-fb3f-11dc-a574-c57244a850eb

--- ksa-core/src/main/java/com/sigmasys/kuali/ksa/model/Constants.java
+++ ksa-core/src/main/java/com/sigmasys/kuali/ksa/model/Constants.java
@@ -71,7 +71,8 @@ public interface Constants {
     String REFUND_ACH_SYSTEM_NAME = "ksa.refund.ach.system.name";
     String REFUND_ACH_BANK_TYPE = "ksa.refund.ach.bank.type";
     String REFUND_ACH_GROUP_ROLLUP = "ksa.refund.ach.group.rollup";
-    String REFUND_METHOD = "refund.method";
-    String OVERRIDE_REFUND_METHOD = "override.refund.method";
-    String DEFAULT_REFUND_METHOD = "default.refund.method";
+    String REFUND_METHOD = "ksa.refund.method";
+    String OVERRIDE_REFUND_METHOD = "ksa.override.refund.method";
+    String DEFAULT_REFUND_METHOD = "ksa.default.refund.method";
+    String REFUND_SOURCE_TYPE = "ksa.refund.source.type";
 }
