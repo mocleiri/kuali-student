a127dfc1ccd57e486aa24a71693d9bba42da3e57
godunovs <godunovs@kuali.org>
2012-11-30 01:28:46 -0500

    An empty form. Can be used for hidden frame operations.
    git-svn-id: https://svn.kuali.org/repos/student/contrib/ksa/trunk@40518 0ac02d80-fb3f-11dc-a574-c57244a850eb

---
+++ ksa-core/src/main/java/com/sigmasys/kuali/ksa/krad/form/EmptyForm.java
@@ -0,0 +1,10 @@
+package com.sigmasys.kuali.ksa.krad.form;
+
+/**
+ * An empty form, used for hidden form submissions.
+ * 
+ * @author Sergey
+ */
+public class EmptyForm extends AbstractViewModel {
+
+}
