b20f175e328bff2a2c99b60d6bce8d9c11672ade
mivanov <mivanov@kuali.org>
2013-01-03 17:39:20 -0500

    Configured Enrollment Maven dependencies in pom.xml, added ATP DDL and DML scripts to KSA sql folder, created atp_create_database.sql script, modified create_users_grants.sql, added a prototype of the future KS context in KSA (ks-beans.xml file), added KS persistence unit and JPA entity manager factory configuration.
    git-svn-id: https://svn.kuali.org/repos/student/contrib/ksa/trunk@41557 0ac02d80-fb3f-11dc-a574-c57244a850eb

---
+++ sql/atp_create_database.sql
@@ -0,0 +1,13 @@
+@atp/ks-enroll-atp-create-schema.sql
+--@atp/KSEN_TYPE.sql
+--@atp/KSEN_STATE.sql
+--@atp/KSEN_STATE_PROCESS.sql
+--@atp/KSEN_STATE_LIFECYCLE.sql
+@atp/KSEN_MSTONE.sql
+@atp/KSEN_ATP.sql
+--@atp/KSEN_TYPETYPE_RELTN.sql
+@atp/KSEN_ATPMSTONE_RELTN.sql
+@atp/KSEN_ATPATP_RELTN.sql
+@atp/ks-enroll-atp-constraints.sql
+
+commit;
