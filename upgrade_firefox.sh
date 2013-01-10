410e9cd5797fbe26d46440e6f978818991df46d0
mike.wyers <mike.wyers@kuali.org>
2013-01-10 16:43:51 -0500

    KSENROLL-4672 Test Automation: schedule of classes tests failing due to firefox version in CI env
    Fix upgrade script
    
    git-svn-id: https://svn.kuali.org/repos/student/test/functional-automation/sambal/trunk@41837 0ac02d80-fb3f-11dc-a574-c57244a850eb

--- upgrade_firefox.sh
+++ upgrade_firefox.sh
@@ -5,12 +5,12 @@ TARGET=/usr/local
 export installroot=$TARGET/src
 export workpath=$TARGET
 
+unlink /usr/bin/firefox
+rm -rf /usr/lib/firefox-9.0.1
+
 cd $workpath
-rm -rf firefox
-unlink /usr/local/bin/firefox
 wget -r --no-parent --reject "index.html*" -nH --cut-dirs=7 https://ftp.mozilla.org/pub/mozilla.org/firefox/releases/14.0.1/linux-x86_64/en-US/
 tar xvf firefox-14.0.1.tar.bz2
 cd bin
 ln -s ../firefox/firefox
 ldconfig
-
