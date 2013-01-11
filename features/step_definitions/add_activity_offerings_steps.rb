1b16ae977a416d3bca0131f4a9f0f22f68bb160b
lpan <lpan@kuali.org>
2013-01-10 23:14:16 -0500

    ksenroll-4452: improve performance
    git-svn-id: https://svn.kuali.org/repos/student/test/functional-automation/sambal/trunk@41842 0ac02d80-fb3f-11dc-a574-c57244a850eb

--- features/step_definitions/add_activity_offerings_steps.rb
+++ features/step_definitions/add_activity_offerings_steps.rb
@@ -1,16 +1,17 @@
 When /^I manage an existing Course Offering in Course Offering Code view$/ do
   @course_offering = make CourseOffering, :course=>"CHEM317"
   @course_offering.manage
+  $total_number = @course_offering.ao_list.count
 end
 
 When /^I manage an existing Course Offering in Subject Code view$/ do
   @course_offering = make CourseOffering, :course=>"CHEM142", :search_by_subj => true
   @course_offering.manage
+  $total_number = @course_offering.ao_list.count
 end
 
 Then /^I am able to add an Activity Offering$/ do
   on ManageCourseOfferings do |page|
-    $total_number = page.codes_list.count
     format = page.format.options[1].text
     page.add_ao format, 1
   end
@@ -18,13 +19,10 @@ end
 
 Then /^I am able to copy an Activity Offering$/ do
   on ManageCourseOfferings do |page|
-    $total_number = page.codes_list.count
-
     if $total_number > 0
       ao_code = page.codes_list[0]
       page.copy(ao_code)
     end
-
   end
 end
 
