e819a958b3feb4c5dea1e59e86c356b30a48e70e
lpan <lpan@kuali.org>
2013-01-10 23:21:55 -0500

    ksenroll-4549: steps for story 12.3
    git-svn-id: https://svn.kuali.org/repos/student/test/functional-automation/sambal/trunk@41843 0ac02d80-fb3f-11dc-a574-c57244a850eb

---
+++ features/approve_course_offerings.feature
@@ -0,0 +1,32 @@
+@wip
+Feature: Approve COs and their AOs for scheduling
+
+  As a Departmental Administrator, I want to indicate that I have finished creating and editing all of the Course Offerings in my department
+  so that the departmental set of Course Offerings and their Activity Offerings will be available for Scheduling
+
+  As a Departmental Administrator, I want to indicate that I have finished editing specific Course Offerings
+  so that these Course Offerings and their Activity Offerings will be available for scheduling.
+
+  As a Departmental Administrator, I want to indicate that I have finished editing specific Activity Offerings
+  so that these Activity Offerings will be available for scheduling.
+
+  Background:
+    Given I am logged in as admin
+
+  Scenario: Approve a departmental set of COs and their AOs for scheduling
+    When I create two new Course Offerings
+    And I add Activity Offerings to the new Course Offerings
+    And I approve the subject code for scheduling
+    Then the Activity Offerings should be in Approved state
+
+  Scenario: Approve a Course Offering for scheduling
+    When I manage a Course Offering
+    And I add Activity Offerings to the Course Offering
+    And I approve the Course Offering for scheduling
+    Then the Activity Offerings should be in Approved state
+
+  Scenario: Approve selected Activity Offerings for scheduling
+    When I manage a Course Offering
+    And I add Activity Offerings to the Course Offering
+    And I approve selected Activity Offerings for scheduling
+    Then the selected Activity Offerings should be in Approved state
---
+++ features/step_definitions/approve_course_offerings_steps.rb
@@ -0,0 +1,56 @@
+When /^I create two new Course Offerings$/ do
+  pending # express the regexp above with the code you wish you had
+end
+
+And /^I add Activity Offerings to the new Course Offerings$/ do
+  pending # express the regexp above with the code you wish you had
+end
+
+And /^I approve the subject code for scheduling$/ do
+  pending # express the regexp above with the code you wish you had
+end
+
+When /^I manage a Course Offering$/ do
+  @course_offering = make CourseOffering, :course=>"CHEM317"
+  @course_offering.manage
+  @orig_ao_list = @course_offering.ao_list
+end
+
+And /^I add Activity Offerings to the Course Offering$/ do
+  on ManageCourseOfferings do |page|
+    format = page.format.options[1].text
+    page.add_ao format, 1
+    #page.add_ao format, 2
+    @updated_ao_list = page.codes_list
+  end
+end
+
+And /^I approve the Course Offering for scheduling$/ do
+  pending # express the regexp above with the code you wish you had
+end
+
+And /^I approve selected Activity Offerings for scheduling$/ do
+  #@new_ao_list = @orig_ao_list ^ @updated_ao_list
+  on ManageCourseOfferings do |page|
+    @new_ao_list = [@updated_ao_list[0],@updated_ao_list[1],@updated_ao_list[2]]
+    page.select_aos(@new_ao_list)
+    page.selected_offering_actions.select("Approve for Scheduling")
+    page.go
+  end
+end
+
+Then /^the Activity Offerings should be in Approved state$/ do
+  on ManageCourseOfferings do |page|
+    for code in @new_ao_list
+      page.ao_status("code", "Approved").should == true
+    end
+  end
+end
+
+Then /^the selected Activity Offerings should be in Approved state$/ do
+  on ManageCourseOfferings do |page|
+    for code in @new_ao_list
+      page.ao_status(code, "Approved").should == true
+    end
+  end
+end
