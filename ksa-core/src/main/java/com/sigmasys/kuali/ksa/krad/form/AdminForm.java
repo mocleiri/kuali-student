080f71192e1835d81e5f1d325823645dbf3859b1
godunovs <godunovs@kuali.org>
2013-01-14 00:42:28 -0500

    Added Account attribute and AccountInformation data holder.
    git-svn-id: https://svn.kuali.org/repos/student/contrib/ksa/trunk@41904 0ac02d80-fb3f-11dc-a574-c57244a850eb

--- ksa-core/src/main/java/com/sigmasys/kuali/ksa/krad/form/AdminForm.java
+++ ksa-core/src/main/java/com/sigmasys/kuali/ksa/krad/form/AdminForm.java
@@ -1,14 +1,26 @@
 package com.sigmasys.kuali.ksa.krad.form;
 
-import com.sigmasys.kuali.ksa.model.Activity;
-
+import java.io.Serializable;
 import java.util.List;
+
+import com.sigmasys.kuali.ksa.model.*;
 
 /**
  * Created by: dmulderink on 10/5/12 at 6:55 PM
  */
+@SuppressWarnings("all")
 public class AdminForm extends AbstractViewModel {
-
+	
+	/**
+	 * Account for creation or editing.
+	 */
+	private Account account;
+	
+	/**
+	 * Account information holder object
+	 */
+	private AccountInformation accountInfo;
+	
    /*
      Activities
    */
@@ -20,11 +32,59 @@ public class AdminForm extends AbstractViewModel {
      Get / Set methods
    */
 
-   public List<Activity> getActivities() {
-      return activities;
-   }
+   	public List<Activity> getActivities() {
+   		return activities;
+   	}
 
-   public void setActivities(List<Activity> activities) {
-      this.activities = activities;
-   }
+   	public void setActivities(List<Activity> activities) {
+   		this.activities = activities;
+   	}
+
+	public Account getAccount() {
+		return account;
+	}
+	
+	public void setAccount(Account account) {
+		this.account = account;
+	}
+	
+	public AccountInformation getAccountInfo() {
+		return accountInfo;
+	}
+	
+	public void setAccountInfo(AccountInformation accountInfo) {
+		this.accountInfo = accountInfo;
+	}
+	
+	/**
+	 * Account Biographic information container class.
+	 * 
+	 * @author Sergey
+	 *
+	 */
+	public static class AccountInformation implements Serializable {
+		private PersonName name;
+		private PostalAddress address;
+		private ElectronicContact electronicContact;
+		
+		public PersonName getName() {
+			return name;
+		}
+		public void setName(PersonName name) {
+			this.name = name;
+		}
+		public PostalAddress getAddress() {
+			return address;
+		}
+		public void setAddress(PostalAddress address) {
+			this.address = address;
+		}
+		public ElectronicContact getElectronicContact() {
+			return electronicContact;
+		}
+		public void setElectronicContact(ElectronicContact electronicContact) {
+			this.electronicContact = electronicContact;
+		}
+	}
+
 }
