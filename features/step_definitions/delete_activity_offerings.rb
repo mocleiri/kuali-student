55d506add51f2dd26336e63ef222cc033b112644
chongzhu <chongzhu@kuali.org>
2013-01-18 12:20:36 -0500

    KSENROLL-3786:cleaned up the code.
    git-svn-id: https://svn.kuali.org/repos/student/test/functional-automation/sambal/trunk@42156 0ac02d80-fb3f-11dc-a574-c57244a850eb

--- features/step_definitions/delete_activity_offerings.rb
+++ features/step_definitions/delete_activity_offerings.rb
@@ -1,12 +1,12 @@
 When /^I delete the selected multiple AOs$/ do
-  $total_number = @course_offering.ao_list.count
-  @course_offering.copy_ao :ao_code =>  @course_offering.ao_list[$total_number-1]
-  @course_offering.copy_ao :ao_code =>  @course_offering.ao_list[$total_number-1]
-  @course_offering.copy_ao :ao_code =>  @course_offering.ao_list[$total_number-1]
+  @total_number = @course_offering.ao_list.count
+  @course_offering.copy_ao :ao_code =>  @course_offering.ao_list[@total_number-1]
+  @course_offering.copy_ao :ao_code =>  @course_offering.ao_list[@total_number-1]
+  @course_offering.copy_ao :ao_code =>  @course_offering.ao_list[@total_number-1]
   @course_offering.manage
   new_total = @course_offering.ao_list.count
-  new_total.should == $total_number + 3
-  $total_number = @course_offering.ao_list.count
+  new_total.should == @total_number + 3
+  @total_number = @course_offering.ao_list.count
   @ao_code_list = [@course_offering.ao_list[0],@course_offering.ao_list[1],@course_offering.ao_list[2]]
   @course_offering.delete_ao_list :code_list =>  @ao_code_list
 end
@@ -14,7 +14,7 @@ end
 Then /^The AOs are Successfully deleted$/ do
   @course_offering.manage
   new_total = @course_offering.ao_list.count
-  new_total.should == $total_number - 3
+  new_total.should == @total_number - 3
   for code in @ao_code_list
     if @course_offering.ao_list.include? code
       raise "Activity Offering with AO_CODE: code is not deleted!"
@@ -24,21 +24,21 @@ end
 
 
 When /^I delete an AO with Draft state$/ do
-  $total_number = @course_offering.ao_list.count
-  @course_offering.copy_ao :ao_code =>  @course_offering.ao_list[$total_number-1]
+  @total_number = @course_offering.ao_list.count
+  @course_offering.copy_ao :ao_code =>  @course_offering.ao_list[@total_number-1]
   @course_offering.manage
   new_total = @course_offering.ao_list.count
-  new_total.should == $total_number + 1
-  $total_number = @course_offering.ao_list.count
-  $deleted_ao_code = @course_offering.ao_list[0]
+  new_total.should == @total_number + 1
+  @total_number = @course_offering.ao_list.count
+  @deleted_ao_code = @course_offering.ao_list[0]
   @course_offering.delete_ao :ao_code =>  @course_offering.ao_list[0]
 end
 
 Then /^The AO is Successfully deleted$/ do
   @course_offering.manage
   new_total = @course_offering.ao_list.count
-  new_total.should == $total_number - 1
-  if @course_offering.ao_list.include? $deleted_ao_code
-    raise "Activity Offering with AO_CODE: $deleted_ao_code is not deleted!"
+  new_total.should == @total_number - 1
+  if @course_offering.ao_list.include? @deleted_ao_code
+    raise "Activity Offering with AO_CODE: @deleted_ao_code is not deleted!"
   end
 end
