1810516f2c857c9eff3e91d00fcdb225ce79301a
mivanov <mivanov@kuali.org>
2013-01-30 15:10:53 -0500

    Fixed the line separator in ksa_test_data_dml.sql
    git-svn-id: https://svn.kuali.org/repos/student/contrib/ksa/trunk@42755 0ac02d80-fb3f-11dc-a574-c57244a850eb

--- sql/test_inserts/ksa_test_data_dml.sql
+++ sql/test_inserts/ksa_test_data_dml.sql
@@ -1263,6 +1263,7 @@ Insert into KSSA_RULE_SET_RULE ( RULE_SET_ID_FK, RULE_ID_FK ) values (1, 5)!
 Insert into KSSA_RULE_SET_RULE ( RULE_SET_ID_FK, RULE_ID_FK ) values (1, 6)!
 Insert into KSSA_RULE_SET_RULE ( RULE_SET_ID_FK, RULE_ID_FK ) values (1, 7)!
 
-set sqlterminator ';'   ;
+set sqlterminator ';'
+
 
 
