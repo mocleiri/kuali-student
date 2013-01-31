a6e64cb6093745eb361bd6538e5b5ba6841a95dd
mivanov <mivanov@kuali.org>
2013-01-31 17:48:26 -0500

    A little cleanup
    git-svn-id: https://svn.kuali.org/repos/student/contrib/ksa/trunk@42819 0ac02d80-fb3f-11dc-a574-c57244a850eb

--- ksa-core/src/main/java/com/sigmasys/kuali/ksa/krad/util/KsaHelper.java
+++ ksa-core/src/main/java/com/sigmasys/kuali/ksa/krad/util/KsaHelper.java
@@ -4,8 +4,6 @@ import com.sigmasys.kuali.ksa.model.Tag;
 import com.sigmasys.kuali.ksa.service.AuditableEntityService;
 import com.sigmasys.kuali.ksa.util.ContextUtils;
 import org.kuali.rice.krad.uif.service.impl.ViewHelperServiceImpl;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Service;
 
 import java.util.List;
 
@@ -14,19 +12,18 @@ import java.util.List;
  * Date: 1/30/13
  * Time: 5:25 PM
  */
-public class KsaHelper  extends ViewHelperServiceImpl {
+public class KsaHelper extends ViewHelperServiceImpl {
 
-    //@Autowired
     private AuditableEntityService auditableEntityService;
 
 
-    public List<Tag> getTagsForSuggest(String suggest){
+    public List<Tag> getTagsForSuggest(String suggest) {
 
         return getAuditableEntityService().getAuditableEntitiesByName(suggest, Tag.class);
     }
 
     private AuditableEntityService getAuditableEntityService() {
-        if(auditableEntityService == null) {
+        if (auditableEntityService == null) {
             auditableEntityService = ContextUtils.getBean(AuditableEntityService.class);
         }
         return auditableEntityService;
