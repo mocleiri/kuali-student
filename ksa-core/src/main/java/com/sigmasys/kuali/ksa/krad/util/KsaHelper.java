1e9e206ec952fba9a99b702003354fe4339f971d
tbornholtz <tbornholtz@kuali.org>
2013-01-31 14:21:37 -0500

    Autowire service
    git-svn-id: https://svn.kuali.org/repos/student/contrib/ksa/trunk@42806 0ac02d80-fb3f-11dc-a574-c57244a850eb

--- ksa-core/src/main/java/com/sigmasys/kuali/ksa/krad/util/KsaHelper.java
+++ ksa-core/src/main/java/com/sigmasys/kuali/ksa/krad/util/KsaHelper.java
@@ -5,6 +5,7 @@ import com.sigmasys.kuali.ksa.service.AuditableEntityService;
 import com.sigmasys.kuali.ksa.util.ContextUtils;
 import org.kuali.rice.krad.uif.service.impl.ViewHelperServiceImpl;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
 
 import java.util.List;
 
@@ -13,6 +14,7 @@ import java.util.List;
  * Date: 1/30/13
  * Time: 5:25 PM
  */
+@Service("ksaHelper")
 public class KsaHelper  extends ViewHelperServiceImpl {
 
     @Autowired
@@ -21,15 +23,18 @@ public class KsaHelper  extends ViewHelperServiceImpl {
 
     public List<Tag> getTagsForSuggest(String suggest){
 
-        return getAuditableEntityService().getAuditableEntitiesByName(suggest, Tag.class);
+        return auditableEntityService.getAuditableEntitiesByName(suggest, Tag.class);
     }
 
 
+    /*
     private AuditableEntityService getAuditableEntityService() {
         if(auditableEntityService == null) {
             auditableEntityService = ContextUtils.getBean(AuditableEntityService.class);
         }
         return auditableEntityService;
     }
+
+    */
 
 }
