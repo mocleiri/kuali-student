81c82f00f141fb58f7a071af2bcd3bb22755c9f3
kmuthusw <kmuthusw@kuali.org>
2013-02-07 13:37:04 -0500

    git-svn-id: https://svn.kuali.org/repos/student/contrib/myplan/branches/ks-myplan-1.1.x@43099 0ac02d80-fb3f-11dc-a574-c57244a850eb

--- ks-myplan/new_branch/ks-myplan/myplan-impl/src/main/java/org/kuali/student/myplan/academicplan/dao/LearningPlanTypeDao.java
+++
@@ -1,9 +0,0 @@
-package org.kuali.student.myplan.academicplan.dao;
-
-import org.kuali.student.enrollment.dao.GenericEntityDao;
-import org.kuali.student.myplan.academicplan.model.LearningPlanTypeEntity;
-
-/**
- *
- */
-public class LearningPlanTypeDao extends GenericEntityDao<LearningPlanTypeEntity> {}
