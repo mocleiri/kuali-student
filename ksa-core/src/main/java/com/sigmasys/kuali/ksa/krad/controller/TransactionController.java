dfd4d341da5e7a15d5a948a0b6ede921efaf8ac7
tbornholtz <tbornholtz@kuali.org>
2013-02-22 11:30:01 -0500

    Code cleanup and adding a test case
    git-svn-id: https://svn.kuali.org/repos/student/contrib/ksa/trunk@43838 0ac02d80-fb3f-11dc-a574-c57244a850eb

--- ksa-core/src/main/java/com/sigmasys/kuali/ksa/krad/controller/TransactionController.java
+++ ksa-core/src/main/java/com/sigmasys/kuali/ksa/krad/controller/TransactionController.java
@@ -28,8 +28,8 @@ public class TransactionController extends GenericSearchController {
 
     private Log logger = LogFactory.getLog(TransactionController.class);
 
-    @Autowired
-    private ActivityService activityService;
+    //@Autowired
+    //private ActivityService activityService;
 
     @Autowired
     private AuditableEntityService auditableEntityService;
@@ -57,24 +57,6 @@ public class TransactionController extends GenericSearchController {
         return getUIFModelAndView(form);
     }
 
-
-    /**
-     * @param form
-     * @param request
-     * @return
-     */
-    @RequestMapping(method = RequestMethod.POST, params = "methodToCall=refresh")
-    public ModelAndView refresh(@ModelAttribute("KualiForm") TransactionForm form, HttpServletRequest request) {
-        // just for the transactions by person page
-        String pageId = request.getParameter("pageId");
-
-        if (pageId != null && pageId.compareTo("bursaActivityPage") == 0) {
-            form.setActivities(activityService.getActivities());
-        }
-
-        // do refresh stuff...
-        return getUIFModelAndView(form);
-    }
 
     /**
      * @param form
@@ -111,169 +93,8 @@ public class TransactionController extends GenericSearchController {
 
 
         }
-        if ("bursaTransByPersonPage".equals(pageId)) {
-            String id = request.getParameter("id");
-            if (id == null || id.isEmpty()) {
-                throw new IllegalArgumentException("'id' request parameter must be specified");
-            }
-
-            String compositePersonName = getAccount(id).getCompositeDefaultPersonName();
-            form.setSelectedPersonName(compositePersonName);
-
-            // charges by ID
-            List<Charge> charges = transactionService.getCharges(id);
-
-            // payments by ID
-            List<Payment> payments = transactionService.getPayments(id);
-
-            form.setChargeList(charges);
-
-            form.setPaymentList(payments);
-        }
-
-        if ("bursaChargePage".equals(pageId)) {
-            String id = request.getParameter("id");
-            if (id == null || id.isEmpty()) {
-                throw new IllegalArgumentException("'id' request parameter must be specified");
-            }
-
-            // charge by ID
-            Charge charge = transactionService.getCharge(Long.valueOf(id));
-
-            if (charge != null) {
-                String compositePersonName = charge.getAccount().getCompositeDefaultPersonName();
-                form.setSelectedPersonName(compositePersonName);
-            }
-
-            form.setCharge(charge);
-        }
-
-        if ("bursaPaymentPage".equals(pageId)) {
-            String id = request.getParameter("id");
-            if (id == null || id.isEmpty()) {
-                throw new IllegalArgumentException("'id' request parameter must be specified");
-            }
-
-            // payment by ID
-            Payment payment = transactionService.getPayment(Long.valueOf(id));
-
-            if (payment != null) {
-                String compositePersonName = payment.getAccount().getCompositeDefaultPersonName();
-                form.setSelectedPersonName(compositePersonName);
-            }
-
-            form.setPayment(payment);
-        }
-
-        // Currency type
-        if ("bursaCurrencyPage".equals(pageId)) {
-            String subMethod = request.getParameter("subMethod");
-            if (subMethod != null && subMethod.compareTo("deleteCurr") == 0) {
-                String id = request.getParameter("id");
-                if (id == null || id.isEmpty()) {
-                    throw new IllegalArgumentException("'id' request parameter must be specified");
-                }
-
-                // remove a currency record by ID
-                auditableEntityService.deleteAuditableEntity(Long.valueOf(id), Currency.class);
-            }
-
-            form.setCurrencies(auditableEntityService.getCurrencies());
-        }
-
-        // Currency type edit
-        if ("bursaCurrencyEditPage".equals(pageId)) {
-
-            String code = request.getParameter("code");
-            if (code == null || code.isEmpty()) {
-                throw new IllegalArgumentException("'code' request parameter must be specified");
-            }
-
-            Currency currency = auditableEntityService.getCurrency(code);
-
-            form.setCurrency(currency);
-        }
-
-        if ("bursaActivityPage".equals(pageId)) {
-            form.setActivities(activityService.getActivities());
-        }
 
         return getUIFModelAndView(form);
-    }
-
-    /**
-     * User searches on a (last) name. The result set is iterated over to create the composite PersonName
-     * and composite address using default records, eventfully creating a browse list that can be displayed
-     * and selected from for further processing as desired.
-     *
-     * @param form
-     * @return
-     */
-    @RequestMapping(method = RequestMethod.POST, params = "methodToCall=searchByName")
-    public ModelAndView searchByName(@ModelAttribute("KualiForm") TransactionForm form) {
-
-        // we do not have a query by name or partial name via last name or contains yet
-        // if no result set from getting full accounts than the List is empty
-        // otherwise the lit contains records and a compsite person name and postal address
-
-        String studentLookupByName = form.getStudentLookupByName();
-
-        // query for all accounts
-
-        List<Account> accountSearchList = accountService.getFullAccounts();
-
-        // create a a list of Account objects for display requirements
-
-        List<Account> accountList = new ArrayList<Account>();
-
-        // if we have a result set of Accounts from the query
-
-        for (Account account : accountSearchList) {
-
-            PersonName personName = account.getDefaultPersonName();
-
-            if (personName != null && personName.getLastName().contains(studentLookupByName)) {
-
-                // add each account copy to a list
-
-                accountList.add(account.getCopy());
-            }
-        }
-
-        // set the account list derived from the full search list
-
-        form.setAccountBrowseList(accountList);
-
-        // do a search by name returning account info
-        return getUIFModelAndView(form);
-    }
-
-    @RequestMapping(method = RequestMethod.POST, params = "methodToCall=addCurrType")
-    @Transactional(readOnly = false)
-    public ModelAndView addCurrType(@ModelAttribute("KualiForm") TransactionForm form) {
-
-        // add a Currency Type
-        Currency currency = new Currency();
-
-        currency.setCode(form.getCode());
-        currency.setName(form.getCurrencyName());
-        currency.setDescription(form.getCurrencyDescription());
-
-        // remove a currency record by ISO type
-        auditableEntityService.persistAuditableEntity(currency);
-
-        // refresh the list of currencies. the form and view manage the refresh
-        form.setCurrencies(auditableEntityService.getCurrencies());
-
-        return getUIFModelAndView(form);
-    }
-
-    private Account getAccount(String accountId) {
-        Account account = accountService.getFullAccount(accountId);
-        if (account != null) {
-            return account;
-        }
-        throw new UserNotFoundException("Cannot find Account by ID = " + accountId);
     }
 
     private void populateRollups(TransactionForm form, List<TransactionModel> transactions) {
--- ksa-core/src/test/java/com/sigmasys/kuali/ksa/service/ServiceTestSuite.java
+++ ksa-core/src/test/java/com/sigmasys/kuali/ksa/service/ServiceTestSuite.java
@@ -1,9 +1,6 @@
 package com.sigmasys.kuali.ksa.service;
 
-import com.sigmasys.kuali.ksa.krad.controller.SearchControllerTest;
-import com.sigmasys.kuali.ksa.krad.controller.SettingsControllerTest;
-import com.sigmasys.kuali.ksa.krad.controller.TemplateControllerTest;
-import com.sigmasys.kuali.ksa.krad.controller.TransactionTypeControllerTest;
+import com.sigmasys.kuali.ksa.krad.controller.*;
 import junit.framework.JUnit4TestAdapter;
 import junit.framework.Test;
 import org.junit.runner.RunWith;
@@ -42,6 +39,7 @@ import org.junit.runners.Suite;
         TemplateControllerTest.class,
         SettingsControllerTest.class,
         SearchControllerTest.class,
+        TransactionControllerTest.class,
         TransactionTypeControllerTest.class,
         AtpServiceTest.class})
 public class ServiceTestSuite extends AbstractServiceTest {
