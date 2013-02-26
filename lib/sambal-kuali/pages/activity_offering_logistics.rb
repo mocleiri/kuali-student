cc6805c9aff67b87f331c688588dcf87865b44a8
akucera <akucera@kuali.org>
2013-02-26 12:06:06 -0500

    KSENROLL-4791
    git-svn-id: https://svn.kuali.org/repos/student/test/functional-automation/sambal/trunk@44036 0ac02d80-fb3f-11dc-a574-c57244a850eb

--- lib/sambal-kuali/pages/activity_offering_logistics.rb
+++ lib/sambal-kuali/pages/activity_offering_logistics.rb
@@ -1,6 +1,9 @@
 class ActivityOfferingLogistics < BasePage
+  wrapper_elements
+  frame_element
 
   action(:save_request) { |b| b.frm.button(text: "Save Request").click; b.loading.wait_while_present }
+  element(:cancel_btn_element) { |b| b.frm.link(text: "Cancel");}
   action(:cancel) { |b| b.frm.link(text: "Cancel").click; b.loading.wait_while_present }
   action(:save_and_process) { |b| b.frm.button(text: "Save and Process Request").click; b.loading.wait_while_present }
 
