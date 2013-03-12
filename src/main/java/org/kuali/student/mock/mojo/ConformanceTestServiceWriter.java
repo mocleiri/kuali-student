4be54851ac50bdfb7c4d893ad6611dc31a951301
mezba.mahtab <mezba.mahtab@kuali.org>
2013-03-12 16:17:47 -0400

    KSENROLL-5901 completed all the unit test methods
    git-svn-id: https://svn.kuali.org/repos/student/tools/maven-kscontractdoc-plugin/trunk@44900 0ac02d80-fb3f-11dc-a574-c57244a850eb

--- src/main/java/org/kuali/student/mock/mojo/ConformanceTestServiceWriter.java
+++ src/main/java/org/kuali/student/mock/mojo/ConformanceTestServiceWriter.java
@@ -138,7 +138,13 @@ public class ConformanceTestServiceWriter extends MockImplServiceWriter {
         openBrace();
         // end method open signature
 
+        // write the test portions
+        incrementIndent();
         writeTestCreate(dtoObjectName);
+        writeTestUpdate(dtoObjectName);
+        writeTestReadAfterUpdate(dtoObjectName);
+        writeTestDelete(dtoObjectName);
+        decrementIndent();
 
         // end method
         closeBrace();
@@ -148,7 +154,6 @@ public class ConformanceTestServiceWriter extends MockImplServiceWriter {
      * Write the 'test create' portion.
      */
     public void writeTestCreate (String dtoObjectName) {
-        incrementIndent();
         indentPrintDecoratedComment("test create");
         indentPrintln(dtoObjectName + " expected = new " + dtoObjectName + " ()");
         indentPrintln("expected.setName(\"Name01\");");
@@ -168,7 +173,7 @@ public class ConformanceTestServiceWriter extends MockImplServiceWriter {
         indentPrintln("new AttributeTester().add2ForCreate(expected.getAttributes());");
 
         indentPrintln("");
-        indentPrintln("DTOInfo actual = // INSERT CODE TO CREATE actual HERE");
+        indentPrintln(dtoObjectName + " actual = // INSERT CODE TO CREATE actual HERE");
         indentPrintln("");
 
         indentPrintln("assertNotNull(actual.getId());");
@@ -180,18 +185,144 @@ public class ConformanceTestServiceWriter extends MockImplServiceWriter {
 
         indentPrintln("new AttributeTester().check(expected.getAttributes(), actual.getAttributes());");
         indentPrintln("new MetaTester().checkAfterCreate(actual.getMeta());");
-
-        decrementIndent();
     }
 
+    /**
+     * Write the 'test update' portion.
+     */
+    public void writeTestUpdate (String dtoObjectName) {
+        indentPrintDecoratedComment("test update");
+        indentPrintln("expected = actual;");
+        indentPrintln("expected.setName(expected.getName() + \" updated\");");
+        indentPrintln("expected.setDescr(new RichTextHelper().fromPlain(expected.getDescr().getPlain() + \" updated\"));");
+        indentPrintln("");
+        indentPrintln("if (expected implements TypeStateEntityInfo)");
+        openBrace();
+        incrementIndent();
+        indentPrintln("expectedTS = (TypeStateEntityInfo) expected;");
+        indentPrintln("expectedTS.setStateKey(expected.getState() + \" updated\");");
+        decrementIndent();
+        closeBrace();
+        indentPrintln("");
+        indentPrintln("// INSERT CODE TO UPDATE MORE DTO FIELDS HERE");
+        indentPrintln("");
+        indentPrintln("new AttributeTester().delete1Update1Add1ForUpdate(expected.getAttributes());");
+        indentPrintln("actual = // INSERT CODE TO CALL UPDATE SERVICE OP HERE");
+        indentPrintln("assertEquals(expected.getId(), actual.getId());");
+        indentPrintln("new IdEntityTester().check(expected, actual);");
+        indentPrintln("");
+        indentPrintln("// INSERT CODE FOR TESTING MORE DTO FIELDS HERE");
+        indentPrintln("");
+        indentPrintln("new AttributeTester().check(expected.getAttributes(), actual.getAttributes());");
+        indentPrintln("new MetaTester().checkAfterUpdate(expected.getMeta(), actual.getMeta());");
+        indentPrintln("");
+    }
+
+    /**
+     * Write the 'read after update' portion.
+     */
+    public void writeTestReadAfterUpdate (String dtoObjectName) {
+        indentPrintDecoratedComment("test read after update");
+        indentPrintln("");
+        indentPrintln("expected = actual;");
+        indentPrintln("actual = // INSERT CODE TO GET actual HERE BY CALLING SERVICE OP");
+        indentPrintln("assertEquals(expected.getId(), actual.getId());");
+        indentPrintln("new IdEntityTester().check(expected, actual);");
+        indentPrintln("");
+        indentPrintln("// INSERT CODE FOR TESTING MORE DTO FIELDS HERE");
+        indentPrintln("");
+        indentPrintln("new AttributeTester().check(expected.getAttributes(), actual.getAttributes());");
+        indentPrintln("new MetaTester().checkAfterGet(expected.getMeta(), actual.getMeta());");
+        indentPrintln("");
+        indentPrintln(dtoObjectName + " alphaDTO = actual;");
+        indentPrintln("");
+        indentPrintln("// create a 2nd DTO");
+        indentPrintln(dtoObjectName + " betaDTO = new " + dtoObjectName + "();");
+        indentPrintln("betaDTO.setName(\"Beta entity name\");");
+        indentPrintln("betaDTO.setDescr(new RichTextHelper().fromPlain(\"Beta entity description\"));");
+        indentPrintln("");
+        indentPrintln("// INSERT CODE TO SET MORE DTO FIELDS HERE");
+        indentPrintln("");
+        indentPrintln("if (betaDTO implements TypeStateEntityInfo)");
+        openBrace();
+        incrementIndent();
+        indentPrintln("betaDTOTS = (TypeStateEntityInfo) betaDTO;");
+        indentPrintln("betaDTOTS.setTypeKey(\"TypeBeta\");");
+        indentPrintln("betaDTOTS.setStateKey(\"StateBeta\");");
+        decrementIndent();
+        closeBrace();
+        indentPrintln("betaDTO = // INSERT CODE TO CREATE betaDTO");
+        indentPrintln("");
+        indentPrintDecoratedComment("test bulk get with no ids supplied");
+        indentPrintln("");
+        indentPrintln("List<String> ids = new ArrayList<String>();");
+        indentPrintln("List<" + dtoObjectName + "> records = // INSERT CODE TO GET DTO BY IDS");
+        indentPrintln("assertEquals(ids.size(), records.size());");
+        indentPrintln("assertEquals(0, ids.size());");
+        indentPrintln("");
+        indentPrintDecoratedComment("test bulk get");
+        indentPrintln("ids = new ArrayList<String>();");
+        indentPrintln("ids.add(alphaDTO.getId());");
+        indentPrintln("ids.add(betaDTO.getId());");
+        indentPrintln("records = // INSERT CODE TO GET DTO BY IDS");
+        indentPrintln("assertEquals(ids.size(), records.size());");
+        indentPrintln("for (" + dtoObjectName + " record : records)");
+        openBrace();
+        incrementIndent();
+        indentPrintln("if (!ids.remove(record.getId()))");
+        openBrace();
+        incrementIndent();
+        indentPrintln("fail(record.getId());");
+        decrementIndent();
+        closeBrace();
+        decrementIndent();
+        closeBrace();
+        indentPrintln("assertEquals(0, ids.size());");
+        indentPrintln("");
+        indentPrintDecoratedComment("test get by type");
+        indentPrintln("ids = // INSERT CODE TO GET BY SPECIFIC TYPE \"Type01\" HERE");
+        indentPrintln("assertEquals(1, ids.size());");
+        indentPrintln("assertEquals(alphaDTO.getId(), ids.get(0));");
+        indentPrintln("");
+        indentPrintln("// test get by other type");
+        indentPrintln("ids = // INSERT CODE TO GET BY SPECIFIC TYPE \"TypeBeta\" HERE");
+        indentPrintln("assertEquals(1, ids.size());");
+        indentPrintln("assertEquals(betaDTO.getId(), ids.get(0));");
+        indentPrintln("");
+    }
+
+    /**
+     * Write the 'read after update' portion.
+     */
+    public void writeTestDelete (String dtoObjectName) {
+        indentPrintDecoratedComment("test delete");
+        indentPrintln("");
+        indentPrintln("StatusInfo status = // INSERT CODE TO DELETE RECORD");
+        indentPrintln("assertNotNull(status);");
+        indentPrintln("assertTrue(status.getIsSuccess());");
+        indentPrintln("try");
+        openBrace();
+        incrementIndent();
+        indentPrintln("record = // INSERT CODE TO RETRIEVE RECENTLY DELETED RECORD");
+        indentPrintln("fail(\"Did not receive DoesNotExistException when attempting to get already-deleted entity\");");
+        decrementIndent();
+        closeBrace();
+        indentPrintln("catch (DoesNotExistException dnee)");
+        openBrace();
+        incrementIndent();
+        indentPrintln("// expected");
+        decrementIndent();
+        closeBrace();
+        indentPrintln("");
+    }
 
     /**
      * Writes out a decorated comment.
      */
     public void indentPrintDecoratedComment (String label) {
-        indentPrintln("// ----------------------");
+        indentPrintln("// -------------------------------------");
         indentPrintln("// " + label);
-        indentPrintln("// ----------------------");
+        indentPrintln("// -------------------------------------");
     }
 
 }
