b3c71b701990325145c470d3a7a130f4e4ec40a4
jcaddel <jcaddel@kuali.org>
2013-04-10 14:02:26 -0400

    git-svn-id: https://svn.kuali.org/repos/student/sandbox/spring-db-jc/ks-lum/trunk@46245 0ac02d80-fb3f-11dc-a574-c57244a850eb

--- ks-lum-sql/src/main/resources/initial-db/lu/data/KSLU_RSLT_USG_TYPE.sql
+++
@@ -1,9 +0,0 @@
-TRUNCATE TABLE KSLU_RSLT_USG_TYPE DROP STORAGE
-/
-INSERT ALL
-  INTO KSLU_RSLT_USG_TYPE (EFF_DT,NAME,OBJ_ID,TYPE_DESC,TYPE_KEY,VER_NBR)
-  VALUES (TO_DATE( '20000101000000', 'YYYYMMDDHH24MISS' ),'Final Grade','66CF4F9EA32B45959A91C3AEA33D0215','Final learning result for an LU. A stereotypical usage is the final grade in a course.','lrType.finalGrade',0)
-  INTO KSLU_RSLT_USG_TYPE (EFF_DT,NAME,OBJ_ID,TYPE_DESC,TYPE_KEY,VER_NBR)
-  VALUES (TO_DATE( '20000101000000', 'YYYYMMDDHH24MISS' ),'Midterm Grade','1811D2788C914B1088FDCD845D56B75E','Midterm learning result for an LU. A stereotypical usage is the midterm grade in a course.','lrType.midtermGrade',0)
-SELECT * FROM DUAL
-/
