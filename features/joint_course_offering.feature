8d17982ce6d54695c06396648165b931e0f6c3c9
brandon.gresham <brandon.gresham@kuali.org>
2013-04-10 20:36:54 -0400

    KSENROLL-6364 Introduced final draft of joint-CO tests.
    git-svn-id: https://svn.kuali.org/repos/student/test/functional-automation/sambal/trunk@46281 0ac02d80-fb3f-11dc-a574-c57244a850eb

--- features/joint_course_offering.feature
+++ features/joint_course_offering.feature
@@ -1,23 +1,28 @@
+@draft
+@adam.campbell
 Feature: Jointly-defined Course Offering
+
   Background:
     Given I am logged in as a Schedule Coordinator
-  Scenario: Create from Catalog a Course Offering which is defined in the CLU as Joint
-    When I create joint Course Offerings
-    And I can add activity offerings to the joint Course Offerings
-  Scenario: Delete jointly-defined Course Offering with NO shared AOs - CANCEL operation
-    When I create joint Course Offerings
-    And I attempt to delete a joint Course Offering
-    Then I can cancel the deletion
-    And the Course Offering is not deleted
-  Scenario: Delete jointly-defined Course Offering with NO shared AOs - COMPLETE operation
-    When I create joint Course Offerings
-    And I attempt to delete a joint Course Offering
-    Then I can complete the deletion
-    And the Course Offering is deleted
+
+  @draft
+  @adam.campbell
+  Scenario Outline: Delete jointly-defined Course Offering with NO shared AOs
+    When I create a new jointly defined Course Offering
+    And I attempt to "<delete>" a joint Course Offering
+    Then the Course Offering is "<deleted>"
+    Examples:
+      | delete            | deleted     |
+      | delete            | deleted     |
+      | delete and cancel | not deleted |
 
 
-  #this is not a real test -- just a scratchpad for Brandon to use while co-developing tests with Adam -- DELETE THIS TEST!
-  @wip
-  @bjg
-  Scenario: Blah blah delete this test it's bogus
-    When I setup my test
+#   CCO 2.1 -- we cannot complete this ATM due to faulty implementation (see KSENROLL-6364)
+#  @wip
+#  @adam.campbell
+#  Scenario: Create from Catalog a Course Offering which is defined in the CLU as Joint
+#    When I remove a joint course offering from the reference data in order to enable this test
+#    And I create a joint course offering from catalog while creating a new course offering
+#    Then I can add activity offerings to the joint Course Offerings
+
+
--- features/step_definitions/joint_course_offering.rb
+++ features/step_definitions/joint_course_offering.rb
@@ -1,17 +1,80 @@
-When /^I create joint Course Offerings$/ do
-
-  # we need to update this once Mike commits his refactor later today
-  @course_offering = create CourseOffering, :term => "201201", :course => "BSCI181", :delivery_format => "Lecture", :grade_format => "Lecture"
-  @course_offering.create_ao :ao_code => "A"
-  puts @course_offering.course + ' created'
+When /^I create a new jointly defined Course Offering$/ do
+  @bsci_co = create CourseOffering, :term => "201201", :course => "BSCI181", :delivery_format => "Lecture", :grade_format => "Lecture"
 end
 
-And /^I can add activity offerings to the joint Course Offerings$/ do
+And /^I attempt to "(delete|delete and cancel)" a joint Course Offering$/ do |delete_flag|
+  @bsci_co.search_by_subjectcode
 
-  # first we build an ao, then we link it the co -- Mikes commit today will change this so wait and see
-  #@activity_offering = create ActivityOffering
-  #@activity_offering.save
-  @course_offering.create_ao :ao_code => "A"
+  $should_confirm_delete = false
+  if delete_flag == 'delete'
+    $should_confirm_delete = true
+  end
+
+  @bsci_co.delete_co :code_list => [@bsci_co.course], :should_confirm_delete=>$should_confirm_delete
+end
+
+Then /^the Course Offering is "(deleted|not deleted)"$/ do |delete_flag|
+  @bsci_co.search_by_coursecode
+
+  on ManageCourseOfferings do |page|
+    if delete_flag == 'deleted'
+      page.error_message_course_not_found.should be_present
+    else # "not deleted"
+      page.error_message_course_not_found.should_not be_present
+    end
+  end
+end
+
+
+##################################################################################################################
+# STEPS BELOW THIS LINE ARE NOT FINISHED -- THEY CANNOT BE BECAUSE THE APPLICATION
+# HAS A BUG WHICH PREVENTS THE TEST FROM COMPLETING SUCCESSFULLY
+# (see joint_course_offering.feature,
+# "Scenario: Create from Catalog a Course Offering which is defined in the CLU as Joint"
+# comment regarding "CCO 2.1")
+
+# This step is required because if (for example) CHEM181 already exists then when you create a new
+# linked CO (for example, BSCI181) the 'create new joint co'-link does not appear
+When /^I remove a joint course offering from the reference data in order to enable this test$/ do
+
+  # yes, we have to create 2 in order to delete all; sorry, caused by side-affect of
+  # having unreliable ref-data plus the app shows different views depending on whether
+  # the course-code has 1 or multiple offerings
+  @ref_data_to_delete = create CourseOffering, :term => "201201", :course => "CHEM181"
+  @ref_data_to_delete = create CourseOffering, :term => "201201", :course => "CHEM181"
+  @ref_data_to_delete.course = "CHEM181"
+  @ref_data_to_delete.search_by_coursecode
+  on ManageCourseOfferingList do |page|
+    page.select_all_cos
+    page.delete_cos
+  end
+  on DeleteCourseOffering do |page|
+    page.confirm_delete
+  end
+
+end
+
+# This step is required because after running the delete-ref-data step (above), the ref-data is
+# left in an unexpected state; this restores the state back to expected condition
+And /^then I restore the joint course offering I removed from the reference data$/ do
+
+end
+
+When /^I create a joint course offering from catalog while creating a new course offering$/ do
+  @bsci_co = create CourseOffering, :term => "201201", :course => "BSCI181", :joint_co_to_create => "CHEM181", :delivery_format => "Lecture", :grade_format => "Lecture"
+end
+
+Then /^I can add activity offerings to the joint Course Offerings$/ do
+  puts 'here we are'
+  sleep 10
+
+  @bsci_co.create_ao :ao_code => "A"
+
+  @chem_co = make CourseOffering, :term => "201201", :course => "CHEM181", :delivery_format => "Lecture", :grade_format => "Lecture"
+  @chem_co.create_ao :ao_code => "A"
+
+  puts 'done?'
+  sleep 60
 end
 
 #REFACTOR THIS STEP NAME TO SOMETHING BETTER
@@ -27,46 +90,3 @@ Then /^we can see the aos in the co we made$/ do
 
 end
 
-When /^I attempt to delete a joint Course Offering$/ do
-  pending # express the regexp above with the code you wish you had
-end
-
-Then /^I can cancel the deletion$/ do
-  pending # express the regexp above with the code you wish you had
-end
-
-Then /^I can complete the deletion$/ do
-  pending # express the regexp above with the code you wish you had
-end
-
-Then /^the Course Offering is deleted$/ do
-  pending # express the regexp above with the code you wish you had
-end
-
-###################################################################################################################################
-#STEPS BELOW THIS LINE ARE TESTS BY BRANDON -- JUST DELETE THEM!
-
-When /^I setup my test$/ do
-
-  @course_offering = make CourseOffering, :term=> Rollover::OPEN_SOC_TERM, :course => "BSCI181ANSNS"
-  @course_offering.manage
-
-  on ManageCourseOfferings do |page|
-    page.edit('B')
-  end
-
-  on ActivityOfferingMaintenance do |page|
-    page.select_colocated_checkbox
-    page.colocated_co_input_field.value = 'CHEM181ABCD'
-    page.colocated_ao_input_field.value = 'B'
-    page.click_colocated_add_button
-    page.colocated_shared_max_enrollment_input_field.value = '10'
-    page.submit
-  end
-
-  puts 'end of this test step'
-  sleep 60
-end
-
-#STEPS ABOVE THIS LINE ARE TESTS BY BRANDON -- JUST DELETE THEM!
-###################################################################################################################################
