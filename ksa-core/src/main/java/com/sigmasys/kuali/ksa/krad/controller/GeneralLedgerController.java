342d7310fdba1e36ede110a8ba32c19bfc639f1f
godunovs <godunovs@kuali.org>
2013-04-18 01:24:19 -0400

    Added Prior Batch loading and conversion to view model.
    git-svn-id: https://svn.kuali.org/repos/student/contrib/ksa/trunk@46615 0ac02d80-fb3f-11dc-a574-c57244a850eb

--- ksa-core/src/main/java/com/sigmasys/kuali/ksa/krad/controller/GeneralLedgerController.java
+++ ksa-core/src/main/java/com/sigmasys/kuali/ksa/krad/controller/GeneralLedgerController.java
@@ -1,7 +1,14 @@
 package com.sigmasys.kuali.ksa.krad.controller;
 
 import com.sigmasys.kuali.ksa.krad.form.ReportReconciliationForm;
+import com.sigmasys.kuali.ksa.krad.model.BatchTransmission;
+import com.sigmasys.kuali.ksa.krad.model.BatchTransmissionModel;
+import com.sigmasys.kuali.ksa.model.GeneralLedgerType;
+import com.sigmasys.kuali.ksa.model.GlTransmission;
 import com.sigmasys.kuali.ksa.service.GeneralLedgerService;
+import com.sigmasys.kuali.ksa.util.TransactionUtils;
+import org.apache.commons.lang.StringUtils;
+import org.apache.commons.lang.mutable.MutableDouble;
 import org.kuali.rice.krad.web.form.UifFormBase;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
@@ -12,6 +19,8 @@ import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.servlet.ModelAndView;
 
 import javax.servlet.http.HttpServletRequest;
+import java.math.BigDecimal;
+import java.util.*;
 
 /**
  * Created with IntelliJ IDEA.
@@ -56,11 +65,32 @@ public class GeneralLedgerController extends DownloadController {
      * @param form GeneralLedger form.
      * @return ModelAndView for the page.
      */
-    @RequestMapping(method = RequestMethod.POST, params = "methodToCall=searchForPriorBatches")
+    @RequestMapping(method = {RequestMethod.GET,RequestMethod.POST}, params = "methodToCall=searchForPriorBatches")
     public ModelAndView searchForPriorBatches(@ModelAttribute("KualiForm") ReportReconciliationForm form) {
-        // Find prior batches:
+        // Find GL Transmissions, create BatchTransmission Map by Batch ID:
+        List<GlTransmission> glTransmissions = generalLedgerService.getGlTransmissionsForExport();
+        MutableDouble totalAllBatches = new MutableDouble(0);
+        Map<String,BatchTransmission> batchTransmissionMap = createBatchTransmissionMap(glTransmissions, totalAllBatches);
+
+        // Create a list of BatchTransmissionModel objects:
+        List<BatchTransmissionModel> priorBatches = new ArrayList<BatchTransmissionModel>();
+
+        for (String batchId : batchTransmissionMap.keySet()) {
+            // Get a BatchTransmission and create a BatchTransmissionModel out of it:
+            BatchTransmission batchTransmission = batchTransmissionMap.get(batchId);
+            BatchTransmissionModel batchTransmissionModel = new BatchTransmissionModel();
+
+            batchTransmissionModel.setBatchId(batchId);
+            batchTransmissionModel.setTransmissionDate(batchTransmission.getDate());
+            batchTransmissionModel.setFormattedAmount(TransactionUtils.getFormattedAmount(batchTransmission.getTotalAmount()));
+
+            // Add to the list:
+            priorBatches.add(batchTransmissionModel);
+        }
 
         // Set objects on the form:
+        form.setPriorBatchTransmissions(priorBatches);
+        form.setAllPriorBatchTotal(TransactionUtils.getFormattedAmount(new BigDecimal(totalAllBatches.doubleValue())));
 
         return getUIFModelAndView(form);
     }
@@ -71,7 +101,7 @@ public class GeneralLedgerController extends DownloadController {
      * @param form GeneralLedger form.
      * @return ModelAndView for the page.
      */
-    @RequestMapping(method = RequestMethod.POST, params = "methodToCall=searchForPendingTransactions")
+    @RequestMapping(method = {RequestMethod.GET,RequestMethod.POST}, params = "methodToCall=searchForPendingTransactions")
     public ModelAndView searchForPendingTransactions(@ModelAttribute("KualiForm") ReportReconciliationForm form) {
         // Find pending transactions:
 
@@ -134,5 +164,60 @@ public class GeneralLedgerController extends DownloadController {
         // Retrieve a list of GL Accounts for a Batch:
 
         return null;
+    }
+
+    /////////////////////////////////////////////////////////////////////////////////////////////////////////////
+    //
+    // Helper Methods.
+    //
+    /////////////////////////////////////////////////////////////////////////////////////////////////////////////
+
+    /**
+     * Creates a Map of all Batches mapped to BatchTransmission objects that contain
+     * their related GL Account Types.
+     *
+     * @param glTransmissions   All GL Transmissions.
+     * @param totalAllBatches   The total for all batches. This is an OUT parameter.
+     * @return  a Map of all Batches IDs mapped to BatchTransmission objects.
+     */
+    private Map<String, BatchTransmission> createBatchTransmissionMap(List<GlTransmission> glTransmissions,
+                                                                      MutableDouble totalAllBatches) {
+        // Create the result Map:
+        Map<String, BatchTransmission> result = new HashMap<String, BatchTransmission>();
+
+        // Go through the list of GLTransmissions
+        for (GlTransmission glTransmission : glTransmissions) {
+            // Get the batch ID from the current transmission:
+            String batchId = glTransmission.getBatchId();
+
+            if (StringUtils.isNotEmpty(batchId)) {
+                // See if the current GlTransmission is already mapped to a BatchTransmission:
+                BatchTransmission batchTransmission = result.get(batchId);
+
+                // If not mapped yet, create a new BatchTransmission, set attributes and map it:
+                if (batchTransmission == null) {
+                    batchTransmission = new BatchTransmission();
+                    batchTransmission.setBatchId(batchId);
+                    batchTransmission.setDate(glTransmission.getDate());
+                    batchTransmission.setGlAccounts(new ArrayList<GeneralLedgerType>());
+                    batchTransmission.setTotalAmount(new BigDecimal(0));
+                    result.put(batchId, batchTransmission);
+                }
+
+                // Add the current transmission's amount to the total:
+                if (glTransmission.getAmount() != null) {
+                    BigDecimal transmissionAmount = glTransmission.getAmount();
+                    BigDecimal newTotal = batchTransmission.getTotalAmount().add(transmissionAmount);
+
+                    batchTransmission.setTotalAmount(newTotal);
+                    totalAllBatches.add(transmissionAmount);
+                }
+
+                // TODO:Add GL Account to the list of all accounts in the batch:
+                // Need to figure out how to link GL Accounts to GL Transmissions.
+            }
+        }
+
+        return result;
     }
 }
