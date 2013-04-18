cf9e928e7eed258b0ae9ffcce4ee89da989a977e
brandon.gresham <brandon.gresham@kuali.org>
2013-04-18 15:52:32 -0400

    KSENROLL-4593 2nd-Refactor of Feature-file based on feedback; added scenario for breaking colo; added in dummy-data to speed development of remainder of AFT.
    git-svn-id: https://svn.kuali.org/repos/student/test/functional-automation/sambal/trunk@46658 0ac02d80-fb3f-11dc-a574-c57244a850eb

--- features/colocated_activity_offerings.feature
+++ features/colocated_activity_offerings.feature
@@ -4,16 +4,17 @@ Feature: Co-location of Activity Offerings
 
 Background:
   Given I am logged in as a Schedule Coordinator
-  And I create "3" COs with an AO in each
+#  And I create "3" COs with an AO in each
+When I create some dummy test data to speed up AFT development
 
   @draft
   @brandon.gresham
   Scenario Outline: Colocate Activity Offerings
-    When I colocate multiple activities, selecting to "<manage>" enrollments
+    When I colocate multiple activities, selecting to "<Manage>" enrollments
     # And I (deselect|select) all colocated AOs <-- this step cannot be developed yet due to partial-colocation not being implemented in the app
     Then the activities indicate they are colocated
     Examples:
-      |     manage        |
+      | Manage            |
       | share             |
       | separately manage |
 
@@ -26,11 +27,16 @@ Background:
   @wip
   @brandon.gresham
   Scenario Outline: Break an AOs colocation relationship
-    When I colocate multiple activities, selecting to "<manage>" enrollments
+    When I colocate multiple activities, selecting to "<Manage>" enrollments
+    And I break colocation on the first colocated AO, "<Remanaging>" max-enrollment
+    Then the first colocated AO is not colocated with the remaining AO(s)
+
     Examples:
-      |     manage        |
-      | share             |
-      | separately manage |
+    | Manage            | Remanaging              |
+    | share             | supplying new           |
+    | separately manage | acknowledging retained  |
+
+
 
 
 
--- features/step_definitions/colocated_activity_offerings_steps.rb
+++ features/step_definitions/colocated_activity_offerings_steps.rb
@@ -2,7 +2,6 @@ When /^I create "([2-9])" COs with an AO in each$/ do |number_of_cos_to_create|
   reference_co_to_copy = make CourseOffering
   @array_of_cos = []
   for i in 1..number_of_cos_to_create.to_i
-    puts "create " + i.to_s
     co = create CourseOffering, :create_by_copy=>reference_co_to_copy
     co.create_ao :ao_code => "A"
     @array_of_cos << co
@@ -24,6 +23,10 @@ When /^I colocate multiple activities, selecting to "(share|separately manage)" 
   for i in 1..@array_of_cos.size
     remaining_cos << @array_of_cos[i]
   end
+
+  puts 'here'
+  sleep 10
+  #pending
 
   # this is broke right now; waiting for fix from Mike W. to come in
   first_co.colocate :cos_to_colo => remaining_cos,
@@ -56,6 +59,34 @@ Then /^the activities indicate they are colocated$/ do
 
 end
 
+When /^I break colocation on the first colocated AO, "(supplying new|acknowledging retained)" max-enrollment$/ do |max_enrollment_flag|
+  #pending
+
+  # MFT has these steps which map to this step:
+  #   1. On Manage Course Offering page, enter code for term, and course code: CHEM131
+  #   2. Click the manage button of CO131B
+  #   3. On manage CO131B page, click on the edit button to edit Activity Offering: A of CHEM131B
+  #   4. On edit AO page, uncheck This activity is co-located checkbox
+  #   5. A dialog to confirm this action is displayed.
+  #   6. On the dialog, select to confirm to leave the colocation set
+  #   7. supply max-enrollment (if max-enrollment is being shared)
+  #       or that max-enrollment was retained (if max-enrollment is being separately managed)
+  #   8. Click Submit to submit the changes for this AO.
+
+end
+
+Then /^the first colocated AO is not colocated with the remaining AO\(s\)$/ do
+  #pending
+
+  # MFT has these steps which map to this step:
+  #   1. Both Activity Offerings: A of CHEM131A and A of CHEM131B no longer have colocated relationship.
+  #   2. Activity Offering: A of CHEM131A will have the RDLs in the colocation set which will be displayed in the edit AO: A of CHEM131A page
+  #   3. Activity Offering: A of CHEM131B has no RDL left.
+  #   4. to include validation of the CO-icons:
+  #       the ones that stayed in the colo should still contain CO-icons (with appropriate tooltip-language);
+  #       the one that broke away from the colo should no longer have a CO-icon
+end
+
 When(/^I designate a valid term and Course Offering Code with a fully colocated AO$/) do
   @course_offering = make CourseOffering, :course=>"CHEM441", :term=>"201208"
   @course_offering.manage
@@ -74,4 +105,27 @@ Then(/^The AO is successfully deleted$/) do
   rescue RuntimeError => e
     e.message.should include "error in activity_offering_results_table - no AOs"
   end
+end
+
+
+### CREATE DUMMY DATA
+When /^I create some dummy test data to speed up AFT development$/ do
+
+  #pending
+
+  # populate an array with a bunch of COs that already exist in the DB
+  @array_of_cos = []
+
+  # 201201 ENGL211 COLOTEST-1 AO:A
+  co = make CourseOffering, :term => '201201', :course => 'ENGL211COLOTEST-1'
+  @array_of_cos << co
+
+  # 201201 ENGL211 COLOTEST-2 AO:A
+  co = make CourseOffering, :term => '201201', :course => 'ENGL211COLOTEST-2'
+  @array_of_cos << co
+
+  # 201201 ENGL211 COLOTEST-3 AO:A
+  co = make CourseOffering, :term => '201201', :course => 'ENGL211COLOTEST-3'
+  @array_of_cos << co
+
 end
