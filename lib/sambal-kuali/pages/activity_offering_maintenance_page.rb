a94fe5981f0588b08956b1855c202fe37f4726fb
brandon.gresham <brandon.gresham@kuali.org>
2013-04-23 09:39:14 -0400

    KSENROLL-4593 "Loading..."-message was replaced by "Adding Line..."
    git-svn-id: https://svn.kuali.org/repos/student/test/functional-automation/sambal/trunk@46839 0ac02d80-fb3f-11dc-a574-c57244a850eb

--- lib/sambal-kuali/pages/activity_offering_maintenance_page.rb
+++ lib/sambal-kuali/pages/activity_offering_maintenance_page.rb
@@ -15,7 +15,7 @@ class ActivityOfferingMaintenance < ActivityOfferingMaintenanceBase
   element(:colocated_co_input_field) { |b| b.frm.text_field(id: "ActivityOfferingEdit-CoLocatedActivities-CoInputField_add_control") }
   element(:colocated_ao_input_field) { |b| b.frm.text_field(id: "ActivityOfferingEdit-CoLocatedActivities-AoInputField_add_control") }
   element(:colocated_add_button) { |b| b.frm.button(id: "ActivityOfferingEdit-CoLocatedActivities_add") }
-  action(:add_colocated) { |b| b.colocated_add_button.click(); b.loading.wait_while_present; b.colocated_co_input_field.wait_until_present; b.colocated_ao_input_field.wait_until_present; b.colocated_add_button.wait_until_present }
+  action(:add_colocated) { |b| b.colocated_add_button.click(); b.adding.wait_while_present; b.colocated_co_input_field.wait_until_present; b.colocated_ao_input_field.wait_until_present; b.colocated_add_button.wait_until_present; }
   element(:jointly_share_enrollment_radio) { |b| b.radio(id: "share_seats_control_0") }
   action(:select_jointly_share_enrollment_radio) { |b| b.jointly_share_enrollment_radio.set }
   element(:separately_manage_enrollment_radio) { |b| b.radio(id: "share_seats_control_1") }
