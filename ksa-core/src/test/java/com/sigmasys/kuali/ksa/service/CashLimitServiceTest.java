a5c16951f4ce948ace7939b8b4a2d1c5d132ce79
mivanov <mivanov@kuali.org>
2013-04-22 21:51:10 -0400

    Removed @Rollback annotation from CashLimitServiceTest class
    git-svn-id: https://svn.kuali.org/repos/student/contrib/ksa/trunk@46814 0ac02d80-fb3f-11dc-a574-c57244a850eb

--- ksa-core/src/test/java/com/sigmasys/kuali/ksa/service/CashLimitServiceTest.java
+++ ksa-core/src/test/java/com/sigmasys/kuali/ksa/service/CashLimitServiceTest.java
@@ -9,7 +9,6 @@ import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.test.annotation.Rollback;
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 import org.springframework.util.Assert;
@@ -46,7 +45,6 @@ public class CashLimitServiceTest extends AbstractServiceTest {
     }
 
     @Test
-    @Rollback(false)
     public void checkCashLimit() throws Exception {
 
         Tag cashTag = entityService.getAuditableEntity("Cash", Tag.class);
