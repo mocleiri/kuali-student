691cea14a6a58924a5794e543a8d5348546a56e7
brandon.gresham <brandon.gresham@kuali.org>
2013-05-06 18:16:17 -0400

    KSENROLL-6538 Finished cleaning-up code and completing steps.
    git-svn-id: https://svn.kuali.org/repos/student/test/functional-automation/sambal/trunk@47576 0ac02d80-fb3f-11dc-a574-c57244a850eb

---
+++ features/add_activity_offerings.feature
@@ -0,0 +1,24 @@
+@nightly
+Feature: Add Activity Offerings to existing Course Offerings
+
+  As a Departmental administrator, I want to be able to add additional Activity Offerings either by copying or creating
+  new Activity Offerings so that the Course Offering will have additional associated Activity Offerings for the term.
+
+  Background:
+    Given I am logged in as a Schedule Coordinator
+
+
+  Scenario: Add new Activity Offerings
+    When I add an Activity Offering to an existing Course Offering
+    Then the new Activity Offering is shown in the list of AOs
+
+
+  Scenario: Copy Activity Offerings
+    When I copy an AO with "Actual" Delivery Logistics
+    Then the "ADLs" are successfully copied as RDLs in the new AO
+    And I copy an AO with "Requested" Delivery Logistics
+    Then the "RDLs" are successfully copied as RDLs in the new AO
+
+
+
+
--- features/step_definitions/add_activity_offerings_steps.rb
+++ features/step_definitions/add_activity_offerings_steps.rb
@@ -1,34 +1,83 @@
-When /^I manage an existing Course Offering in "(.*?)" view$/ do |view|
-  if view == 'Subject Code'
-    @course_offering = create CourseOffering, :create_by_copy => (make CourseOffering, :course=>"CHEM277", :search_by_subj => true)
-  else
-    @course_offering = create CourseOffering, :create_by_copy => (make CourseOffering, :course=>"CHEM277")
+When /^I add an Activity Offering to an existing Course Offering$/ do
+  course_offering = make CourseOffering, :course=>"CHEM277"
+  course_offering.manage_and_init
+  @new_ao = course_offering.create_ao(make ActivityOffering, :format => "Lecture/Lab")
+end
+
+
+Then /^the new Activity Offering is shown in the list of AOs$/ do
+  on(ManageCourseOfferings).codes_list.should include(@new_ao.code)
+end
+
+
+When /^I copy an AO with "(Requested|Actual)" Delivery Logistics$/ do |delivery_logistics|
+
+  # in ref-data this CO has ADLs but no RDLs (copy an ADL first if you need an RDL)
+  course_offering = make CourseOffering, :course => "CHEM277"
+  course_offering.manage_and_init
+
+  # get AOs that match the desired DL-type
+  all_aos = course_offering.activity_offering_cluster_list[0].ao_list
+  target_status = "Draft"
+  if delivery_logistics == "Actual"
+    target_status = "Offered"
   end
-
-  @course_offering.manage_and_init
-  @total_number = @course_offering.activity_offering_cluster_list[0].ao_list.count
-end
-
-Then /^I am able to add an Activity Offering$/ do
-  @course_offering.create_ao(make ActivityOffering, :format => "Lecture/Lab")
-end
-
-Then /^I am able to copy an Activity Offering$/ do
-  on ManageCourseOfferings do |page|
-    if @total_number > 0
-      ao_code = page.codes_list[0]
-      page.copy(ao_code)
+  target_aos = course_offering.get_aos_by_status :aos => all_aos, :ao_status => target_status
+  if target_aos.empty?
+    case target_status
+      when "Offered"
+        raise "CO has no ADLs"
+      else
+        raise "CO has no RDLs"
     end
   end
+
+  # copy the first AO that matches the desired DL-type
+  @ao_source = target_aos[0]
+  @ao_copy = create ActivityOffering, :create_by_copy => true,
+                    :code => @ao_source.code,
+                    :parent_course_offering => course_offering
 end
 
-And /^verify the new Activity Offering appears on the list$/ do
-  @course_offering.manage
-  on ManageCourseOfferings do |page|
-    new_total = page.codes_list.count
-    new_total.should == @total_number + 1
+
+Then /^the "(ADL|RDL)s" are successfully copied as RDLs in the new AO$/ do |source_delivery_logistics_type|
+
+  # capture sched for source-AO
+  ao_source_schedule = []
+  @ao_source.parent_course_offering.manage
+  @ao_source.edit
+  on ActivityOfferingMaintenance do |page|
+    if source_delivery_logistics_type == "ADL"
+      ao_source_schedule = page.get_actual_delivery_logistics_data_as_array_of_sets
+    else
+      ao_source_schedule = page.get_requested_delivery_logistics_data_as_array_of_sets
+    end
   end
+
+  # capture sched for copy-AO
+  ao_copy_schedule = []
+  @ao_copy.parent_course_offering.manage
+  @ao_copy.edit
+  on ActivityOfferingMaintenance do |page|
+    ao_copy_schedule = page.get_requested_delivery_logistics_data_as_array_of_sets
+  end
+
+  # verify that both AOs have same DL-*details* (validation of DL-*type* is handled in the capture-blocks just prior to this)
+  diff = ao_source_schedule - ao_copy_schedule
+  diff.should be_empty
+
 end
+
+
+### CREATE DUMMY DATA
+### This data should already exist in the DB, having been put in there manually
+#When /^I create some dummy test data to speed up AFT development for activity offerings$/ do
+#  course_offering = make CourseOffering, :course => "CHEM277"
+#  course_offering.manage_and_init
+
+#  @ao_source = course_offering.activity_offering_cluster_list[0].get_ao_obj_by_code("D")
+#  @ao_copy = course_offering.activity_offering_cluster_list[0].get_ao_obj_by_code("E")
+#end
 
 
 
