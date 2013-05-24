3419a35d7f4e93a6df68dd20c66b83307592d824
mike.wyers <mike.wyers@kuali.org>
2013-05-24 09:59:45 -0400

    KSENROLL-7137 - fix logout link for distributed env AFTs
    git-svn-id: https://svn.kuali.org/repos/student/test/functional-automation/sambal/trunk@48725 0ac02d80-fb3f-11dc-a574-c57244a850eb

--- lib/sambal-kuali/pages/lum_main_page.rb
+++ lib/sambal-kuali/pages/lum_main_page.rb
@@ -12,7 +12,7 @@ class LUMMainPage < PageFactory
   element(:username_field) { |b| b.text_field(:name=>"j_username") }
   element(:password_field) { |b| b.text_field(:name=>"j_password") }
   element(:login_button) { |b| b.button(:value=>"Login") }
-  action(:logout) { |b| b.button(value: "Logout").click }
+  action(:logout) { |b| b.link(text: "Logout").click }
 
   def login_with username, password
     username_field.set username
