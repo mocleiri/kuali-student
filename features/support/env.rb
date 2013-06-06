9d1a45dbbefe918660704986fb29255aa8fc8a67
mike.wyers <mike.wyers@kuali.org>
2013-06-06 16:33:46 -0400

    git-svn-id: https://svn.kuali.org/repos/student/test/functional-automation/sambal/trunk@49364 0ac02d80-fb3f-11dc-a574-c57244a850eb

--- features/support/env.rb
+++ features/support/env.rb
@@ -1,7 +1,8 @@
-#$test_site = "http://localhost:8081/ks-with-rice-bundled-dev" #Local Env
-$test_site = "http://env2.ks.kuali.org"
+$test_site = "http://localhost:8081/ks-with-rice-bundled-dev" #Local Env
+#$test_site = "http://env2.ks.kuali.org"
 $test_site = ENV['TEST_SITE'] unless ENV['TEST_SITE'] == nil
 $distributed_env = ENV['DISTRIBUTED_ENV']
+#$distributed_env = true
 
 $: << File.dirname(__FILE__)+'/../../lib'
 
@@ -14,9 +15,27 @@ World DateFactory
 World Workflows
 
 client = Selenium::WebDriver::Remote::Http::Default.new
+browser = nil
+begin
+browser = Watir::Browser.new :firefox, :http_client => client
+rescue
+browser = nil
+end
+
+retry_ctr = 0
+while browser.nil? and retry_ctr < 100
+  sleep 5
+  retry_ctr = retry_ctr + 1
+  puts "debug retrying browser init - retry #{retry_ctr}"
+  begin
+    browser = Watir::Browser.new :firefox, :http_client => client
+  rescue
+    browser = nil
+  end
+end
 #client.timeout = 15 # seconds default is 60
 
-browser = nil
+#browser = nil
 headless = nil
 
 if ENV['HEADLESS']
@@ -34,7 +53,7 @@ Before do
   @browser = browser
 end
 
-at_exit { browser.close unless browser == nil }
+#at_exit { browser.close unless browser == nil }
 
 
 if ENV['HEADLESS']
