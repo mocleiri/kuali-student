8122ad52c445095acd43ffddc39a785bbff11ada
mike.wyers <mike.wyers@kuali.org>
2013-06-11 13:43:29 -0400

    KSENROLL-7345 AFTs: add code to test browser connection before starting tests (avoid nil browser error problem)
    git-svn-id: https://svn.kuali.org/repos/student/test/functional-automation/sambal/trunk@49595 0ac02d80-fb3f-11dc-a574-c57244a850eb

--- features/support/env.rb
+++ features/support/env.rb
@@ -23,25 +23,47 @@ if ENV['HEADLESS']
   require 'headless'
   headless = Headless.new :destroy_at_exit => false
   headless.start
+
+  #to avoid nil browser error, retry initial browser connect in headless env
+  retry_ctr = 0
+  while browser.nil? and retry_ctr < 10
+    retry_ctr += 1
+    puts "debug env.rb - browser init - attempt #{retry_ctr}"
+    begin
+      browser = Watir::Browser.new :firefox, :http_client => client
+      browser.goto("#{$test_site}/login.jsp")
+      sleep 2
+      raise "connect failed" unless  browser.text_field(id: "j_username").exists?
+      browser.close
+      puts "debug env.rb - success: browser connection attempt #{retry_ctr}"
+    rescue
+      puts "debug env.rb - connect failed"
+      browser.close unless browser.nil?
+      browser = nil
+      sleep 30
+    end
+  end
 end
 
+#re-use browser for each scenario
 Before do
   if browser == nil
-    puts "debug -- creating new browser"
+    puts "debug  env.rb - creating new browser"
     browser = Watir::Browser.new :firefox, :http_client => client
-    puts "debug -- browser.nil? #{browser.nil?}"
+    puts "debug  env.rb - browser.nil? #{browser.nil?}"
   end
   @browser = browser
 end
 
 at_exit { browser.close unless browser == nil }
 
-
 if ENV['HEADLESS']
+  # commented out to allow parallel execution
   #at_exit do
   #  headless.destroy
   #end
 
+  #re-start browser after each failed scenario
   After do | scenario |
     if scenario.failed?
       @browser.close unless @browser == nil
