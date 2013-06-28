1f9be7529db4f075d63ffe2802d858cd04852336
mike.wyers <mike.wyers@kuali.org>
2013-06-27 20:34:51 -0400

    KSENROLL-7349 AFTs work in progress for multi ACAL tests -- finish updating the AZ Milestones tests with the new AcademicTerm data object methods
    git-svn-id: https://svn.kuali.org/repos/student/test/functional-automation/sambal/trunk@50567 0ac02d80-fb3f-11dc-a574-c57244a850eb

--- features/step_definitions/az_milestones_steps.rb
+++ features/step_definitions/az_milestones_steps.rb
@@ -1,22 +1,13 @@
 Given /^It is "After" the first day of classes$/ do
-  @term = make AcademicTerm, :term_name=>Rollover::PUBLISHED_MILESTONES_SOC_TERM_NAME
-  @term.search
-  on CalendarSearch do |page|
-    page.edit @term.term_name
-  end
+  step "I am logged in as a Schedule Coordinator"
+  @term = make AcademicTerm, :term_name=> Rollover::PUBLISHED_MILESTONES_SOC_TERM_NAME, :term_type=>AcademicTerm::WINTER_TERM_TYPE
+  @term.edit
 
-  on EditAcademicTerms do |page|
-    @tindex = page.get_term_index(@term.term_name)
+  @keydategroup = make KeyDateGroup, :key_date_group_type=> "Instructional", :term_index=> @tindex, :term_type=> @term.term_type
+  @keydate = make KeyDate, :parent_key_date_group => @keydategroup, :key_date_type => "First Day of Classes", :start_date => (Date.today - 1).strftime("%m/%d/%Y"), :end_date=>(Date.today - 1).strftime("%m/%d/%Y"), :term_index=> @tindex
+  @keydategroup.key_dates = Array.new(1){@keydate}
+  @keydategroup.create
 
-    if page.key_date_exist(@tindex,0,0) and page.key_date_type(@tindex,0,0) == "First Day of Classes"
-      @keydate = make KeyDate
-      @keydate.edit  :key_date_type => "First Day of Classes", :start_date => (Date.today - 1).strftime("%m/%d/%Y"),  :term_index=> @tindex
-    else
-      @keydate = make KeyDate, :key_date_type => "First Day of Classes", :start_date => (Date.today - 1).strftime("%m/%d/%Y"), :end_date=>(Date.today - 1).strftime("%m/%d/%Y"), :term_index=> @tindex
-      @keydategroup = make KeyDateGroup, :key_dates=>Array.new(1){@keydate}, :term_index=> @tindex
-      @keydategroup.create
-    end
-  end
 end
 
 Given /^It is "Before" the first day of classes and "Before" the first day to add classes/ do
@@ -33,39 +24,23 @@ Given /^It is "Before" the first day of classes and "Before" the first day to ad
   @keydate2 = make KeyDate, :parent_key_date_group => @keydategroup, :key_date_type => "Registration Period 1", :start_date => (Date.today + 1).strftime("%m/%d/%Y"), :end_date=>(Date.today + 4).strftime("%m/%d/%Y"), :term_index=> @tindex, :date_range => true
   @keydategroup.key_dates = Array.new(1){@keydate2}
   @keydategroup.create
-    #end
-  #end
-  step "I am logged in as a Department Schedule Coordinator"
+  #step "I am logged in as a Department Schedule Coordinator"
 end
 
 Given /^It is "Before" the first day of classes and "After" the first day to add classes/ do
-  @term = make AcademicTerm, :term_name=>Rollover::PUBLISHED_MILESTONES_SOC_TERM_NAME
-  @term.search
-  on CalendarSearch do |page|
-    page.edit @term.term_name
-  end
-  on EditAcademicTerms do |page|
+  step "I am logged in as a Schedule Coordinator"
+  @term = make AcademicTerm, :term_name=> Rollover::PUBLISHED_MILESTONES_SOC_TERM_NAME, :term_type=>AcademicTerm::WINTER_TERM_TYPE
+  @term.edit
 
-    @tindex = page.get_term_index(@term.term_name)
+  @keydategroup = make KeyDateGroup, :key_date_group_type=> "Instructional", :term_index=> @tindex, :term_type=> @term.term_type
+  @keydate = make KeyDate, :parent_key_date_group => @keydategroup, :key_date_type => "First Day of Classes", :start_date => (Date.today + 5).strftime("%m/%d/%Y"), :end_date=>(Date.today + 5).strftime("%m/%d/%Y"), :term_index=> @tindex
+  @keydategroup.key_dates = Array.new(1){@keydate}
+  @keydategroup.create
 
-    if page.key_date_exist(@tindex,0,0) and page.key_date_type(@tindex,0,0) == "First Day of Classes"
-      @keydate = make KeyDate
-      @keydate.edit :key_date_type => "First Day of Classes", :start_date => (Date.today + 1).strftime("%m/%d/%Y"), :term_index=> @tindex
-    else
-      @keydate = make KeyDate, :key_date_type => "First Day of Classes", :start_date => (Date.today + 5).strftime("%m/%d/%Y"), :end_date=>(Date.today + 5).strftime("%m/%d/%Y"), :term_index=> @tindex
-      @keydategroup = make KeyDateGroup, :key_dates=> Array.new(1){@keydate}, :term_index=> @tindex
-      @keydategroup.create
-    end
-
-    if page.key_date_exist(@tindex,1,0) and page.key_date_type(@tindex,1,0) == "Registration Period 1"
-      @keydate2 = make KeyDate
-      @keydate2.edit :key_date_type => "Registration Period 1", :start_date => (Date.today - 4).strftime("%m/%d/%Y"), :end_date=>(Date.today +1).strftime("%m/%d/%Y"), :term_index=> @tindex, :key_date_group=> 1
-    else
-      @keydate2 = make KeyDate, :key_date_type => "Registration Period 1", :start_date => (Date.today - 4).strftime("%m/%d/%Y"), :end_date=>(Date.today +1).strftime("%m/%d/%Y"), :term_index=> @tindex
-      @keydategroup = make KeyDateGroup,:key_date_group_type=> "Registration", :key_dates=>Array.new(1){@keydate2}, :term_index=> @tindex
-      @keydategroup.create
-    end
-  end
+  @keydategroup = make KeyDateGroup,:key_date_group_type=> "Registration", :term_index=> @tindex, :term_type=> @term.term_type
+  @keydate2 = make KeyDate, :parent_key_date_group => @keydategroup, :key_date_type => "Registration Period 1", :start_date => (Date.today - 4).strftime("%m/%d/%Y"), :end_date=>(Date.today +1).strftime("%m/%d/%Y"), :term_index=> @tindex
+  @keydategroup.key_dates = Array.new(1){@keydate2}
+  @keydategroup.create
 end
 
 When /^I do not have access to copy an activity offering$/ do
@@ -81,25 +56,15 @@ Then /^I edit an activity offering in my department$/ do
 end
 
 Given /^It is "After" the first day to add classes$/ do
-  @term = make AcademicTerm, :term_name=>Rollover::PUBLISHED_MILESTONES_SOC_TERM_NAME
-  @term.search
-  on CalendarSearch do |page|
-    page.edit @term.term_name
-  end
+  step "I am logged in as a Schedule Coordinator"
+  @term = make AcademicTerm, :term_name=> Rollover::PUBLISHED_MILESTONES_SOC_TERM_NAME, :term_type=>AcademicTerm::WINTER_TERM_TYPE
+  @term.edit
 
-  on EditAcademicTerms do |page|
+  @keydategroup = make KeyDateGroup, :key_date_group_type=> "Instructional", :term_index=> @tindex, :term_type=> @term.term_type
+  @keydate = make KeyDate, :parent_key_date_group => @keydategroup, :key_date_type => "First Day of Classes", :start_date => (Date.today - 1).strftime("%m/%d/%Y"), :end_date=>(Date.today - 1).strftime("%m/%d/%Y"), :term_index=> @tindex
+  @keydategroup.key_dates = Array.new(1){@keydate}
+  @keydategroup.create
 
-    @tindex = page.get_term_index(@term.term_name)
-
-    if page.key_date_exist(@tindex,0,0) and page.key_date_type(@tindex,0,0) == "First Day of Classes"
-      @keydate = make KeyDate
-      @keydate.edit  :key_date_type => "First Day of Classes", :start_date => (Date.today - 1).strftime("%m/%d/%Y")
-    else
-      @keydate = make KeyDate, :key_date_type => "First Day of Classes", :start_date => (Date.today - 1).strftime("%m/%d/%Y"), :end_date=>(Date.today - 1).strftime("%m/%d/%Y")
-      @keydategroup = make KeyDateGroup, :key_dates=>Array.new(1){@keydate}
-      @keydategroup.create
-    end
-  end
 end
 
 When /^I do not have access to edit maximum enrollment$/ do
--- lib/sambal-kuali/pages/acal/edit_academic_terms_page.rb
+++ lib/sambal-kuali/pages/acal/edit_academic_terms_page.rb
@@ -18,11 +18,11 @@ class EditAcademicTerms < BasePage
   action(:get_term_index) { |term_name, b| b.acal_term_list_div.text_field(value: "#{term_name}").name[/\d+/]}
 
   def term_index_by_term_type(term_type)
-    acal_term_list_div.link(text: /\b#{term_type}\b/).id[/\d+(?=_toggle)/]
+    acal_term_list_div.link(text: /^#{term_type}$/).id[/\d+(?=_toggle)/]
   end
 
   def open_term_section(term_type)
-    link =  acal_term_list_div.link(text: /\b#{term_type}\b/)
+    link =  acal_term_list_div.link(text: "#{term_type}")
     if link.image.attribute_value("alt") == "collapse" then # expand means is already expanded
       link.click
     end
