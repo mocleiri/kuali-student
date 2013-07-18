6f337e358cb2cabe7181f3665df0d1c7d8268eec
mike.wyers <mike.wyers@kuali.org>
2013-07-18 08:41:52 -0400

    KSENROLL-2860 KSENROLL-3022
    revert change to env.rb
    
    git-svn-id: https://svn.kuali.org/repos/student/test/functional-automation/sambal/trunk@52030 0ac02d80-fb3f-11dc-a574-c57244a850eb

--- features/support/env.rb
+++ features/support/env.rb
@@ -1,5 +1,5 @@
-$test_site = "http://localhost:8081/ks-with-rice-bundled-dev" #Local Env
-#$test_site = "http://env2.ks.kuali.org"
+#$test_site = "http://localhost:8081/ks-with-rice-bundled-dev" #Local Env
+$test_site = "http://env2.ks.kuali.org"
 $test_site = ENV['TEST_SITE'] unless ENV['TEST_SITE'] == nil
 $distributed_env = ENV['DISTRIBUTED_ENV']
 
@@ -56,7 +56,7 @@ Before do
   @browser = browser
 end
 
-at_exit { browser.close unless browser == nil }
+#at_exit { browser.close unless browser == nil }
 
 if ENV['HEADLESS']
   # commented out to allow parallel execution
