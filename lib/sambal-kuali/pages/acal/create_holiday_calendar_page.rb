1bff6026d034306af8b785ba8fcd005da9946a1a
mike.wyers <mike.wyers@kuali.org>
2013-07-18 10:53:26 -0400

    KSENROLL-8177 AFTs updates for dev work on HCAL
    git-svn-id: https://svn.kuali.org/repos/student/test/functional-automation/sambal/trunk@52042 0ac02d80-fb3f-11dc-a574-c57244a850eb

--- lib/sambal-kuali/pages/acal/create_holiday_calendar_page.rb
+++ lib/sambal-kuali/pages/acal/create_holiday_calendar_page.rb
@@ -5,14 +5,11 @@ class CreateHolidayCalendar < HolidayBase
   wrapper_elements
   frame_element
 
-  expected_element :start_blank_calendar_link
+  expected_element :calendar_name
 
   element(:calendar_name) { |b| b.frm.div(data_label: "Holiday Calendar Name").text_field }
   element(:start_date) { |b| b.frm.div(data_label: "Start Date").text_field }
   element(:end_date) { |b| b.frm.div(data_label: "End Date").text_field }
   element(:holiday_table) { |b| b.frm.div(id: "KS-HolidayCalendar-HolidaySection").table(class: "uif-tableCollectionLayout") }
-  element(:start_blank_calendar_link) { |b| b.frm.link(text: "Create Blank Holiday Calendar")}
-
-  action(:start_blank_calendar) { |b| b.start_blank_calendar_link.click; b.loading.wait_while_present }
 
 end
