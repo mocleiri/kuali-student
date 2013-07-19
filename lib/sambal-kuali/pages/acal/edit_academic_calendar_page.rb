01273b4fe9be09a2383c6e1b5f84222ed2543c47
jbergste <jbergste@kuali.org>
2013-07-19 11:48:53 -0400

    KSENROLL-8228  Change sticky footer div class name.
    git-svn-id: https://svn.kuali.org/repos/student/test/functional-automation/sambal/trunk@52127 0ac02d80-fb3f-11dc-a574-c57244a850eb

--- lib/sambal-kuali/pages/acal/edit_academic_calendar_page.rb
+++ lib/sambal-kuali/pages/acal/edit_academic_calendar_page.rb
@@ -41,7 +41,7 @@ class EditAcademicCalendar < BasePage
   action(:make_official) { |b| b.make_official_link.click; b.loading.wait_while_present }
 
 
-  element(:sticky_footer_div) { |b| b.frm.div(class: "uif-footer uif-stickyFooter uif-stickyButtonFooter") } # Persistent ID needed!
+  element(:sticky_footer_div) { |b| b.frm.div(class: "ks-uif-footer uif-stickyFooter uif-stickyButtonFooter") } # Persistent ID needed!
 
   action(:save) { |b| b.sticky_footer_div.button(text: "Save").click; b.loading.wait_while_present } # Persistent ID needed!
   action(:delete_draft) { |b| b.sticky_footer_div.link(text: "Delete Calendar Draft").click; b.loading.wait_while_present } # Persistent ID needed!
