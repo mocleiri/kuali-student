ace1b227aee065b3958c396c045a7690e05e5405
mike.wyers <mike.wyers@kuali.org>
2013-07-23 15:32:32 -0400

    KSENROLL-8057 KSENROLL-8052 KSENROLL-8145 AFTs update/fix failing scenarios after redesign of Create CO screens -- add new page files
    git-svn-id: https://svn.kuali.org/repos/student/test/functional-automation/sambal/trunk@52422 0ac02d80-fb3f-11dc-a574-c57244a850eb

---
+++ lib/sambal-kuali/pages/course_offering/create_co_from_catalog_page.rb
@@ -0,0 +1,93 @@
+class CreateCOFromCatalog < BasePage
+
+  #expected_title /Kuali :: Create Course Offering/
+
+  wrapper_elements
+  frame_element
+  validation_elements
+
+  expected_element :suffix
+
+  value(:target_term) { |b| b.frm.div(data_label: "Term").span.text }
+  value(:course_code) { |b| b.frm.div(data_label: "Course Code").span.text }
+
+  element(:suffix) { |b| b.frm.div(data_label: "Course Number Suffix").text_field() }
+
+  element(:grading_reg_opts_div) { |b| b.frm.div(id: "KS-GradingAndRegOptions") }
+  element(:grading_letter) { |b| b.grading_reg_opts_div.radio(text: "Letter") }
+  element(:pass_fail_checkbox) { |b| b.grading_reg_opts_div.checkbox(name: "document.newMaintainableObject.dataObject.passFailStudentRegOpts") }
+  element(:credit_type_fixed) { |b| b.div(id: "KS-CourseOfferingEdit-CreditType_OptionTypeSelector").radio(text: "Fixed") }
+  element(:credit_type_fixed_select ) { |b| b.select(id: "KS-CourseOfferingEdit-CreditType_OptionTypeFixed_control") }
+
+  element(:delivery_assessment_opt_div) { |b| b.frm.div(id: "KS-CourseOfferingEdit-DeliveryAndAssessment") }
+  element(:final_exam_option_div) { |b| b.frm.div(id: "finalExamType") }
+  action(:final_exam_option_standard) { |b| b.frm.radio(value: "STANDARD").set; b.loading.wait_while_present}
+  action(:final_exam_option_alternate) { |b| b.frm.radio(value: "ALTERNATE").set; b.loading.wait_while_present }
+  action(:final_exam_option_none) { |b| b.frm.radio(value: "NONE").set; b.loading.wait_while_present }
+
+  #element(:cross_listed_co_check_boxes) { |b| b.frm.dvi(id:"KS-CoListed-Checkbox-Group")}
+  element(:cross_listed_co_check_box) { |b| b.checkbox(id: "KS-COEditListed-Checkbox-Group_control_0") }
+
+ element(:growl_message) { |b| b.div(text: /Course offering .* has been successfully created/) }
+
+  element(:joint_defined_courses_table) { |b| b.div(id: "KS-Catalog-JointCourse-Section").table() }
+  element(:create_new_joint_defined_course_first_row) { |b| b.joint_defined_courses_table.rows[1].cells[3].link}
+  action(:create_new_joint_defined_course_row_1) { |b| b.joint_defined_courses_table.rows[1].cells[3].link.click }
+  action(:create_new_joint_defined_course_row_2) { |b| b.joint_defined_courses_table.rows[2].cells[3].link.click }
+  element(:joint_defined_course_row_1) { |b| b.joint_defined_courses_table.rows[1].text }
+  element(:joint_defined_course_row_2) { |b| b.joint_defined_courses_table.rows[2].text }
+
+  element(:create_offering) { |b| b.frm.button(id: "ks-uif-primaryActionButton").click; b.loading.wait_while_present }
+
+  #element(:delivery_format_add_element) {|b| b.frm.delivery_formats_table.rows[1].cells[ACTIONS_COLUMN].button(text: "add")  }
+  #action(:delivery_format_add) {|b| b.delivery_format_add_element.click; b.loading.wait_while_present   }
+
+  element(:delivery_formats_table) { |b| b.frm.div(id: "KS-CourseOffering-FormatOfferingSubSection").table() }
+  FORMAT_COLUMN = 0
+  GRADE_ROSTER_LEVEL_COLUMN = 1
+  FINAL_EXAM_COLUMN = 2
+  ACTIONS_COLUMN = 5
+
+  element(:format_select) {|b| b.delivery_formats_table.rows[1].cells[FORMAT_COLUMN].select }
+  element(:grade_roster_level_select) {|b| b.delivery_formats_table.rows[1].cells[GRADE_ROSTER_LEVEL_COLUMN].select }
+  element(:final_exam_driver_select)  {|b| b.delivery_formats_table.rows[1].cells[FINAL_EXAM_COLUMN].select }
+  #action(:add_format_btn) { |b| b.frm.button(text: "Add").click; b.loading.wait_while_present }
+  action(:delete_format_btn) {|b| b.frm.button(id: "KS-CourseOffering-FormatOfferingSubSection_del_line0") }
+
+  def add_random_delivery_format
+    begin
+      selected_options = {:del_format => select_random_option(format_select), :grade_format => select_random_option(grade_roster_level_select), :final_exam_driver => select_random_option(final_exam_driver_select)}
+    rescue
+      selected_options = {:del_format => select_random_option(format_select), :grade_format => select_random_option(grade_roster_level_select)}
+    end
+
+    #add_format_btn
+    return selected_options
+  end
+
+  def select_grade_roster_level(format)
+    delivery_format_row(format).cells[GRADE_ROSTER_LEVEL_COLUMN].select().select(format)
+  end
+
+  def select_final_exam_driver(format)
+    delivery_format_row(format).cells[FINAL_EXAM_COLUMN].select().select(format)
+  end
+
+  def delivery_format_row(format)
+    delivery_formats_table.row(text: /#{Regexp.escape(format)}/)
+  end
+
+  def select_random_option(select_element)
+    options = select_element.options.map{|option| option.text}
+    options.delete_if{|a| a.index("Select") != nil or  a == "" }
+    sel_opt = rand(options.length)
+    if options != []
+      select_element.select(options[sel_opt])
+      loading.wait_while_present
+      return options[sel_opt]
+    else
+      return nil
+    end
+  end
+
+end
---
+++ lib/sambal-kuali/pages/course_offering/create_co_from_existing_page.rb
@@ -0,0 +1,41 @@
+class CreateCOFromExisting < BasePage
+
+  wrapper_elements
+  frame_element
+  validation_elements
+
+  expected_element :exclude_instructor_checkbox
+
+  action(:create) { |b| b.frm.button(id: "ks-uif-primaryActionButton").click; b.loading.wait_while_present(120) }
+
+  element(:exclude_cancelled_aos_checkbox) { |b| b.frm.label(text: /Exclude cancelled Activity Offerings/) }
+  action(:select_exclude_cancelled_aos_checkbox) { |b| b.exclude_instructor_checkbox.wait_until_present; b.exclude_instructor_checkbox.click }
+
+  element(:exclude_scheduling_checkbox) { |b| b.frm.label(text: /Exclude scheduling information/) }
+  action(:select_exclude_sheduling_checkbox) { |b| b.exclude_instructor_checkbox.wait_until_present; b.exclude_instructor_checkbox.click }
+
+  element(:exclude_instructor_checkbox) { |b| b.frm.label(text: /Exclude instructor information/) }
+  action(:select_exclude_instructor_checkbox) { |b| b.exclude_instructor_checkbox.wait_until_present; b.exclude_instructor_checkbox.click }
+
+
+  element(:growl_message) { |b| b.div(text: /Course offering .* has been successfully created/) }
+
+  element(:course_offering_existing_table) { |b| b.frm.div(id: "KS-ExistingOffering-ListCOs").table() }
+
+  #TODO just selects the first row - needs to be deprecated
+  element(:course_offering_copy_element) {|b| b.frm.course_offering_existing_table.rows[1].cells[ACTIONS_COLUMN_CO].radio.click  }
+  action(:course_offering_copy) {|b| b.course_offering_copy_element.click }
+
+  ACTIONS_COLUMN = 0
+  CO_CODE_COLUMN = 1
+  OFFERED_TERM_COLUMN = 2
+
+ def existing_co_target_row(term, course)
+    course_offering_existing_table.row(text: /#{Regexp.escape(course)}[\S\s]#{Regexp.escape(term)}/)
+  end
+
+  def select_copy_for_existing_course(term, course)
+    existing_co_target_row(term, course).cells[ACTIONS_COLUMN].radio.click
+  end
+
+end
