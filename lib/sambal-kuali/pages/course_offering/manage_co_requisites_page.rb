56c0fbae98d213fe1f5bf2c194cd21cc332d4c29
llfourie <llfourie@kuali.org>
2013-07-30 07:51:43 -0400

    KSENROLL-7709 : Fix AFTs for the change in groups not having letters anymore
    git-svn-id: https://svn.kuali.org/repos/student/test/functional-automation/sambal/trunk@52890 0ac02d80-fb3f-11dc-a574-c57244a850eb

--- lib/sambal-kuali/pages/course_offering/manage_co_requisites_page.rb
+++ lib/sambal-kuali/pages/course_offering/manage_co_requisites_page.rb
@@ -9,7 +9,7 @@ class ManageCORequisites < BasePage
   element(:tab_section) { |b| b.frm.div(id: "KSCO-RuleEdit-TabSection_tabs")}
   element(:edit_tree_section) { |b| b.frm.div(id: "KRMS-RuleEditor-TreeGroup")}
   element(:logic_tab_section) { |b| b.frm.div(id: "KSCO-EditWithLogic-EditGroup")}
-  element(:preview_tree_section) { |b| b.logic_tab_section.div(id: "KSCO-LogicPreview-Tree")}
+  element(:preview_tree_section) { |b| b.logic_tab_section.div(id: "KSCO-EditWithLogic-TreeGroup")}
   element(:compare_rule_section) { |b| b.frm.div(id: "compareRuleLightBox")}
   element(:preview_rule_section) { |b| b.frm.div(id: "KSCO-RulePreview-ActionLinks")}
   element(:grade_section) { |b| b.edit_tree_section.div(:id => /KRMS-GradeScale-Field/)}
@@ -17,7 +17,7 @@ class ManageCORequisites < BasePage
   element(:background_div) { |b| b.frm.li(:id => /^u\d+_node_0_parent_root$/)}
   element(:logic_tab) { |b| b.tab_section.a(:text => /Edit Rule Logic/)}
   element(:object_tab) { |b| b.tab_section.a(:text => /Edit Rule/)}
-  element(:logic_text) { |b| b.tab_section.text_field(id: "KRMS-LogicArea-InputField_control")}
+  element(:logic_text) { |b| b.tab_section.text_field(name: "document.newMaintainableObject.dataObject.logicArea")}
 
   #action(:add_btn) { |b| b.frm.button(id: "KRMS-RuleEditorView-AddButton-Enabled").click; b.edit_loading.wait_while_present; b.loading.wait_while_present}
   action(:add_btn) { |b| b.frm.button(:text => /Add Statement/).when_present.click; b.edit_loading.wait_while_present; b.loading.wait_while_present}
@@ -65,6 +65,7 @@ class ManageCORequisites < BasePage
   element(:program_dropdown) { |b| b.frm.select(:name => /.*editTree.*proposition\.programType/)}
   element(:term_field) { |b| b.frm.text_field(:name => /.*editTree.*proposition\.termCode/)}
   element(:term_two_field) { |b| b.frm.text_field(:name => /.*editTree.*proposition\.termCode2/)}
+  element(:population_field) { |b| b.frm.text_field(:name => /.*editTree.*proposition\.population\.name/)}
 
   action(:completed) { |b| b.grade_section.radio(:id => /KRMS-GradeScale-Field.*_control_0/).when_present.click}
   action(:letter) { |b| b.grade_section.radio(:id => /KRMS-GradeScale-Field.*_control_1/).when_present.click}
@@ -79,7 +80,7 @@ class ManageCORequisites < BasePage
   element(:lookup_course_code) { |b| b.frm_popup.text_field(name: "lookupCriteria[code]")}
   element(:lookup_description) { |b| b.frm_popup.text_field(name: "lookupCriteria[description]")}
   element(:lookup_set_name) { |b| b.frm_popup.text_field(name: "lookupCriteria[name]")}
-  element(:lookup_class_standing) { |b| b.frm_popup.text_field(name: "lookupCriteria[keyword]")}
+  element(:lookup_population) { |b| b.frm_popup.text_field(name: "lookupCriteria[keyword]")}
   element(:lookup_abrev_org) { |b| b.frm_popup.text_field(name: "lookupCriteria[shortName]")}
   action(:lookup_search_button) { |b| b.frm_popup.button(id: "button_search").click; b.edit_loading.wait_while_present; b.loading.wait_while_present}
   element(:lookup_results_table) { |b| b.frm_popup.div(id: "uLookupResults").table() }
