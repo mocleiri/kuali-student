ed38feb0fa81eb919ad934ee2219a5eed1f90579
michael.ocleirigh <michael.ocleirigh@kuali.org>
2013-08-07 16:39:25 -0400

    KSENROLL-8498 create helper script for aggregate branch creation
    git-svn-id: https://svn.kuali.org/repos/student/tools/svn-merge-tools@53470 0ac02d80-fb3f-11dc-a574-c57244a850eb

---
+++ create-aggregate-branch.sh
@@ -0,0 +1,122 @@
+#!/bin/bash
+#
+# NAME: create-aggregate-branch.sh
+#
+# PURPOSE: To help creating aggregates
+# 
+
+usage () {
+	MSG=$1
+
+	if test -n "$MSG"
+	then
+		echo "ERROR: $MSG"
+	fi
+
+	echo "USAGE: <ks-api branch> <aggregate name> <modules> <in_branches:0 or 1>  <commit message> [<target prefix: like sandbox>]"
+	echo "<ks-api branch>: either trunk or branches/some_branch"
+	echo "<aggregate name>: in branches mode this is the name of the branch for each module.  i.e. ks-enroll/brances/aggregate_name"
+	echo "<modules>:non-api based modules to apply"
+	echo "<in_branches>:1 if $module/branches/aggregate_name is the target module location. 0 if $module is the target location. use 0 for sandbox based aggregates."
+	echo "<commit message>: commit message to use"
+	echo "<target_prefix>: optional.  Used if the target path is in the sandbox"
+	exit 1
+
+}
+
+SVNMUCC_CMD=svnmucc
+
+SOURCE_PATH="https://svn.kuali.org/repos/student/enrollment"
+
+API_SOURCE=$1
+
+if test -z "$API_SOURCE"
+then
+	usage "Missing Api Source"
+fi
+
+AGGREGATE_NAME=$2
+
+if test -z "$AGGREGATE_NAME"
+then
+	usage "Missing Aggregate Name"
+fi
+
+MODULES=$3
+
+if test -z "$MODULES"
+then
+	usage "Missing modules"
+fi
+
+IN_BRANCH=$4
+
+if test -z "$IN_BRANCH"
+then
+	usage "Missing In branch mode"
+fi
+
+COMMIT_MESSAGE=$5
+
+if test -z "$COMMIT_MESSAGE"
+then
+	usage "Missing Commit Message"
+fi
+
+
+TARGET_PATH=$6
+
+if test -z "$TARGET_PATH"
+then
+	TARGET_PATH="https://svn.kuali.org/repos/student/enrollment"
+fi
+
+SOURCE_REV=$(svn info https://svn.kuali.org/repos/student | grep Revision: | cut -d: -f 2 | sed 's/^\ //')
+
+# echo $SOURCE_REV
+# echo "TARGET_PATH=$TARGET_PATH"
+
+CMD_FILE=/tmp/$RANDOM.dat
+
+#echo "CMD_FILE=$CMD_FILE"
+
+printf "$SVNMUCC_CMD " > $CMD_FILE
+
+printf "cp $SOURCE_REV $SOURCE_PATH/ks-api/$API_SOURCE " >> $CMD_FILE
+
+if test $IN_BRANCH == "1"
+then
+	printf "    $TARGET_PATH/ks-api/branches/$AGGREGATE_NAME " >> $CMD_FILE
+
+else
+	printf "    $TARGET_PATH/$AGGREGATE_NAME/ks-api  " >> $CMD_FILE
+fi
+
+for M in $MODULES
+do
+	# printf "module = $M"
+	printf "cp $SOURCE_REV $SOURCE_PATH/$M/trunk " >> $CMD_FILE
+
+	if test $IN_BRANCH == "1"
+	then
+		printf "    $TARGET_PATH/$M/branches/$AGGREGATE_NAME " >> $CMD_FILE
+
+	else
+		printf "    $TARGET_PATH/$AGGREGATE_NAME/$M " >> $CMD_FILE
+	fi
+
+done
+
+printf " -m \"$COMMIT_MESSAGE\"" >> $CMD_FILE
+
+cat $CMD_FILE
+
+printf "\nApply Change (y/n)"
+
+read command
+
+if test $command == "y"
+then
+	bash -c $CMD_FILE
+	echo "applied"
+fi
