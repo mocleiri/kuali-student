389d3a44374935d8d4bd934b25ce25f763e62bf7
mike.wyers <mike.wyers@kuali.org>
2013-08-12 10:57:09 -0400

    KSENROLL-8464 AFTs fix hard-coded urls for Student AZ tests
    git-svn-id: https://svn.kuali.org/repos/student/test/functional-automation/sambal/trunk@53743 0ac02d80-fb3f-11dc-a574-c57244a850eb

--- features/step_definitions/az_student_steps.rb
+++ features/step_definitions/az_student_steps.rb
@@ -1,5 +1,4 @@
 When /^I navigate to manage course offerings and I do not have access$/ do
-  @direct_navigation = make DirectNavigation
   go_to_manage_course_offerings
   on ErrorPage do |page|
     page.error_401.should == true
@@ -13,55 +12,60 @@ When /^I navigate to create course offerings and I do not have access$/ do
   end
 end
 
-When /^I do not have direct access to the manage course offerings search page$/ do
+When /^I do not have direct url access to the manage course offerings search page$/ do
   @browser.goto @direct_navigation.manage_course_offering
   on ErrorPage do |page|
     page.error_401.should == true
   end
 end
 
-When /^I do not have direct access to the course offering results page$/ do
+When /^I do not have direct url access to the course offering results page$/ do
   @browser.goto @direct_navigation.course_offering_results
   on ErrorPage do |page|
     page.error_401.should == true
   end
 end
 
-When /^I do not have direct access to the course offering single course results page$/ do
+When /^I do not have direct url access to the course offering single course results page$/ do
   @browser.goto @direct_navigation.single_course_results
   on ErrorPage do |page|
     page.error_401.should == true
   end
 end
-When /^I do not have direct access to the edit course offerings page$/ do
+When /^I do not have direct url access to the edit course offerings page$/ do
   @browser.goto @direct_navigation.edit_course_offering
   on ErrorPage do |page|
     page.error_401.should == true
   end
 end
 
-When /^I do not have direct access to the edit activity offering page$/ do
+When /^I do not have direct url access to the edit activity offering page$/ do
   @browser.goto @direct_navigation.edit_activity_offering
   on ErrorPage do |page|
     page.error_401.should == true
   end
 end
-When /^I do not have direct access to the manage registration group page$/ do
+When /^I do not have direct url access to the manage registration group page$/ do
   @browser.goto @direct_navigation.manage_registration_group
   on ErrorPage do |page|
     page.error_401.should == true
   end
 end
-When /^I do not have direct access to the create course offerings page$/ do
+When /^I do not have direct url access to the create course offerings page$/ do
   @browser.goto @direct_navigation.create_course_offering
   on ErrorPage do |page|
     page.error_401.should == true
   end
 end
 
-Given /^There is direct access to edit a course offering$/ do
-  step "I am logged in as a Schedule Coordinator"
+Given /^there is direct url access to manage and search a course offering$/ do
   @direct_navigation = make DirectNavigation
-  @direct_navigation.setup_navigation
-  step "I am logged in as a Student"
+  @direct_navigation.setup_search_edit_co_urls
+end
+
+Given /^there is direct url access to edit a course offering and activity offering$/ do
+  @direct_navigation = make DirectNavigation
+  @direct_navigation.setup_search_edit_co_urls
+  @direct_navigation.setup_edit_ao_urls
+  @direct_navigation.setup_create_co_urls
 end
--- lib/sambal-kuali/data_objects/direct_navigation_object.rb
+++ lib/sambal-kuali/data_objects/direct_navigation_object.rb
@@ -18,20 +18,40 @@ class DirectNavigation
 
   def initialize(browser, opts={})
     @browser = browser
-
-    defaults = {
-        :edit_course_offering=> "#{$test_site}/kr-krad/courseOffering?editCrossListedCoAlias=false&showHome=false&courseOfferingInfo.id=8ff7a167-9a14-450b-9901-dbc3a72d0133&methodToCall=maintenanceEdit&dataObjectClassName=org.kuali.student.enrollment.class2.courseoffering.dto.CourseOfferingEditWrapper&formKey=317e1602-3132-4506-8fba-05694804d8cb&cacheKey=s4j8mcnbqzgkc516z8r5",
-        :edit_activity_offering=>"#{$test_site}/kr-krad/activityOffering?showHome=false&courseOfferingId=b8bfd4cd-a804-41d0-8659-f4a1a3771e92&methodToCall=maintenanceEdit&history=null%255E%2524%255Enull%255E%2524%255EHome%255E%2524%255Ehttp%253A%252F%252Fenv2.ks.kuali.org%252Fportal.do%255E%2524%255Enull%2524%255E%2524courseOfferingManagementView%255E%2524%255EsearchInputPage%255E%2524%255EManage%2BCourse%2BOfferings%255E%2524%255Ehttp%253A%252F%252Fenv2.ks.kuali.org%252Fkr-krad%252FcourseOfferingManagement%253FpageId%253DsearchInputPage%2526methodToCall%253Dstart%2526withinPortal%253Dfalse%2526viewId%253DcourseOfferingManagementView%2526formKey%253Dbb17bf70-210e-4cfd-be30-940cc828b5af%2526showHome%253Dfalse%255E%2524%255Ebb17bf70-210e-4cfd-be30-940cc828b5af&aoInfo.id=ff255717-2a11-4a97-ac8a-cf4a38433f79&dataObjectClassName=org.kuali.student.enrollment.class2.courseoffering.dto.ActivityOfferingWrapper",
-        :manage_course_offering=>"#{$test_site}/kr-krad/courseOfferingManagement?viewId=courseOfferingManagementView&methodToCall=start&withinPortal=false&formKey=544fc9d2-4b1d-4c4a-a28c-e5de1a34b7eb&cacheKey=arxeqbry3vrovky0197s4&pageId=searchInputPage",
-        :course_offering_results=>"#{$test_site}/kr-krad/courseOfferingManagement?viewId=courseOfferingManagementView&methodToCall=start&withinPortal=false&formKey=544fc9d2-4b1d-4c4a-a28c-e5de1a34b7eb&cacheKey=qx892pmpk1px877losv&pageId=manageCourseOfferingsPage",
-        :single_course_results=>"#{$test_site}/kr-krad/courseOfferingManagement?formKey=544fc9d2-4b1d-4c4a-a28c-e5de1a34b7eb&cacheKey=55fld3imscj26ddjren271&pageId=manageTheCourseOfferingPage",
-        :create_course_offering=>"#{$test_site}/kr-krad/courseOfferingCreate?methodToCall=start&dataObjectClassName=org.kuali.student.enrollment.class2.courseoffering.dto.CourseOfferingCreateWrapper&hideReturnLink=true&formKey=1eea52b4-f0ac-420b-8b54-a0855babe289&cacheKey=c0gozz2z65sc39pgx2ey7&pageId=courseOfferingCreatePage"
-    }
-    options = defaults.merge(opts)
-    set_options(options)
   end
 
-  def setup_navigation
+  def setup_search_edit_co_urls
+
+    @course_offering = make CourseOffering, :term=>Rollover::MAIN_TEST_TERM_SOURCE, :course=>"CHEM132"
+    @course_offering.search_by_subjectcode
+    @course_offering_results = @browser.url
+
+    @course_offering.manage
+    @manage_course_offering = @browser.url
+    @single_course_results = @browser.url
+
+    on ManageCourseOfferings do |page|
+      page.edit_course_offering
+    end
+    @edit_course_offering = @browser.url
+    on CourseOfferingEdit do |page|
+      page.cancel
+    end
+
+  end
+
+  def setup_create_co_urls
+    @course_offering = make CourseOffering, :term=>Rollover::MAIN_TEST_TERM_SOURCE, :course=>"CHEM132"
+    @create_course_offering = @course_offering.start_create_by_search
+    @create_course_offering = @browser.url
+    on CreateCourseOffering do  |page|
+      page.catalogue_course_code.set ""
+      page.target_term.set ""
+      page.cancel
+    end
+  end
+
+  def setup_edit_ao_urls
     @course_offering = make CourseOffering, :term=>Rollover::MAIN_TEST_TERM_SOURCE, :course=>"CHEM132"
     @course_offering.manage
     on ManageCourseOfferings do |page|
