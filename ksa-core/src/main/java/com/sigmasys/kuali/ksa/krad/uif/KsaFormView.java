d75416aaa91ce3686974ed86501d1989dd8f9d12
mivanov <mivanov@kuali.org>
2013-08-23 12:49:35 -0400

    Removed the property "viewHelperService" from KsaFormView class as the KRAD's View (since 2.3.0) now supports it as well.
    git-svn-id: https://svn.kuali.org/repos/student/contrib/ksa/trunk@54508 0ac02d80-fb3f-11dc-a574-c57244a850eb

--- ksa-core/src/main/java/com/sigmasys/kuali/ksa/krad/uif/KsaFormView.java
+++ ksa-core/src/main/java/com/sigmasys/kuali/ksa/krad/uif/KsaFormView.java
@@ -1,7 +1,6 @@
 package com.sigmasys.kuali.ksa.krad.uif;
 
 import org.kuali.rice.krad.uif.element.Header;
-import org.kuali.rice.krad.uif.service.ViewHelperService;
 import org.kuali.rice.krad.uif.view.FormView;
 
 /**
@@ -11,8 +10,6 @@ import org.kuali.rice.krad.uif.view.FormView;
  */
 public class KsaFormView extends FormView {
 
-    private ViewHelperService viewHelperService;
-
     @Override
     public void setHeader(Header header) {
         if (header != null && header.getUpperGroup() != null) {
@@ -21,18 +18,4 @@ public class KsaFormView extends FormView {
         super.setHeader(header);
     }
 
-    /**
-     * KRAD original View class does not allow Spring-managed view helpers to be injected,
-     * but it would really be nice to support it as well.
-     *
-     * @return ViewHelperService instance.
-     */
-    @Override
-    public ViewHelperService getViewHelperService() {
-        return (viewHelperService != null) ? viewHelperService : super.getViewHelperService();
-    }
-
-    public void setViewHelperService(ViewHelperService viewHelperService) {
-        this.viewHelperService = viewHelperService;
-    }
 }
