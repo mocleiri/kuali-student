08d75242e5d43e4819a17da5329eac98e1717307
nelson.leduc <nelson.leduc@kuali.org>
2013-08-23 02:39:41 -0400

    KSENROLL-8877 - completely refactored the first couple steps in the second scenario.  It was succeeding only by the luck of the reference data and began to fail after the Rice 2.3 upgrade
    git-svn-id: https://svn.kuali.org/repos/student/test/functional-automation/sambal/trunk@54458 0ac02d80-fb3f-11dc-a574-c57244a850eb

--- features/step_definitions/delete_co_steps.rb
+++ features/step_definitions/delete_co_steps.rb
@@ -1,7 +1,5 @@
 When /^I create a Course Offering with Draft Activity Offerings$/ do
-  #setup
   @course_offering = create CourseOffering, :create_by_copy=>(make CourseOffering)
-  @course_offering.check_course_in_status(CourseOffering::DRAFT_STATUS)
 end
 
 And /^I cancel the deletion of a Course Offering in Course Offering Code view$/ do
@@ -61,8 +59,8 @@ When /^I create multiple Course Offerings with Draft Activity Offerings$/ do
 end
 
 And /^I cancel the deletion of the Course Offerings in Course Offering Code view$/ do
-  @course_offering.manage
-  @course_offering.delete_co_coc_view :should_confirm_delete=>false
+  @course_offering.search_by_subjectcode
+  @course_offering.delete_co_list :co_obj_list => [@course_offering], :should_confirm_delete => false
 end
 
 And /^I delete these Course Offerings$/ do
