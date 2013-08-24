142a2a224919a974643541b3b381133662184b6a
godunovs <godunovs@kuali.org>
2013-08-24 03:10:10 -0400

    Unit testing.
    git-svn-id: https://svn.kuali.org/repos/student/contrib/ksa/trunk@54574 0ac02d80-fb3f-11dc-a574-c57244a850eb

--- ksa-core/src/test/java/com/sigmasys/kuali/ksa/service/FeeManagementServiceTest.java
+++ ksa-core/src/test/java/com/sigmasys/kuali/ksa/service/FeeManagementServiceTest.java
@@ -1,11 +1,16 @@
 package com.sigmasys.kuali.ksa.service;
 
+import com.sigmasys.kuali.ksa.model.fm.FeeManagementSessionStatus;
+import junit.framework.Assert;
+
+import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 
+import com.sigmasys.kuali.ksa.model.fm.FeeManagementSession;
 import com.sigmasys.kuali.ksa.service.fm.FeeManagementService;
 
 /**
@@ -19,6 +24,9 @@ public class FeeManagementServiceTest extends AbstractServiceTest {
 
 	@Autowired
 	private FeeManagementService fmService;
+	
+	@Autowired
+	private PersistenceService persistenceService;
 	
 	
 	@Test
@@ -38,4 +46,87 @@ public class FeeManagementServiceTest extends AbstractServiceTest {
 		
 		// Validate the results:
 	}
+	
+	@Test
+	public void testChargeSessionNoSessionIdFound() throws Exception {
+		// Call the method with a fake FM Session ID:
+		boolean exceptionCaught = false;
+
+		try {
+			fmService.chargeSession(0L);
+		} catch (IllegalArgumentException e) {
+			Assert.assertEquals("Cannot find an FM session with the ID 0", e.getMessage());
+            exceptionCaught = true;
+		} catch (Throwable t) {
+			// We should never catch any other type of a Throwable:
+			Assert.assertTrue("Must never catch anything by IllegalArgumentException", false);
+		}
+		
+		// Assert Exception was caught
+		Assert.assertTrue("Never caught an IllegalArgumentException", exceptionCaught);
+	}
+	
+	@Test
+	public void testChargeSessionNullStatus() throws Exception {
+		// Create an FM session:
+		FeeManagementSession fmSession = new FeeManagementSession();
+		
+		persistenceService.persistEntity(fmSession);
+		
+		// Call the method:
+		boolean exceptionCaught = false;
+		
+		try {
+			fmService.chargeSession(fmSession.getId());
+		} catch (IllegalStateException ise) {
+			// We should catch this exception here
+			exceptionCaught = true;
+		} catch (Throwable e) {
+			// We should never catch any other type of a Throwable:
+			Assert.assertTrue("Must never catch anything by IllegalStateException", false);
+		}
+		
+		// Assert Exception was caught
+		Assert.assertTrue("Never caught an IllegalStateException", exceptionCaught);
+	}
+
+    @Test
+    public void testChargeSessionWrongStatus() throws Exception {
+        // Create an FM session:
+        FeeManagementSession fmSession = new FeeManagementSession();
+
+        fmSession.setChargeStatus(FeeManagementSessionStatus.CURRENT);
+        persistenceService.persistEntity(fmSession);
+
+        // Call the method:
+        boolean exceptionCaught = false;
+
+        try {
+            fmService.chargeSession(fmSession.getId());
+        } catch (IllegalStateException ise) {
+            // We should catch this exception here
+            exceptionCaught = true;
+        } catch (Throwable e) {
+            // We should never catch any other type of a Throwable:
+            Assert.assertTrue("Must never catch anything by IllegalStateException", false);
+        }
+
+        // Assert Exception was caught
+        Assert.assertTrue("Never caught an IllegalStateException", exceptionCaught);
+    }
+
+    @Test
+    public void testChargeSessionNoManifestsFound() throws Exception {
+        // Create an FM session:
+        FeeManagementSession fmSession = new FeeManagementSession();
+
+        fmSession.setChargeStatus(FeeManagementSessionStatus.RECONCILED);
+        persistenceService.persistEntity(fmSession);
+
+        // Call the method:
+        fmService.chargeSession(fmSession.getId());
+
+        // Verify session's status changed to CHARGED:
+        Assert.assertEquals("FM Session status has never changed to CHARGED", FeeManagementSessionStatus.CHARGED, fmSession.getStatus());
+    }
 }
