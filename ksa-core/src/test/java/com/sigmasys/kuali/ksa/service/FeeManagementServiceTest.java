0bfb592e110affd67b758c6701025f5f8b5b8382
godunovs <godunovs@kuali.org>
2013-08-25 03:31:42 -0400

    More unit testing of "chargeSession"
    git-svn-id: https://svn.kuali.org/repos/student/contrib/ksa/trunk@54594 0ac02d80-fb3f-11dc-a574-c57244a850eb

--- ksa-core/src/test/java/com/sigmasys/kuali/ksa/service/FeeManagementServiceTest.java
+++ ksa-core/src/test/java/com/sigmasys/kuali/ksa/service/FeeManagementServiceTest.java
@@ -1,6 +1,8 @@
 package com.sigmasys.kuali.ksa.service;
 
-import com.sigmasys.kuali.ksa.model.fm.FeeManagementSessionStatus;
+import com.sigmasys.kuali.ksa.model.Account;
+import com.sigmasys.kuali.ksa.model.Transaction;
+import com.sigmasys.kuali.ksa.model.fm.*;
 import junit.framework.Assert;
 
 import org.junit.Before;
@@ -10,8 +12,15 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 
-import com.sigmasys.kuali.ksa.model.fm.FeeManagementSession;
 import com.sigmasys.kuali.ksa.service.fm.FeeManagementService;
+
+import java.math.BigDecimal;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Date;
+import java.util.List;
+
+import static junit.framework.Assert.*;
 
 /**
  * FeeManagementService unit tests.
@@ -22,13 +31,23 @@ import com.sigmasys.kuali.ksa.service.fm.FeeManagementService;
 @ContextConfiguration(locations = {ServiceTestSuite.TEST_KSA_CONTEXT})
 public class FeeManagementServiceTest extends AbstractServiceTest {
 
+    private static final String ACCOUNT_ID = "admin1";
+    private static final String TRANSACTION_TYPE_ID = "cash";
+
 	@Autowired
 	private FeeManagementService fmService;
 	
 	@Autowired
 	private PersistenceService persistenceService;
-	
-	
+
+    @Autowired
+    private TransactionService transactionService;
+
+    @Autowired
+    private AccountService accountService;
+
+
+
 	@Test
 	public void testReconcileSession() throws Exception {
 		// Preposition the data:
@@ -46,6 +65,13 @@ public class FeeManagementServiceTest extends AbstractServiceTest {
 		
 		// Validate the results:
 	}
+
+    /*****************************************************************
+     *
+     * Unit tests for "chargeSession"
+     *
+     ****************************************************************/
+
 	
 	@Test
 	public void testChargeSessionNoSessionIdFound() throws Exception {
@@ -55,15 +81,15 @@ public class FeeManagementServiceTest extends AbstractServiceTest {
 		try {
 			fmService.chargeSession(0L);
 		} catch (IllegalArgumentException e) {
-			Assert.assertEquals("Cannot find an FM session with the ID 0", e.getMessage());
+			assertEquals("Cannot find an FM session with the ID 0", e.getMessage());
             exceptionCaught = true;
 		} catch (Throwable t) {
 			// We should never catch any other type of a Throwable:
-			Assert.assertTrue("Must never catch anything by IllegalArgumentException", false);
+			assertTrue("Must never catch anything by IllegalArgumentException", false);
 		}
 		
 		// Assert Exception was caught
-		Assert.assertTrue("Never caught an IllegalArgumentException", exceptionCaught);
+		assertTrue("Never caught an IllegalArgumentException", exceptionCaught);
 	}
 	
 	@Test
@@ -83,11 +109,11 @@ public class FeeManagementServiceTest extends AbstractServiceTest {
 			exceptionCaught = true;
 		} catch (Throwable e) {
 			// We should never catch any other type of a Throwable:
-			Assert.assertTrue("Must never catch anything by IllegalStateException", false);
+			assertTrue("Must never catch anything by IllegalStateException", false);
 		}
 		
 		// Assert Exception was caught
-		Assert.assertTrue("Never caught an IllegalStateException", exceptionCaught);
+		assertTrue("Never caught an IllegalStateException", exceptionCaught);
 	}
 
     @Test
@@ -108,11 +134,11 @@ public class FeeManagementServiceTest extends AbstractServiceTest {
             exceptionCaught = true;
         } catch (Throwable e) {
             // We should never catch any other type of a Throwable:
-            Assert.assertTrue("Must never catch anything by IllegalStateException", false);
+            assertTrue("Must never catch anything by IllegalStateException", false);
         }
 
         // Assert Exception was caught
-        Assert.assertTrue("Never caught an IllegalStateException", exceptionCaught);
+        assertTrue("Never caught an IllegalStateException", exceptionCaught);
     }
 
     @Test
@@ -127,6 +153,194 @@ public class FeeManagementServiceTest extends AbstractServiceTest {
         fmService.chargeSession(fmSession.getId());
 
         // Verify session's status changed to CHARGED:
-        Assert.assertEquals("FM Session status has never changed to CHARGED", FeeManagementSessionStatus.CHARGED, fmSession.getStatus());
+        assertEquals("FM Session status has never changed to CHARGED", FeeManagementSessionStatus.CHARGED, fmSession.getStatus());
+    }
+
+    @Test
+    public void testChargeSessionManifestHasTransaction() throws Exception {
+        // Create an FM session and manifest:
+        FmSession fmSession = createFmSession(1, true, false, FeeManagementManifestType.ORIGINAL);
+
+        // Call the method:
+        fmService.chargeSession(fmSession.getSession().getId());
+
+        // Verify session's status changed to CHARGED:
+        assertEquals("FM Session status has never changed to CHARGED", FeeManagementSessionStatus.CHARGED, fmSession.getSession().getStatus());
+    }
+
+    @Test
+    public void testChargeSessionManifestHasNoTransactionTypeId() throws Exception {
+        // Create an FM Session:
+        FmSession fmSession = createFmSession(1, false, false, FeeManagementManifestType.CHARGE);
+        FeeManagementManifest manifest = fmSession.getManifests().get(0);
+
+        // Nullify the Transaction type ID:
+        manifest.setTransactionTypeId(null);
+        persistenceService.persistEntity(manifest);
+
+        // Call the method:
+        fmService.chargeSession(fmSession.getSession().getId());
+
+        // Verify a Charge was not created:
+        assertNull("A CHARGE Transaction was created, but not expected.", manifest.getTransaction());
+
+        // Verify session's status changed to CHARGED:
+        assertEquals("FM Session status has never changed to CHARGED", FeeManagementSessionStatus.CHARGED, fmSession.getSession().getStatus());
+    }
+
+    @Test
+    public void testChargeSessionChargeManifestNoLinkedManifest() throws Exception {
+        // Create an FM Session:
+        FmSession fmSession = createFmSession(1, false, false, FeeManagementManifestType.CHARGE);
+        FeeManagementManifest manifest = fmSession.getManifests().get(0);
+        Transaction primaryTransaction = manifest.getTransaction();
+
+        // Call the method:
+        fmService.chargeSession(fmSession.getSession().getId());
+
+        // Validate that a new Transaction was created:
+        Transaction chargeTransaction = manifest.getTransaction();
+
+        assertNull(primaryTransaction);
+        assertNotNull("No CHARGE Transaction was created.", chargeTransaction);
+        assertNotNull("CHARGE Transaction's ID must not be null", chargeTransaction.getId());
+        assertNotNull("New Transaction's amount must not be empty.", chargeTransaction.getAmount());
+        assertTrue("CHARGE Transaction was created in a different amount than the manifest.", manifest.getAmount().compareTo(chargeTransaction.getAmount()) == 0);
+
+        // Validate there is no linked manifest:
+        assertNull("Linked manifest must not exist", manifest.getLinkedManifest());
+
+        // Validate manifest is marked session current:
+        assertTrue("FM Session must be current.", manifest.isSessionCurrent());
+    }
+
+    @Test
+    public void testChargeSessionChargeManifestLinkedManifestHasNoTransaction() throws Exception {
+        // Create an FM session with linked manifests:
+        FmSession fmSession = createFmSession(1, FeeManagementManifestType.CHARGE);
+        FeeManagementManifest manifest = fmSession.getManifests().get(0);
+
+        // Nullify the implicated Transaction:
+        FeeManagementManifest linkedManifest = manifest.getLinkedManifest();
+
+        linkedManifest.setTransaction(null);
+        persistenceService.persistEntity(linkedManifest);
+
+        // Call the method:
+        fmService.chargeSession(fmSession.getSession().getId());
+
+        // Validate that a new Transaction was created:
+        Transaction chargeTransaction = manifest.getTransaction();
+
+        assertNotNull("No CHARGE Transaction was created.", chargeTransaction);
+        assertNotNull("CHARGE Transaction's ID must not be null", chargeTransaction.getId());
+        assertNotNull("New Transaction's amount must not be empty.", chargeTransaction.getAmount());
+        assertTrue("CHARGE Transaction was created in a different amount than the manifest.", manifest.getAmount().compareTo(chargeTransaction.getAmount()) == 0);
+
+        // Validate Implicated Transaction does not exist:
+        assertNull("Implicated Transaction must not exist", linkedManifest.getTransaction());
+
+        // Validate manifest is marked session current:
+        assertTrue("FM Session must be current.", manifest.isSessionCurrent());
+    }
+
+    /********************************************************************************
+     *
+     * Helper methods
+     *
+     ********************************************************************************/
+
+    /**
+     * Creates FM Session, primary manifests WITHOUT Transactions, creates linked manifests with implicated transactions.
+     *
+     * @param numManifests                  Number of FM manifests to create.
+     * @param manifestTypes                 Types of primary manifests to create. Must be the size of "numManifests"
+     * @return An <code>FmSession</code> with new objects.
+     */
+    private FmSession createFmSession(int numManifests, FeeManagementManifestType... manifestTypes) {
+        return createFmSession(numManifests, false, true, manifestTypes);
+    }
+
+    /**
+     * Creates an FM Session, Manifests, Linked Manifests, Transactions and Implicated Transactions.
+     *
+     * @param numManifests                  Number of FM manifests to create.
+     * @param addPrimaryTransactions        Whether to add primary manifests' transactions.
+     * @param addLinkedManifests            Whether to add linked manifests and their implicated transactions.
+     * @param manifestTypes                 Types of primary manifests to create. Must be the size of "numManifests"
+     * @return An <code>FmSession</code> with new objects.
+     */
+    private FmSession createFmSession(int numManifests, boolean addPrimaryTransactions, boolean addLinkedManifests, FeeManagementManifestType... manifestTypes) {
+        // Create Session:
+        Account account = accountService.getOrCreateAccount(ACCOUNT_ID);
+        FeeManagementSession fmSession = new FeeManagementSession();
+        FmSession result = new FmSession();
+
+        fmSession.setChargeStatus(FeeManagementSessionStatus.RECONCILED);
+        fmSession.setAccount(account);
+        persistenceService.persistEntity(fmSession);
+        result.setSession(fmSession);
+
+        // Create manifests:
+        for (int i=0; i<numManifests; i++) {
+            FeeManagementManifest manifest = new FeeManagementManifest();
+            BigDecimal manifestAmount = new BigDecimal(10d * (i+1));
+
+            manifest.setSession(fmSession);
+            manifest.setType(manifestTypes[i]);
+            manifest.setAmount(manifestAmount);
+            manifest.setTransactionTypeId(TRANSACTION_TYPE_ID);
+
+            // Add Transaction:
+            if (addPrimaryTransactions) {
+                Transaction transaction = transactionService.createTransaction(TRANSACTION_TYPE_ID, ACCOUNT_ID, new Date(), manifestAmount);
+
+                manifest.setTransaction(transaction);
+            }
+
+            // Persist the manifest:
+            persistenceService.persistEntity(manifest);
+            result.getManifests().add(manifest);
+
+            // Add linked manifest and implicated transaction:
+            if (addLinkedManifests) {
+                FeeManagementManifest linkedManifest = new FeeManagementManifest();
+                Transaction implicatedTransaction = transactionService.createTransaction(TRANSACTION_TYPE_ID, ACCOUNT_ID, new Date(), manifestAmount.add(new BigDecimal(1d)));
+
+                //linkedManifest.setSession(fmSession);
+                linkedManifest.setTransaction(implicatedTransaction);
+                linkedManifest.setType(FeeManagementManifestType.CORRECTION);
+                linkedManifest.setLinkedManifest(manifest);
+                persistenceService.persistEntity(manifest);
+                manifest.setLinkedManifest(linkedManifest);
+                persistenceService.persistEntity(linkedManifest);
+            }
+        }
+
+        return result;
+    }
+
+    /**
+     * Encapsulates an FM Session and related Manifests.
+     */
+    private class FmSession {
+        private FeeManagementSession session;
+        private List<FeeManagementManifest> manifests = new ArrayList<FeeManagementManifest>();
+
+        public FeeManagementSession getSession() {
+            return session;
+        }
+
+        public void setSession(FeeManagementSession session) {
+            this.session = session;
+        }
+
+        public List<FeeManagementManifest> getManifests() {
+            return manifests;
+        }
+
+        public void setManifests(List<FeeManagementManifest> manifests) {
+            this.manifests = manifests;
+        }
     }
 }
