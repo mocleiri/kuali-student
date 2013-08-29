3514dc79e6e8c9a923dc94a5669e661814a46058
tbornholtz <tbornholtz@kuali.org>
2013-08-29 16:31:26 -0400

    KSA-251 - Fix memos from stacktracing
    git-svn-id: https://svn.kuali.org/repos/student/contrib/ksa/trunk@54956 0ac02d80-fb3f-11dc-a574-c57244a850eb

--- ksa-core/src/main/java/com/sigmasys/kuali/ksa/krad/form/MemoForm.java
+++ ksa-core/src/main/java/com/sigmasys/kuali/ksa/krad/form/MemoForm.java
@@ -2,7 +2,10 @@ package com.sigmasys.kuali.ksa.krad.form;
 
 import com.sigmasys.kuali.ksa.model.Account;
 import com.sigmasys.kuali.ksa.model.Memo;
+import org.kuali.rice.core.api.util.tree.Node;
+import org.kuali.rice.core.api.util.tree.Tree;
 
+import java.util.ArrayList;
 import java.util.Date;
 import java.util.List;
 
@@ -19,6 +22,8 @@ public class MemoForm extends AbstractViewModel {
     private Date toDate;
 
     private List<Memo> memoModels;
+
+    private Tree<Memo, String> memoTree = new Tree<Memo, String>();
 
     private Memo memoModel;
 
@@ -84,5 +89,46 @@ public class MemoForm extends AbstractViewModel {
 
     public void setAefInstructionalText(String aefInstructionalText) {
         this.aefInstructionalText = aefInstructionalText;
+    }
+
+    public Tree<Memo, String> getMemoTree(){
+
+        Node<Memo, String> rootNode = new Node<Memo, String>(new Memo(), "Root");
+        memoTree.setRootElement(rootNode);
+
+        if(this.memoModels == null){ return memoTree; }
+
+        List<Long> storedIds = new ArrayList<Long>(memoModels.size());
+        // Need to put the memos in order
+        for(Memo memo : memoModels){
+            addNode(rootNode, new Node<Memo, String>(memo, memo.getText()), storedIds);
+        }
+
+        return memoTree;
+    }
+
+
+    private void addNode(Node<Memo, String> root, Node<Memo, String> newNode, List<Long> storedIds){
+        // Is this really the root?
+        Memo rootMemo = root.getData();
+        Long id = rootMemo.getId();
+
+        Memo newMemo = newNode.getData();
+        Long newId = newMemo.getId();
+
+        if(storedIds.contains(newId)){
+            // Already in the tree somewhere, return.
+            return;
+        }
+
+        root.addChild(newNode);
+        storedIds.add(newId);
+
+        if(newMemo.getNextMemo() != null){
+            Memo next = newMemo.getNextMemo();
+            addNode(newNode, new Node<Memo, String>(next, next.getText()), storedIds);
+        }
+
+
     }
 }
