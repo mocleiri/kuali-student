32df682743945438dbeea979950e4d0ca379ff32
michael.ocleirigh <michael.ocleirigh@kuali.org>
2013-09-04 14:21:44 -0400

    KSENROLL-8908 improve feature branch creation and deletion scripts
    Now the revision to copy the feature branch from can be specified instead
    of defaulting to HEAD.
    
    The delete-aggregate-branch.sh script is useful if feature branches were
    created in the wrong spots.
    
    git-svn-id: https://svn.kuali.org/repos/student/tools/svn-merge-tools@55215 0ac02d80-fb3f-11dc-a574-c57244a850eb

--- delete-aggregate-branch.sh
+++ delete-aggregate-branch.sh
@@ -13,15 +13,13 @@ usage () {
 		echo "ERROR: $MSG"
 	fi
 
-	echo "USAGE: <ks-api branch> <aggregate name> <modules> <in_branches:0 or 1>  <commit message> <source revision> [<source prefix> <target prefix: like sandbox>]"
+	echo "USAGE: <ks-api branch> <aggregate name> <modules> <in_branches:0 or 1>  <commit message> [<source prefix>]"
 	echo "<ks-api branch>: either trunk or branches/some_branch"
 	echo "<aggregate name>: in branches mode this is the name of the branch for each module.  i.e. ks-enroll/brances/aggregate_name"
 	echo "<modules>:non-api based modules to apply"
 	echo "<in_branches>:1 if $module/branches/aggregate_name is the target module location. 0 if $module is the target location. use 0 for sandbox based aggregates."
 	echo "<commit message>: commit message to use"
-	echo "<source revision>: 0 will find out the current revision. >0 will use the indicated revision."
 	echo "<source_prefix>: optional. enrollment or sandbox or contrib/CM"
-	echo "<target_prefix>: optional. enrollment or sandbox or contrib/CM"
 	exit 1
 
 }
@@ -34,7 +32,7 @@ API_SOURCE=$1
 
 if test -z "$API_SOURCE"
 then
-	usage "Missing Api Source"
+	usage "Missing API Source"
 fi
 
 AGGREGATE_NAME=$2
@@ -65,14 +63,8 @@ then
 	usage "Missing Commit Message"
 fi
 
-SOURCE_REVISION=$6
 
-if test -z "$SOURCE_REVISION"
-then
-	usage "Missing Source Revision"
-fi
-
-SOURCE_PREFIX=$7
+SOURCE_PREFIX=$6
 
 SOURCE_PATH=""
 
@@ -83,56 +75,22 @@ else
 	SOURCE_PATH="$REPOSITORY/$SOURCE_PREFIX"
 fi
 
-TARGET_PREFIX=$8
-
-TARGET_PATH=""
-
-if test -z "$TARGET_PREFIX"
-then
-	TARGET_PATH="$REPOSITORY/enrollment"
-else
-	TARGET_PATH="$REPOSITORY/$TARGET_PREFIX"
-fi
-
-SOURCE_REV=""
-
-if test 0 -eq "$SOURCE_REVISION"
-then
-	SOURCE_REV=$(svn info https://svn.kuali.org/repos/student | grep Revision: | cut -d: -f 2 | sed 's/^\ //')
-else
-	SOURCE_REV=$SOURCE_REVISION
-fi
-
-# echo $SOURCE_REV
-# echo "TARGET_PATH=$TARGET_PATH"
-
 CMD_FILE=/tmp/$RANDOM.dat
 
 #echo "CMD_FILE=$CMD_FILE"
 
 printf "$SVNMUCC_CMD " > $CMD_FILE
 
-printf "cp $SOURCE_REV $SOURCE_PATH/ks-api/$API_SOURCE " >> $CMD_FILE
-
-if test $IN_BRANCH == "1"
-then
-	printf "    $TARGET_PATH/ks-api/branches/$AGGREGATE_NAME " >> $CMD_FILE
-
-else
-	printf "    $TARGET_PATH/$AGGREGATE_NAME/ks-api  " >> $CMD_FILE
-fi
+printf "rm $SOURCE_PATH/ks-api/$API_SOURCE " >> $CMD_FILE
 
 for M in $MODULES
 do
-	# printf "module = $M"
-	printf "cp $SOURCE_REV $SOURCE_PATH/$M/trunk " >> $CMD_FILE
-
 	if test $IN_BRANCH == "1"
 	then
-		printf "    $TARGET_PATH/$M/branches/$AGGREGATE_NAME " >> $CMD_FILE
+		printf "rm $SOURCE_PATH/$M/branches/$AGGREGATE_NAME " >> $CMD_FILE
 
 	else
-		printf "    $TARGET_PATH/$AGGREGATE_NAME/$M " >> $CMD_FILE
+		printf "rm $SOURCE_PATH/$AGGREGATE_NAME/$M " >> $CMD_FILE
 	fi
 
 done
