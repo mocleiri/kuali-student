02883c83ee7fb7e59e5031aaab6d7c74dff80f27
tbornholtz <tbornholtz@kuali.org>
2013-09-04 10:08:14 -0400

    KSA-238 - Display org name if it is populated
    git-svn-id: https://svn.kuali.org/repos/student/contrib/ksa/trunk@55199 0ac02d80-fb3f-11dc-a574-c57244a850eb

--- ksa-core/src/main/java/com/sigmasys/kuali/ksa/model/Account.java
+++ ksa-core/src/main/java/com/sigmasys/kuali/ksa/model/Account.java
@@ -1,26 +1,14 @@
 package com.sigmasys.kuali.ksa.model;
 
+import com.sigmasys.kuali.ksa.annotation.Auditable;
+import com.sigmasys.kuali.ksa.util.BeanUtils;
+
+import javax.persistence.*;
 import java.math.BigDecimal;
 import java.util.Date;
 import java.util.Set;
 
-import javax.persistence.CascadeType;
-import javax.persistence.Column;
-import javax.persistence.DiscriminatorColumn;
-import javax.persistence.Entity;
-import javax.persistence.FetchType;
-import javax.persistence.Id;
-import javax.persistence.Inheritance;
-import javax.persistence.InheritanceType;
-import javax.persistence.JoinColumn;
-import javax.persistence.JoinTable;
-import javax.persistence.ManyToMany;
-import javax.persistence.ManyToOne;
-import javax.persistence.Table;
-import javax.persistence.Transient;
-
-import com.sigmasys.kuali.ksa.annotation.Auditable;
-import com.sigmasys.kuali.ksa.util.BeanUtils;
+import static com.sigmasys.kuali.ksa.util.CommonUtils.nvl;
 
 /**
  * KSA Account model
@@ -379,12 +367,27 @@ public abstract class Account implements Identifiable {
     }
 
     /**
-     * Get lastName, firstName of the default PersonName Set record
+     * Get Organization - lastName, firstName of the default PersonName Set record
      */
     @Transient
     public String getCompositeDefaultPersonName() {
+        StringBuilder defaultNameBuilder = new StringBuilder();
+
+
         PersonName personName = getDefaultPersonName();
-        return (personName != null) ? personName.getDisplayValue() : "";
+        if(orgName != null) {
+            defaultNameBuilder.append(nvl(getOrgName().getName()));
+        }
+
+        if(orgName != null && personName != null) {
+            defaultNameBuilder.append(" - ");
+        }
+
+        if(personName != null) {
+            defaultNameBuilder.append(nvl(personName.getDisplayValue()));
+        }
+
+        return defaultNameBuilder.toString();
     }
 
     /**
