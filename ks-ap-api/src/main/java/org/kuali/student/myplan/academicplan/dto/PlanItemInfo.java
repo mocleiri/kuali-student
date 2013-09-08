371125ef86f56b03e790dcb0608005eaf5ff0d42
mwfyffe <mwfyffe@kuali.org>
2013-09-08 16:14:10 -0400

    SCT-5568 final pre-production milestone for 0.7.5
    git-svn-id: https://svn.kuali.org/repos/student/enrollment/ks-api/branches/KSAP-0.7-iu@55438 0ac02d80-fb3f-11dc-a574-c57244a850eb

--- ks-ap-api/src/main/java/org/kuali/student/myplan/academicplan/dto/PlanItemInfo.java
+++ ks-ap-api/src/main/java/org/kuali/student/myplan/academicplan/dto/PlanItemInfo.java
@@ -1,137 +1,153 @@
 package org.kuali.student.myplan.academicplan.dto;
 
-import org.kuali.student.myplan.academicplan.infc.PlanItem;
-import org.kuali.student.r2.common.dto.RichTextInfo;
-import org.kuali.student.r2.common.dto.TypeStateEntityInfo;
-import org.w3c.dom.Element;
-
-import javax.xml.bind.annotation.*;
-
 import java.math.BigDecimal;
 import java.util.ArrayList;
 import java.util.List;
 
+import javax.xml.bind.annotation.XmlAccessType;
+import javax.xml.bind.annotation.XmlAccessorType;
+import javax.xml.bind.annotation.XmlAnyElement;
+import javax.xml.bind.annotation.XmlAttribute;
+import javax.xml.bind.annotation.XmlElement;
+import javax.xml.bind.annotation.XmlType;
+
+import org.kuali.student.myplan.academicplan.infc.PlanItem;
+import org.kuali.student.r2.common.dto.AttributeInfo;
+import org.kuali.student.r2.common.dto.RichTextInfo;
+import org.kuali.student.r2.common.dto.TypeStateEntityInfo;
+import org.kuali.student.r2.common.infc.Attribute;
+import org.w3c.dom.Element;
+
 /**
  * PlanItem message structure
- *
- * @Author kmuthu
- * Date: 1/5/12
+ * 
+ * @Author kmuthu Date: 1/5/12
  */
 @XmlAccessorType(XmlAccessType.FIELD)
-@XmlType(name = "PlanItemInfo", propOrder = {"refObjectId", "refObjectType", "learningPlanId", "planPeriods", "id",
-        "credit", "typeKey", "stateKey", "descr", "meta", "attributes", "_futureElements"})
+@XmlType(name = "PlanItemInfo", propOrder = { "refObjectId", "refObjectType", "learningPlanId", "planPeriods", "id",
+		"credit", "typeKey", "stateKey", "descr", "meta", "attributes", "_futureElements" })
 public class PlanItemInfo extends TypeStateEntityInfo implements PlanItem {
 
- 	private static final long serialVersionUID = 7795677206429530520L;
+	private static final long serialVersionUID = 7795677206429530520L;
 
 	@XmlAttribute
-    private String id;
+	private String id;
 
-    @XmlElement
-    private RichTextInfo descr;
+	@XmlElement
+	private RichTextInfo descr;
 
-    @XmlElement
-    private String refObjectId;
+	@XmlElement
+	private String refObjectId;
 
-    @XmlElement
-    private String refObjectType;
+	@XmlElement
+	private String refObjectType;
 
-    @XmlElement
-    private String learningPlanId;
+	@XmlElement
+	private String learningPlanId;
 
-    @XmlElement
-    private List<String> planPeriods;
+	@XmlElement
+	private List<String> planPeriods;
 
-    @XmlElement
-    private BigDecimal credit;
+	@XmlElement
+	private BigDecimal credit;
 
-    @SuppressWarnings("unused")
+	@SuppressWarnings("unused")
 	@XmlAnyElement
-    private List<Element> _futureElements;
+	private List<Element> _futureElements;
 
-    public PlanItemInfo() {
-        this.id = null;
-        this.descr = null;
-        this.refObjectId = null;
-        this.refObjectType = null;
-        this.learningPlanId = null;
-        this.planPeriods = new ArrayList<String>();
-        this._futureElements = null;
-    }
+	public PlanItemInfo() {
+		this.id = null;
+		this.descr = null;
+		this.refObjectId = null;
+		this.refObjectType = null;
+		this.learningPlanId = null;
+		this.planPeriods = new ArrayList<String>();
+		this._futureElements = null;
+	}
 
-    public PlanItemInfo(PlanItem item) {
-        super(item);
+	public PlanItemInfo(PlanItem item) {
+		super(item);
 
-        if(null != item) {
-            this.id = item.getId();
-            this.refObjectId = item.getRefObjectId();
-            this.refObjectType = item.getRefObjectType();
-            this.learningPlanId = item.getLearningPlanId();
+		if (null != item) {
+			this.id = item.getId();
+			this.setTypeKey(item.getTypeKey());
+			this.setStateKey(item.getStateKey());
+			this.refObjectId = item.getRefObjectId();
+			this.refObjectType = item.getRefObjectType();
+			this.learningPlanId = item.getLearningPlanId();
+			this.credit = item.getCredit();
 
-            if(null != item.getPlanPeriods()) {
-                for(String atpId : item.getPlanPeriods()) {
-                    this.planPeriods.add(atpId);
-                }
-            }
+			List<String> planPeriods = item.getPlanPeriods();
+			if (null != planPeriods) {
+				this.planPeriods = new ArrayList<String>(planPeriods);
+			}
 
-            this.descr = (null != item.getDescr()) ? new RichTextInfo(item.getDescr()) : null;
+			this.descr = (null != item.getDescr()) ? new RichTextInfo(item.getDescr()) : null;
 
-        }
-    }
+			List<? extends Attribute> attrs = item.getAttributes();
+			if (attrs != null) {
+				List<AttributeInfo> attrInfos = new ArrayList<AttributeInfo>(attrs.size());
+				for (Attribute attr : attrs) {
+					attrInfos.add(new AttributeInfo(attr));
+				}
+				this.setAttributes(attrInfos);
+			}
+		}
+	}
 
-    public String getId() {
-        return id;
-    }
+	public String getId() {
+		return id;
+	}
 
-    public void setId(String id) {
-        this.id = id;
-    }
+	public void setId(String id) {
+		this.id = id;
+	}
 
-    public RichTextInfo getDescr() {
-        return descr;
-    }
+	public RichTextInfo getDescr() {
+		return descr;
+	}
 
-    public void setDescr(RichTextInfo descr) {
-        this.descr = descr;
-    }
+	public void setDescr(RichTextInfo descr) {
+		this.descr = descr;
+	}
 
-    public String getRefObjectId() {
-        return refObjectId;
-    }
+	public String getRefObjectId() {
+		return refObjectId;
+	}
 
-    public void setRefObjectId(String refObjectId) {
-        this.refObjectId = refObjectId;
-    }
+	public void setRefObjectId(String refObjectId) {
+		this.refObjectId = refObjectId;
+	}
 
-    public String getRefObjectType() {
-        return refObjectType;
-    }
+	public String getRefObjectType() {
+		return refObjectType;
+	}
 
-    public void setRefObjectType(String refObjectType) {
-        this.refObjectType = refObjectType;
-    }
+	public void setRefObjectType(String refObjectType) {
+		this.refObjectType = refObjectType;
+	}
 
-    public String getLearningPlanId() {
-        return learningPlanId;
-    }
+	public String getLearningPlanId() {
+		return learningPlanId;
+	}
 
-    public void setLearningPlanId(String learningPlanId) {
-        this.learningPlanId = learningPlanId;
-    }
+	public void setLearningPlanId(String learningPlanId) {
+		this.learningPlanId = learningPlanId;
+	}
 
-    public List<String> getPlanPeriods() {
-        return planPeriods;
-    }
+	public List<String> getPlanPeriods() {
+		return planPeriods;
+	}
 
-    public void setPlanPeriods(List<String> planPeriods) {
-        this.planPeriods = planPeriods;
-    }
+	public void setPlanPeriods(List<String> planPeriods) {
+		this.planPeriods = planPeriods;
+	}
 
-     public BigDecimal getCredit() {
-        return credit;
-    }
+	public BigDecimal getCredit() {
+		return credit;
+	}
 
-    public void setCredit(BigDecimal credit) {
-         this.credit = credit;
-    }
+	public void setCredit(BigDecimal credit) {
+		this.credit = credit;
+	}
 }
