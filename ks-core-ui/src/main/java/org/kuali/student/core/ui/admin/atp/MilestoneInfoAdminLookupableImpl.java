b9ac2c1d1fef7dc2db3a07ba54a82ce401a714d1
nwright <nwright@kuali.org>
2013-09-10 12:58:25 -0400

    KSCM-1070 aligned manual changes with generator and re-generated
    git-svn-id: https://svn.kuali.org/repos/student/contrib/CM/ks-core/branches/org-service@55590 0ac02d80-fb3f-11dc-a574-c57244a850eb

--- ks-core-ui/src/main/java/org/kuali/student/core/ui/admin/atp/MilestoneInfoAdminLookupableImpl.java
+++ ks-core-ui/src/main/java/org/kuali/student/core/ui/admin/atp/MilestoneInfoAdminLookupableImpl.java
@@ -1,9 +1,9 @@
 /*
- * Copyright 2011 The Kuali Foundation
+ * Copyright 2013 The Kuali Foundation
  *
  * Licensed under the Educational Community License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
- * You may	obtain a copy of the License at
+ * You may obtain a copy of the License at
  *
  * 	http://www.osedu.org/licenses/ECL-2.0
  *
@@ -38,7 +38,8 @@ public class MilestoneInfoAdminLookupableImpl extends LookupableImpl
 {
 	private static final Logger LOG = Logger.getLogger(MilestoneInfoAdminLookupableImpl.class);
 	private transient AtpService atpService;
-    private static final long serialVersionUID = 1L;
+	private static final long serialVersionUID = 1L;
+
 	@Override
 	protected List<MilestoneInfo> getSearchResults(LookupForm lookupForm, Map<String, String> fieldValues, boolean unbounded)
 	{
