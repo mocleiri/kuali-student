dfa2b8ad46966da2adbc287f36b1a732939897a5
nwright <nwright@kuali.org>
2013-09-10 15:38:21 -0400

    KSCM-1070 Got baseline org screens working!!!!!
    git-svn-id: https://svn.kuali.org/repos/student/contrib/CM/ks-core/branches/org-service@55621 0ac02d80-fb3f-11dc-a574-c57244a850eb

---
+++ ks-core-ui/src/main/java/org/kuali/student/core/ui/admin/organization/OrgInfoAdminInquirableImpl.java
@@ -0,0 +1,73 @@
+/*
+ * Copyright 2013 The Kuali Foundation
+ *
+ * Licensed under the Educational Community License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * 	http://www.osedu.org/licenses/ECL-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.kuali.student.core.ui.admin.organization;
+
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Map;
+import javax.xml.namespace.QName;
+import org.apache.log4j.Logger;
+import org.kuali.rice.core.api.resourceloader.GlobalResourceLoader;
+import org.kuali.rice.krad.inquiry.InquirableImpl;
+import org.kuali.student.common.util.ContextBuilder;
+import org.kuali.student.r2.common.dto.ContextInfo;
+import org.kuali.student.r2.core.constants.OrganizationServiceConstants;
+import org.kuali.student.r2.core.organization.dto.OrgInfo;
+import org.kuali.student.r2.core.organization.service.OrganizationService;
+
+
+public class OrgInfoAdminInquirableImpl extends InquirableImpl
+{
+	private static final Logger LOG = Logger.getLogger(OrgInfoAdminInquirableImpl.class);
+	private transient OrganizationService organizationService;
+	private final static String PRIMARY_KEY = "id";
+	private static final long serialVersionUID = 1L;
+
+	@Override
+	public OrgInfo retrieveDataObject(Map<String, String> parameters)
+	{
+		String key = parameters.get(PRIMARY_KEY);
+		try
+		{
+			OrgInfo info = this.getOrganizationService().getOrg(key, getContextInfo());
+			return info;
+		}
+		catch (Exception ex) {
+		    throw new RuntimeException(ex);
+		}
+	}
+
+	public void setOrganizationService(OrganizationService organizationService)
+	{
+		    this.organizationService = organizationService;
+	}
+
+	public OrganizationService getOrganizationService()
+	{
+		if (organizationService == null)
+		{
+			QName qname = new QName(OrganizationServiceConstants.NAMESPACE,OrganizationServiceConstants.SERVICE_NAME_LOCAL_PART);
+			organizationService = (OrganizationService) GlobalResourceLoader.getService(qname);
+		}
+		return this.organizationService;
+	}
+
+	private ContextInfo getContextInfo() {
+	    return ContextBuilder.loadContextInfo();
+	}
+}
+
---
+++ ks-core-ui/src/main/java/org/kuali/student/core/ui/admin/organization/OrgPositionRestrictionInfoAdminLookupableImpl.java
@@ -0,0 +1,94 @@
+/*
+ * Copyright 2013 The Kuali Foundation
+ *
+ * Licensed under the Educational Community License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * 	http://www.osedu.org/licenses/ECL-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.kuali.student.core.ui.admin.organization;
+
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Map;
+import javax.xml.namespace.QName;
+import org.apache.log4j.Logger;
+import org.kuali.rice.core.api.criteria.Predicate;
+import org.kuali.rice.core.api.criteria.PredicateFactory;
+import org.kuali.rice.core.api.criteria.QueryByCriteria;
+import org.kuali.rice.core.api.resourceloader.GlobalResourceLoader;
+import org.kuali.rice.krad.lookup.LookupableImpl;
+import org.kuali.rice.krad.web.form.LookupForm;
+import org.kuali.student.common.util.ContextBuilder;
+import org.kuali.student.r2.common.dto.ContextInfo;
+import org.kuali.student.r2.core.constants.OrganizationServiceConstants;
+import org.kuali.student.r2.core.organization.dto.OrgPositionRestrictionInfo;
+import org.kuali.student.r2.core.organization.service.OrganizationService;
+
+
+public class OrgPositionRestrictionInfoAdminLookupableImpl extends LookupableImpl
+{
+	private static final Logger LOG = Logger.getLogger(OrgPositionRestrictionInfoAdminLookupableImpl.class);
+	private transient OrganizationService organizationService;
+	private static final long serialVersionUID = 1L;
+
+	@Override
+	protected List<OrgPositionRestrictionInfo> getSearchResults(LookupForm lookupForm, Map<String, String> fieldValues, boolean unbounded)
+	{
+		QueryByCriteria.Builder qBuilder = QueryByCriteria.Builder.create();
+		List<Predicate> pList = new ArrayList<Predicate>();
+		for (String fieldName : fieldValues.keySet())
+		{
+			String value = fieldValues.get(fieldName);
+			if (value != null && !value.isEmpty())
+			{
+				if (fieldName.equals("maxResultsToReturn"))
+				{
+					qBuilder.setMaxResults (Integer.parseInt(value));
+					continue;
+				}
+				pList.add(PredicateFactory.equal(fieldName, value));
+			}
+		}
+		if (!pList.isEmpty())
+		{
+			qBuilder.setPredicates(PredicateFactory.and(pList.toArray(new Predicate[pList.size()])));
+		}
+		try
+		{
+			List<OrgPositionRestrictionInfo> list = this.getOrganizationService().searchForOrgPositionRestrictions(qBuilder.build(), getContextInfo());
+			return list;
+		}
+		catch (Exception ex) {
+		    throw new RuntimeException(ex);
+		}
+	}
+
+	public void setOrganizationService(OrganizationService organizationService)
+	{
+		    this.organizationService = organizationService;
+	}
+
+	public OrganizationService getOrganizationService()
+	{
+		if (organizationService == null)
+		{
+			QName qname = new QName(OrganizationServiceConstants.NAMESPACE,OrganizationServiceConstants.SERVICE_NAME_LOCAL_PART);
+			organizationService = (OrganizationService) GlobalResourceLoader.getService(qname);
+		}
+		return this.organizationService;
+	}
+
+	private ContextInfo getContextInfo() {
+	    return ContextBuilder.loadContextInfo();
+	}
+}
+
