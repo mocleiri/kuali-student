d15a60e73cd841f3dd3182bd3c2c21e36d9f9bbf
jannie <jannie@kuali.org>
2013-09-12 04:43:41 -0400

    KSLAB-2681 : Replace hard coded values with Message Catalogue lookups
    git-svn-id: https://svn.kuali.org/repos/student/contrib/CM/ks-lum/trunk@55773 0ac02d80-fb3f-11dc-a574-c57244a850eb

---
+++ ks-lum-sql/src/main/resources/upgrades/18-institute-customization/2013-03-08-ks-cm-messages.sql
@@ -0,0 +1,7 @@
+INSERT INTO KSMG_MESSAGE (ID, MSG_ID, LOCALE, GRP_NAME, MSG_VALUE, OBJ_ID, VER_NBR) 
+VALUES   (SYS_GUID(), 'cluSetAddCourseLabel', 'en', 'clusetmanagement', '<b>Add a course, course set, or course range</b>', SYS_GUID(), '1')
+/
+
+INSERT INTO KSMG_MESSAGE (ID, MSG_ID, LOCALE, GRP_NAME, MSG_VALUE, OBJ_ID, VER_NBR) 
+VALUES   (SYS_GUID(), 'cluSetManagementCourse-instruct', 'en', 'clusetmanagement', 'Add courses, course sets, or course ranges to your course set. You may <br/>add any combination of courses, dynamic course ranges, or Course sets.', SYS_GUID(), '1')
+/
