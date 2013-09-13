80844acef5b6e33f58f51e9e0a5ea6d70c0b0ec7
nwright <nwright@kuali.org>
2013-09-13 16:11:47 -0400

    KSCM-1075 Configured Org maintenance roles in RICE adding them to constants and copied over decorators that we will need for permission checks (changing package because they were enrollment!)
    git-svn-id: https://svn.kuali.org/repos/student/contrib/CM/ks-core/branches/org-service@55918 0ac02d80-fb3f-11dc-a574-c57244a850eb

---
+++ ks-core-impl/src/main/java/org/kuali/student/kim/permission/mock/KimPermissionConstants.java
@@ -0,0 +1,44 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package org.kuali.student.kim.permission.mock;
+
+/**
+ *
+ * @author nwright
+ */
+public class KimPermissionConstants {
+
+    /**
+     * Namespaces
+     */
+    public static final String KS_ENROLLMENT_NAMESPACE = "KS-ENRL";
+    public static final String KS_SYS_NAMESPACE = "KS-SYS";
+    public static final String KS_LUM_NAMESPACE = "KS-LUM";
+    public static final String KUALI_NAMESPACE = "KUALI";
+    public static final String KR_WORKFLOW_NAMESPACE = "KR-WKFLW";
+    public static final String KR_IDM_NAMESPACE = "KR-IDM";
+    /**
+     * org permission templates
+     */
+    public static final String CAN_INVOKE_SERVICE_METHOD_TEMPLATE_NAME = "Can Invoke Service Method";
+    /**
+     * org permissions
+     */
+    public static final String CAN_INVOKE_SERVICE_METHOD_READ_ORGANIZATION_DATA_PERMISSION = "Can Invoke Service Method Read Organization Data";
+    public static final String CAN_INVOKE_SERVICE_METHOD_UPDATE_ORGANIZATION_DATA_PERMISSION = "Can Invoke Service Method Update Organization Data";
+    public static final String CAN_INVOKE_SERVICE_METHOD_DELETE_ORGANIZATION_DATA_PERMISSION = "Can Invoke Service Method Delete Organization Data";
+    public static final String CAN_INVOKE_SERVICE_METHOD_READ_ORGANIZATION_HIERARCHY_DATA_PERMISSION = "Can Invoke Service Method Read Organization Hierarchy Data";
+    public static final String CAN_INVOKE_SERVICE_METHOD_UPDATE_ORGANIZATION_HIERARCHY_DATA_PERMISSION = "Can Invoke Service Method Update Organization Hierarchy Data";
+    public static final String CAN_INVOKE_SERVICE_METHOD_DELETE_ORGANIZATION_HIERARCHY_DATA_PERMISSION = "Can Invoke Service Method Delete Organization Hierarchy Data";
+    public static final String CAN_INVOKE_SERVICE_METHOD_READ_ORGANIZATION_PERSON_RELATION_DATA_PERMISSION = "Can Invoke Service Method Read Organization Person Relation Data";
+    public static final String CAN_INVOKE_SERVICE_METHOD_UPDATE_ORGANIZATION_PERSON_RELATION_DATA_PERMISSION = "Can Invoke Service Method Update Organization Person Relation Data";
+    public static final String CAN_INVOKE_SERVICE_METHOD_DELETE_ORGANIZATION_PERSON_RELATION_DATA_PERMISSION = "Can Invoke Service Method Delete Organization Person Relation Data";
+   
+    /**
+     * org role constants
+     */
+    public static final String KS_ORG_MAINTENANCE_ROLE_NAME = "KS Org Maintenance";
+    public static final String KS_ORG_VIEW_ROLE_NAME = "KS Org View";
+}
