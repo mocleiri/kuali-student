4bcf2fb308ac5dbf36978af806e1f9d8cb803dc1
nwright <nwright@kuali.org>
2013-09-17 19:28:49 -0400

    KSCM-1063 copied over find and replace code customized for KS
    git-svn-id: https://svn.kuali.org/repos/student/tools@56088 0ac02d80-fb3f-11dc-a574-c57244a850eb

--- run-maven-put-jars-into-local-repository.cmd
+++
@@ -1,4 +0,0 @@
-echo this will put the j6plugin jar into the local repository
-pause
-call D:\apache\apache-maven-3.0.4\bin\mvn install:install-file -DgroupId=net.pandoragames -DartifactId=j6plugin -Dversion=0.2 -Dpackaging=jar -DgeneratePom=true -Dfile=j6plugin-0.2.jar
-pause
