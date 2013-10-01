b2a28693893d8f306d5561299c827e308c07725c
lprzybylski <lprzybylski@kuali.org>
2013-10-01 01:38:37 -0400

    KSCM-1137 adding missing constant entry back. We should investigate why this was removed to begin with
    git-svn-id: https://svn.kuali.org/repos/student/enrollment/ks-api/branches/CM-KRAD-rice-2.3.0@56908 0ac02d80-fb3f-11dc-a574-c57244a850eb

--- ks-core-api/src/main/java/org/kuali/student/r2/core/constants/EnumerationManagementServiceConstants.java
+++ ks-core-api/src/main/java/org/kuali/student/r2/core/constants/EnumerationManagementServiceConstants.java
@@ -32,5 +32,6 @@ public class EnumerationManagementServiceConstants {
     public static final String NAMESPACE = CommonServiceConstants.REF_OBJECT_URI_GLOBAL_PREFIX + "enumerationmanagement";
     public static final String REF_OBJECT_URI_ENUMERATION_MANAGEMENT = NAMESPACE + "/" + EnumerationInfo.class.getSimpleName();
     public static final String REF_OBJECT_URI_ENUMERATION_MANAGEMENT_VALUE = NAMESPACE + "/" + EnumeratedValueInfo.class.getSimpleName();
+    public static final String SERVICE_NAME_LOCAL_PART = "EnumerationManagementService";
 
 }
