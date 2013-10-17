2a797c0fc1f4b5104cd901329e266df91bacf003
haroon.rafique <haroon.rafique@kuali.org>
2013-10-17 16:21:02 -0400

    KSCM-1162: sync SQL directory structure between contrib and CM-2.0 * cosmetic - removed trailing spaces
    git-svn-id: https://svn.kuali.org/repos/student/contrib/CM/ks-lum/trunk@58278 0ac02d80-fb3f-11dc-a574-c57244a850eb

--- ks-lum-sql/src/main/resources/upgrades/18-CM-2.0.2/2013-09-12-message-catalogue.sql
+++ ks-lum-sql/src/main/resources/upgrades/18-CM-2.0.2/2013-09-12-message-catalogue.sql
@@ -1,32 +1,32 @@
-insert into KSMG_MESSAGE (ID, MSG_ID, LOCALE, GRP_NAME, MSG_VALUE, OBJ_ID, VER_NBR) 
+insert into KSMG_MESSAGE (ID, MSG_ID, LOCALE, GRP_NAME, MSG_VALUE, OBJ_ID, VER_NBR)
 values (sys_guid(), 'versionBackButtonLabel', 'en', 'course', 'Return to Current Course', sys_guid(), 1)
 /
 insert into KSMG_MESSAGE (ID, MSG_ID, LOCALE, GRP_NAME, MSG_VALUE, OBJ_ID, VER_NBR)
 values (sys_guid(), 'browseCourseCatalogLabel', 'en', 'course', 'Browse Course Catalog', sys_guid(), 1)
 /
-insert into KSMG_MESSAGE (ID, MSG_ID, LOCALE, GRP_NAME, MSG_VALUE, OBJ_ID, VER_NBR) 
+insert into KSMG_MESSAGE (ID, MSG_ID, LOCALE, GRP_NAME, MSG_VALUE, OBJ_ID, VER_NBR)
 values (sys_guid(), 'browseCourseCatalogueByTabLabel', 'en', 'course', 'Browse by Subject Area', sys_guid(), 1)
 /
-INSERT INTO KSMG_MESSAGE (ID, MSG_ID, LOCALE, GRP_NAME, MSG_VALUE, OBJ_ID, VER_NBR) 
+INSERT INTO KSMG_MESSAGE (ID, MSG_ID, LOCALE, GRP_NAME, MSG_VALUE, OBJ_ID, VER_NBR)
 VALUES   (SYS_GUID(), 'dependencyResultPanelCourseDependencyLabel', 'en', 'course', 'Course Dependencies', SYS_GUID(), '1')
 /
-INSERT INTO KSMG_MESSAGE (ID, MSG_ID, LOCALE, GRP_NAME, MSG_VALUE, OBJ_ID, VER_NBR) 
+INSERT INTO KSMG_MESSAGE (ID, MSG_ID, LOCALE, GRP_NAME, MSG_VALUE, OBJ_ID, VER_NBR)
 VALUES   (SYS_GUID(), 'dependencyAnalysisHyperlinkCourse', 'en', 'course', 'View Course', SYS_GUID(), '1')
 /
-INSERT INTO KSMG_MESSAGE (ID, MSG_ID, LOCALE, GRP_NAME, MSG_VALUE, OBJ_ID, VER_NBR) 
+INSERT INTO KSMG_MESSAGE (ID, MSG_ID, LOCALE, GRP_NAME, MSG_VALUE, OBJ_ID, VER_NBR)
 VALUES   (SYS_GUID(), 'course', 'en', 'course', 'course', SYS_GUID(), '1')
 /
-INSERT INTO KSMG_MESSAGE (ID, MSG_ID, LOCALE, GRP_NAME, MSG_VALUE, OBJ_ID, VER_NBR) 
+INSERT INTO KSMG_MESSAGE (ID, MSG_ID, LOCALE, GRP_NAME, MSG_VALUE, OBJ_ID, VER_NBR)
 VALUES   (SYS_GUID(), 'browseBySchool', 'en', 'course', 'Browse by School', SYS_GUID(), '1')
 /
-INSERT INTO KSMG_MESSAGE (ID, MSG_ID, LOCALE, GRP_NAME, MSG_VALUE, OBJ_ID, VER_NBR) 
+INSERT INTO KSMG_MESSAGE (ID, MSG_ID, LOCALE, GRP_NAME, MSG_VALUE, OBJ_ID, VER_NBR)
 VALUES   (SYS_GUID(), 'cluSetAddCourseLabel', 'en', 'clusetmanagement', '<b>Add a course, course set, or course range</b>', SYS_GUID(), '1')
 /
 
-INSERT INTO KSMG_MESSAGE (ID, MSG_ID, LOCALE, GRP_NAME, MSG_VALUE, OBJ_ID, VER_NBR) 
+INSERT INTO KSMG_MESSAGE (ID, MSG_ID, LOCALE, GRP_NAME, MSG_VALUE, OBJ_ID, VER_NBR)
 VALUES   (SYS_GUID(), 'cluSetManagementCourse-instruct', 'en', 'clusetmanagement', 'Add courses, course sets, or course ranges to your course set. You may <br/>add any combination of courses, dynamic course ranges, or Course sets.', SYS_GUID(), '1')
 /
-INSERT INTO KSMG_MESSAGE (ID, MSG_ID, LOCALE, GRP_NAME, MSG_VALUE, OBJ_ID, VER_NBR) 
+INSERT INTO KSMG_MESSAGE (ID, MSG_ID, LOCALE, GRP_NAME, MSG_VALUE, OBJ_ID, VER_NBR)
 VALUES   (SYS_GUID(), 'courseCatalogBreadcrumb', 'en', 'course', 'Course Catalog', SYS_GUID(), '1')
 /
 
