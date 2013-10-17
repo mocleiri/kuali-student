00543bce3e952d870da43ba4c1a57acaa8d4bd81
godunovs <godunovs@kuali.org>
2013-10-17 01:24:04 -0400

    1. Added default object validation. Only one default object of each type must be selected. Error is displayed if a single object is not selected. 2. Added object removal support in controller.
    Issue ID: KSA-352
    
    git-svn-id: https://svn.kuali.org/repos/student/contrib/ksa/trunk@58209 0ac02d80-fb3f-11dc-a574-c57244a850eb

--- ksa-core/src/main/java/com/sigmasys/kuali/ksa/krad/controller/BiographicController.java
+++ ksa-core/src/main/java/com/sigmasys/kuali/ksa/krad/controller/BiographicController.java
@@ -1,14 +1,17 @@
 package com.sigmasys.kuali.ksa.krad.controller;
 
 import com.sigmasys.kuali.ksa.krad.form.BiographicForm;
-import com.sigmasys.kuali.ksa.model.Account;
-import com.sigmasys.kuali.ksa.model.ElectronicContact;
-import com.sigmasys.kuali.ksa.model.PersonName;
-import com.sigmasys.kuali.ksa.model.PostalAddress;
+import com.sigmasys.kuali.ksa.model.*;
+import com.sigmasys.kuali.ksa.service.PersistenceService;
+import org.apache.commons.collections.CollectionUtils;
+import org.apache.commons.lang.BooleanUtils;
+import org.apache.commons.lang.ObjectUtils;
+import org.apache.commons.lang.math.NumberUtils;
 import org.apache.commons.logging.Log;
 import org.apache.commons.logging.LogFactory;
 import org.kuali.rice.core.api.util.RiceKeyConstants;
 import org.kuali.rice.krad.util.GlobalVariables;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -16,6 +19,7 @@ import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.servlet.ModelAndView;
 
 import javax.servlet.http.HttpServletRequest;
+import java.lang.reflect.Method;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -27,6 +31,9 @@ import java.util.List;
 public class BiographicController extends GenericSearchController {
 
     private static final Log logger = LogFactory.getLog(BiographicController.class);
+
+    @Autowired
+    private PersistenceService persistenceService;
 
     /**
      * @see org.kuali.rice.krad.web.controller.UifControllerBase#createInitialForm(javax.servlet.http.HttpServletRequest)
@@ -89,31 +96,56 @@ public class BiographicController extends GenericSearchController {
 
         try {
 
+            // Get all objects from the screen and validate single Default object selection:
             List<PersonName> personList = form.getPersonNameList();
+            List<PostalAddress> addressList = form.getPostalAddressList();
+            List<ElectronicContact> contactList = form.getElectronicContactList();
+
+            assertSingleDefaultSelection(personList, "Person Name");
+            assertSingleDefaultSelection(addressList, "Postal Address");
+            assertSingleDefaultSelection(contactList, "Electronic Contact");
+
+            // Define arrays objects to be deleted:
+            List<PersonName> namesToDelete = new ArrayList<PersonName>(account.getPersonNames());
+            List<PostalAddress> postalAddressesToDelete = new ArrayList<PostalAddress>(account.getPostalAddresses());
+            List<ElectronicContact> electronicContactsToDelete = new ArrayList<ElectronicContact>(account.getElectronicContacts());
+
+            // Persist PersonNames
             for (PersonName person : personList) {
                 if (person.getId() == null) {
                     accountService.addPersonName(userId, person);
                 } else {
                     accountService.persistPersonName(person);
+                    removeById(namesToDelete, person);
                 }
             }
 
-            List<PostalAddress> addressList = form.getPostalAddressList();
+            // Persist PostalAddresses
             for (PostalAddress address : addressList) {
                 if (address.getId() == null) {
                     accountService.addPostalAddress(userId, address);
                 } else {
                     accountService.persistPostalAddress(address);
+                    removeById(postalAddressesToDelete, address);
                 }
             }
 
-            List<ElectronicContact> contactList = form.getElectronicContactList();
+            // Persist ElectronicContacts:
             for (ElectronicContact contact : contactList) {
                 if (contact.getId() == null) {
                     accountService.addElectronicContact(userId, contact);
                 } else {
                     accountService.persistElectronicContact(contact);
+                    removeById(electronicContactsToDelete, contact);
                 }
+            }
+
+            // Delete objects:
+            if (!namesToDelete.isEmpty() || !postalAddressesToDelete.isEmpty() || !electronicContactsToDelete.isEmpty()) {
+                account.getPersonNames().removeAll(namesToDelete);
+                account.getPostalAddresses().removeAll(postalAddressesToDelete);
+                account.getElectronicContacts().removeAll(electronicContactsToDelete);
+                persistenceService.persistEntity(account);
             }
 
             GlobalVariables.getMessageMap().putInfo("BiographicView", RiceKeyConstants.ERROR_CUSTOM, "Saved");
@@ -124,4 +156,60 @@ public class BiographicController extends GenericSearchController {
         return getUIFModelAndView(form);
     }
 
+    private <T extends Identifiable> void removeById(List<T> list, Identifiable toRemove) {
+        for (int i=0,sz=list.size(); i<sz; i++) {
+            if (ObjectUtils.equals(list.get(i).getId(), toRemove.getId())) {
+                list.remove(i);
+                return;
+            }
+        }
+    }
+
+    private <T> void assertSingleDefaultSelection(List<T> objects, String objectType) {
+        int count = 0;
+
+        for (int i=0,sz=objects.size(); (i<sz) && (count<2); i++) {
+            DefaultMethodHolder dmh = toDefaultMethodHolder(objects.get(i));
+            if ((dmh != null) && BooleanUtils.isTrue(dmh.isDefault())) {
+                count++;
+            }
+        }
+
+        if (count > 1) {
+            String errorMsg = String.format("Only one default %s may be specified.", objectType);
+            logger.error(errorMsg);
+            throw new IllegalStateException(errorMsg);
+        }
+
+        if (count < 1) {
+            String errorMsg = String.format("One default %s must be specified.", objectType);
+            logger.error(errorMsg);
+            throw new IllegalStateException(errorMsg);
+        }
+    }
+
+    private interface DefaultMethodHolder {
+        Boolean isDefault();
+    }
+    private class PersonNameAdapter implements DefaultMethodHolder {
+        private PersonName adaptee;
+        public PersonNameAdapter(PersonName pn) { adaptee = pn; }
+        public Boolean isDefault() { return adaptee.isDefault(); }
+    }
+    private class PostalAddressAdapter implements DefaultMethodHolder {
+        private PostalAddress adaptee;
+        public PostalAddressAdapter(PostalAddress pa) { adaptee = pa; }
+        public Boolean isDefault() { return adaptee.isDefault(); }
+    }
+    private class ElectronicContactAdapter implements DefaultMethodHolder {
+        private ElectronicContact adaptee;
+        public ElectronicContactAdapter(ElectronicContact ec) { adaptee = ec; }
+        public Boolean isDefault() { return adaptee.isDefault(); }
+    }
+    private DefaultMethodHolder toDefaultMethodHolder(Object o) {
+        if (o instanceof PersonName) { return new PersonNameAdapter((PersonName)o); }
+        else if (o instanceof ElectronicContact) { return new ElectronicContactAdapter((ElectronicContact)o); }
+        else if (o instanceof PostalAddress) { return new PostalAddressAdapter((PostalAddress)o); }
+        else return null;
+    }
 }
