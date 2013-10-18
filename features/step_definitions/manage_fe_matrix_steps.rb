ccc22ea5e5c86a80a9f7d5072dd233caeee91b6a
llfourie <llfourie@kuali.org>
2013-10-18 05:56:55 -0400

    KSENROLL-9793 : Create AFT for FE 7.3 - Updated wording of the step defs as suggested in the code review
    git-svn-id: https://svn.kuali.org/repos/student/test/functional-automation/sambal/trunk@58318 0ac02d80-fb3f-11dc-a574-c57244a850eb

--- features/step_definitions/manage_fe_matrix_steps.rb
+++ features/step_definitions/manage_fe_matrix_steps.rb
@@ -1,4 +1,4 @@
-When /^I edit the TH at 06:00 AM Standard Final Exam rule on the matrix$/ do
+When /^I edit a Standard Final Exam rule on the matrix$/ do
   @matrix = make FinalExamMatrix
   @matrix.edit :defer_save => true
 end
@@ -32,12 +32,9 @@ end
 
 Then /^I should be able to choose any one of Day 1 to 6 for the rule$/ do
   on FEMatrixEdit do |page|
-    page.fe_days_select.option( value: "1").text.should == "Day 1"
-    page.fe_days_select.option( value: "2").text.should == "Day 2"
-    page.fe_days_select.option( value: "3").text.should == "Day 3"
-    page.fe_days_select.option( value: "4").text.should == "Day 4"
-    page.fe_days_select.option( value: "5").text.should == "Day 5"
-    page.fe_days_select.option( value: "6").text.should == "Day 6"
+    for i in 1..6
+      page.rdl_days.option( value: "#{i}").text.should == "Day #{i}"
+    end
   end
 end
 
