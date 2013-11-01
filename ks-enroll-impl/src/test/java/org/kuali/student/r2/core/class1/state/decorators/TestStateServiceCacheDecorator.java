81cfa5d22623e92d3243569a6b0e2d72d22c4103
michael.ocleirigh <michael.ocleirigh@kuali.org>
2013-11-01 08:58:16 -0400

    KSENROLL-10560 fix StateServiceMockDataImpl in upgrade branch
    git-svn-id: https://svn.kuali.org/repos/student/enrollment/ks-enroll/branches/2.0.0-m9-api-upgrade@59298 0ac02d80-fb3f-11dc-a574-c57244a850eb

--- ks-enroll-impl/src/test/java/org/kuali/student/r2/core/class1/state/decorators/TestStateServiceCacheDecorator.java
+++ ks-enroll-impl/src/test/java/org/kuali/student/r2/core/class1/state/decorators/TestStateServiceCacheDecorator.java
@@ -4,8 +4,6 @@
  */
 package org.kuali.student.r2.core.class1.state.decorators;
 
-import static org.junit.Assert.assertEquals;
-
 import org.junit.After;
 import org.junit.AfterClass;
 import org.junit.Before;
@@ -13,11 +11,9 @@ import org.junit.BeforeClass;
 import org.junit.Test;
 import org.kuali.rice.core.api.criteria.QueryByCriteria;
 import org.kuali.student.r2.common.dto.ContextInfo;
-import org.kuali.student.r2.common.dto.RichTextInfo;
-import org.kuali.student.r2.core.class1.state.dto.LifecycleInfo;
-import org.kuali.student.r2.core.class1.state.dto.StateInfo;
-import org.kuali.student.r2.core.class1.state.mock.StateServiceMockImpl;
-import org.kuali.student.r2.core.class1.state.service.StateService;
+import org.kuali.student.r2.core.class1.state.mock.StateServiceMockDataImpl;
+
+import static org.junit.Assert.assertEquals;
 
 /**
  *
@@ -53,10 +49,7 @@ public class TestStateServiceCacheDecorator {
         ContextInfo contextInfo = new ContextInfo ();
         contextInfo.setPrincipalId("TESTUSER");
         
-        StateServiceMockImpl mock = new StateServiceMockImpl ();
-        
-        setupStateServiceData(mock);
-        
+        StateServiceMockDataImpl mock = new StateServiceMockDataImpl ();
         StateServiceCacheDecorator instance = new StateServiceCacheDecorator();
         instance.setNextDecorator(mock);
         
@@ -78,48 +71,6 @@ public class TestStateServiceCacheDecorator {
         assertEquals (mock.searchForLifecycles(criteria.build(), contextInfo), instance.searchForLifecycles(criteria.build(), contextInfo));
         assertEquals (mock.searchForLifecycles(criteria.build(), contextInfo), instance.searchForLifecycles(criteria.build(), contextInfo));
         
-    }
-
-	private void setupStateServiceData(StateService service) throws Exception {
-		
-		 addLifecycle(service, "kuali.atp.process", "ATP Lifecycle", "Lifecycle process for Academic Time Periods.", "");
-		         addState(service, "kuali.atp.process", "kuali.atp.state.Draft", "Draft", "Indicates that this ATP is tentative.");
-		         addState(service, "kuali.atp.process", "kuali.atp.state.Official", "Official", "Indicates that this ATP has been established.");
-		
-		         addLifecycle(service, "kuali.milestone.process", "Milestone Lifecycle", "Lifecycle process for Milestones.", "");
-		        addState(service, "kuali.milestone.process", "kuali.milestone.state.Draft", "Draft", "Indicates that this Milestone is tentative.");
-		        addState(service, "kuali.milestone.process", "kuali.milestone.state.Official", "Official", "Indicates that this Milestone has been established.");     
-	}
-	
-
-    private void addLifecycle(StateService service, String key, String name, String desc, String ref)
-        throws Exception {
-
-        LifecycleInfo lifecycle = new LifecycleInfo();
-        lifecycle.setName(name);
-
-        RichTextInfo rtDesc = new RichTextInfo();
-        rtDesc.setPlain(desc);
-        rtDesc.setFormatted(desc);
-        lifecycle.setDescr(rtDesc);
-        lifecycle.setKey(key);
-        lifecycle.setRefObjectUri(ref);
-
-        service.createLifecycle(key, lifecycle, new ContextInfo());
-    }
-
-    private void addState(StateService service, String lifecycleKey, String key, String name, String desc)
-        throws Exception {
-
-        StateInfo state = new StateInfo();
-        state.setName(name);
-
-        RichTextInfo rtDesc = new RichTextInfo();
-        rtDesc.setPlain(desc);
-        rtDesc.setFormatted(desc);
-        state.setDescr(rtDesc);
-
-        service.createState(lifecycleKey, key, state, new ContextInfo());
     }
 
 }
