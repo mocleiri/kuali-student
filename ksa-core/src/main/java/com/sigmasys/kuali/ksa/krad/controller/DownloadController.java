eee967298734d80693026ffa6471c7751b7cd29a
godunovs <godunovs@kuali.org>
2013-11-08 02:59:43 -0500

    Added overloaded methods for all kinds of downloading.
    git-svn-id: https://svn.kuali.org/repos/student/contrib/ksa/trunk@59852 0ac02d80-fb3f-11dc-a574-c57244a850eb

--- ksa-core/src/main/java/com/sigmasys/kuali/ksa/krad/controller/DownloadController.java
+++ ksa-core/src/main/java/com/sigmasys/kuali/ksa/krad/controller/DownloadController.java
@@ -2,6 +2,7 @@ package com.sigmasys.kuali.ksa.krad.controller;
 
 import java.io.ByteArrayInputStream;
 import java.io.IOException;
+import java.io.InputStream;
 
 import javax.servlet.ServletOutputStream;
 import javax.servlet.http.HttpServletResponse;
@@ -16,10 +17,8 @@ import javax.servlet.http.HttpServletResponse;
 public abstract class DownloadController extends GenericSearchController {
 
     /**
-     * Sends content as a String to the ServletResponse output stream.  Typically
-     * you want the browser to receive a different name than the
-     * name the file has been saved in your local database, since
-     * your local names need to be unique.
+     * Sends content as a String to the ServletResponse output stream.
+     * Use this method for String content downloads, such as text or XML.
      *
      * @param content	Content to be sent to the client for downloading.
      * @param fileName	Name of the file to save the content as. 
@@ -27,11 +26,36 @@ public abstract class DownloadController extends GenericSearchController {
      * @param response 	Response to send content to.
      */
     protected void doDownload(String content, String fileName, String mimeType, HttpServletResponse response) throws IOException {
+        doDownload(content.getBytes(), fileName, mimeType, response);
+    }
+
+    /**
+     * Sends content as a byte array to the ServletResponse output stream.
+     * Use this method for binary content downloads, such as ZIP or Excel.
+     *
+     * @param bytes	    Content to be sent to the client for downloading.
+     * @param fileName	Name of the file to save the content as.
+     * @param mimeType	MIME type of the file to download.
+     * @param response 	Response to send content to.
+     */
+    protected void doDownload(byte[] bytes, String fileName, String mimeType, HttpServletResponse response) throws IOException {
+        response.setContentLength(bytes.length);
+        doDownload(new ByteArrayInputStream(bytes), fileName, mimeType, response);
+    }
+
+    /**
+     * Sends content from an InputStream to the ServletResponse output stream.
+     * Use this method for binary content downloads, such as ZIP or Excel.
+     *
+     * @param inputStream	InputStream to be sent to the client for downloading.
+     * @param fileName	    Name of the file to save the content as.
+     * @param mimeType	    MIME type of the file to download.
+     * @param response 	    Response to send content to.
+     */
+    protected void doDownload(InputStream inputStream, String fileName, String mimeType, HttpServletResponse response) throws IOException {
         response.setContentType(mimeType);
-        response.setContentLength(content.length());
         response.setHeader("Content-Disposition", "attachment; filename=\"" + fileName + "\"");
 
-        ByteArrayInputStream inputStream = new ByteArrayInputStream(content.getBytes());
         ServletOutputStream outputStream = response.getOutputStream();
 
         try {
