c79750c9fa40bb91a12cb949fe63260c6a9c5a5b
mike.wyers <mike.wyers@kuali.org>
2013-11-13 16:02:09 -0500

    KSENROLL-10659 AFTs fix failing Student AZ tests - tweak error page expected_element
    git-svn-id: https://svn.kuali.org/repos/student/test/functional-automation/sambal/trunk@60219 0ac02d80-fb3f-11dc-a574-c57244a850eb

--- lib/sambal-kuali/pages/error_page.rb
+++ lib/sambal-kuali/pages/error_page.rb
@@ -2,10 +2,10 @@ class ErrorPage < BasePage
   wrapper_elements
   frame_element
 
-  expected_element :message_element
+  expected_element :error_message_element
 
-  value(:error_message){|b| b.message_element.text}
-  element(:message_element) { |b| b.frm.div(id: "Uif-Application")}
+  value(:error_message){|b| b.error_message_element.text}
+  element(:error_message_element) { |b| b.frm.div(id: "Uif-Application").div(class: "error_403 uif-validationMessages")}
 
   def error_401
     present = error_message.include? "Error 403: This page contains restricted content"
