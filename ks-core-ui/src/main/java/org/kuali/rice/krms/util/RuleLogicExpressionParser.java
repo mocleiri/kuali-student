dafb1b7311f262cd55c6a0cc49a28ddfc1862c52
pmarais <pmarais@kuali.org>
2013-11-27 04:18:02 -0500

    KSCM-1232: Applied a patch from the KRMS team to fix an exception.
    git-svn-id: https://svn.kuali.org/repos/student/enrollment/ks-core/branches/CM-KRAD@60903 0ac02d80-fb3f-11dc-a574-c57244a850eb

--- ks-core-ui/src/main/java/org/kuali/rice/krms/util/RuleLogicExpressionParser.java
+++ ks-core-ui/src/main/java/org/kuali/rice/krms/util/RuleLogicExpressionParser.java
@@ -329,6 +329,11 @@ public class RuleLogicExpressionParser {
             tokenStack.push(token);
             index++;
         }
+        
+        //Return null if stack is empty, all propositions deleted from logic expression.
+        if (tokenStack.isEmpty()) {
+            return null;
+        }
 
         Map<String, PropositionEditor> simplePropositions = new LinkedHashMap<String, PropositionEditor>();
         Queue<PropositionEditor> compoundPropositions = new LinkedList<PropositionEditor>();
@@ -392,6 +397,12 @@ public class RuleLogicExpressionParser {
      */
     public PropositionEditor ruleFromStack(Stack<ExpressionToken> tokenStack, Map<String, PropositionEditor> simplePropositions,
                                            Queue<PropositionEditor> compoundPropositions, RuleEditor rule, RuleViewHelperService viewHelper) {
+        
+        //Check for single statement.
+        if (tokenStack.size() == 1) {
+            ExpressionToken token = tokenStack.pop();
+            return simplePropositions.remove(token.getValue().toUpperCase());
+        }
 
         //Get the first compound from the queue, if nothing left create new one.
         PropositionEditor currentCompound = null;
