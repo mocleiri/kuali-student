5f9ac9821b2504434593d11766f933cc61c09c63
llfourie <llfourie@kuali.org>
2013-11-27 05:20:44 -0500

    KSENROLL-9797 : Create AFT for user story FE 1.2 - Fixed the failing AFT. Failure was caused by changes made to the Matrix page
    git-svn-id: https://svn.kuali.org/repos/student/test/functional-automation/sambal/trunk@60909 0ac02d80-fb3f-11dc-a574-c57244a850eb

--- lib/sambal-kuali/pages/exam/manage_final_exam_matrix_page.rb
+++ lib/sambal-kuali/pages/exam/manage_final_exam_matrix_page.rb
@@ -3,13 +3,12 @@ class ManageFEMatrix < BasePage
   wrapper_elements
   frame_element
 
-  expected_element :show_btn
+  expected_element :term_type_select
 
   element(:fe_search_page) { |b| b.frm.div( id: "finalExamMatrixManagementView")}
-  element(:fe_matrix_criteria_section) { |b| b.fe_search_page.div( id: "finalExamMatrixManagementCriteriaSection")}
 
-  element(:term_type_select) { |b| b.fe_matrix_criteria_section.select( name: "type.key")}
-  action(:term_type) { |b| b.term_type_select.select}
-  element(:show_btn) { |b| b.fe_matrix_criteria_section.button( id: "show_button")}
+  element(:term_type_select) { |b| b.frm.select( id: "termType_control")}
+  #action(:term_type) { |b| b.term_type_select.select}
+  element(:show_btn) { |b| b.frm.button( id: "show_button")}
   action(:show) { |b| b.show_btn.click; b.loading.wait_while_present}
 end
