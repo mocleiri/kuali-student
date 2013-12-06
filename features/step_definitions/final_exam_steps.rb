bd8034c5e37128d7461c757ca6d7e6a650b31fe3
mike.wyers <mike.wyers@kuali.org>
2013-12-05 19:33:22 -0500

    KSENROLL-11114 AFTs fix page element issue for "SA.FE3-9 View Exam Offerings.Test that the AO table is shown when viewing the exam offerings and that it's state is draft"
    git-svn-id: https://svn.kuali.org/repos/student/test/functional-automation/sambal/trunk@61266 0ac02d80-fb3f-11dc-a574-c57244a850eb

--- features/step_definitions/final_exam_steps.rb
+++ features/step_definitions/final_exam_steps.rb
@@ -285,9 +285,12 @@ end
 
 When /^I view the Exam Offerings for a CO created from an existing CO with a standard final exam driven by Activity Offering$/ do
   @course_offering = create CourseOffering, :create_by_copy=>(make CourseOffering, :term => "201208", :course => "ENGL305")
+  delivery_format_list = []
+  delivery_format_list[0] = make DeliveryFormat, :format => "Lecture Only", :grade_format => "Course Offering", :final_exam_activity => "Lecture"
+
   @course_offering.edit_offering :final_exam_type => "Standard Final Exam",
                                  :final_exam_driver => "Final Exam Per Activity Offering",
-                                 :final_exam_activity => "Lecture"
+                                 :delivery_format_list => delivery_format_list
   @course_offering.save
   @activity_offering =  make ActivityOffering, :code => "A", :parent_course_offering => @course_offering
   @activity_offering.edit :send_to_scheduler => true, :defer_save => false
