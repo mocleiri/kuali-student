9ec3034730083d43266d62016cafdfd0c481a5cd
arun.ramaiah <arun.ramaiah@kuali.org>
2013-12-20 07:42:01 -0500

    Course Planner step definition - draft
    git-svn-id: https://svn.kuali.org/repos/student/test/functional-automation/ksap-aft/trunk@62243 0ac02d80-fb3f-11dc-a574-c57244a850eb

---
+++ features/step_definitions/course_planner_steps.rb
@@ -0,0 +1,34 @@
+When /^I add a fixed credit course with notes to current term$/ do
+pending # express the regexp above with the code you wish you had
+end
+
+
+
+When /^I add a variable credit course with notes to a future term$/ do
+  pending # express the regexp above with the code you wish you had
+end
+
+
+
+And /^I view the details of the added course from edit plan item$/ do
+pending # express the regexp above with the code you wish you had
+end
+
+
+Then /^fixed credit and note details are displayed under the current term$/ do
+pending # express the regexp above with the code you wish you had
+end
+
+Then /^the course should be added to the current term$/ do
+  pending # express the regexp above with the code you wish you had
+end
+
+
+Then /^the course should be added to the future term$/ do
+pending # express the regexp above with the code you wish you had
+end
+
+
+Then /^variable credit and note details are displayed under the future term$/ do
+pending # express the regexp above with the code you wish you had
+end
