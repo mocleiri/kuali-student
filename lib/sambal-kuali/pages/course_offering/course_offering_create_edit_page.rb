aa470340ec0c59f0fa2770f8e0b77dd8a0362fcb
mike.wyers <mike.wyers@kuali.org>
2013-12-20 11:50:45 -0500

    KSENROLL-11374 AFTs fix sync issue for failing scenario: WC.Edit Course Offerings.Change Honors Course setting and jump to an arbitrary CO without saving
    git-svn-id: https://svn.kuali.org/repos/student/test/functional-automation/sambal/trunk@62250 0ac02d80-fb3f-11dc-a574-c57244a850eb

--- lib/sambal-kuali/pages/course_offering/course_offering_create_edit_page.rb
+++ lib/sambal-kuali/pages/course_offering/course_offering_create_edit_page.rb
@@ -19,7 +19,7 @@ class CourseOfferingCreateEdit < BasePage
   action(:edit_previous_co) { |b| b.edit_previous_co_link.click; b.loading.wait_while_present }
   element(:edit_next_co_link) { |b| b.frm.link(id: "edit_co_next") }
   action(:edit_next_co) { |b| b.edit_next_co_link.click; b.loading.wait_while_present }
-  element(:navigation_confirmation_dialog) { |b| b.div(id: "CourseOfferingEdit-NavigationConfirmation") }
+  element(:navigation_confirmation_dialog) { |b| b.div(class: "fancybox-wrap fancybox-desktop fancybox-type-html fancybox-opened").div(id: "CourseOfferingEdit-NavigationConfirmation") }
 
   element(:nav_save_and_continue_element) { |b| b.navigation_confirmation_dialog.button(id: "edit_co_save_and_continue") }
   action(:navigation_save_and_continue) { |b| b.nav_save_and_continue_element.wait_until_present; b.nav_save_and_continue_element.click; b.loading.wait_while_present }
