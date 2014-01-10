68368c692f96a717522fb58466efea0f0d4e8a98
mivanov <mivanov@kuali.org>
2014-01-10 11:57:43 -0500

    Added a new FM signup rule set to the FM rule test data (ksa_load_fm_rules_dml.sql). Made changes to Constants interface.
    git-svn-id: https://svn.kuali.org/repos/student/contrib/ksa/trunk@63245 0ac02d80-fb3f-11dc-a574-c57244a850eb

--- sql/ksa_load_fm_rules_dml.sql
+++ sql/ksa_load_fm_rules_dml.sql
@@ -23,10 +23,10 @@ global FeeManagementSession fmSession;
 
 Insert into KSSA_RULE (ID, NAME, RULE_TYPE_ID_FK, PRIORITY, HEADER, LHS, RHS) values (5000, 'FM Main Rule', 3, 20, null,
 '(Context is initialized)',
-'on each session signup fire "Fee Management 1" rule set
+'on each session signup fire "FM Signup 1" rule set
 ')!
 
-Insert into KSSA_RULE_SET (ID, NAME, RULE_TYPE_ID_FK, HEADER) values (101, 'Fee Management 1', 3,
+Insert into KSSA_RULE_SET (ID, NAME, RULE_TYPE_ID_FK, HEADER) values (101, 'FM Signup 1', 3,
 'import java.util.*;
 import java.math.*;
 import com.sigmasys.kuali.ksa.model.*;
@@ -46,50 +46,57 @@ global FeeManagementSignup fmSignup;
 ')!
 
 Insert into KSSA_RULE (ID, NAME, RULE_TYPE_ID_FK, PRIORITY, HEADER, LHS, RHS) values (5001, 'FM Rule 1', 3, 10, null,
-'(signup date is on or before atp milestone "kuali.atp.milestone.lateRegistration" and signup operation is "ADD")',
-'charge incidental rate "late.registration", "1" using id "late.registration"
+'(signup date is on or before atp milestone "kuali.atp.milestone.firstDayOfClass" and signup operation is "ADD")',
+'set session key "early.registration" to "true"
  mark signup as taken
 ')!
 
 Insert into KSSA_RULE (ID, NAME, RULE_TYPE_ID_FK, PRIORITY, HEADER, LHS, RHS) values (5002, 'FM Rule 2', 3, 10, null,
 '(signup operation is "ADD_WITHOUT_PENALTY,TRANSFER_IN")',
-'mark signup as taken
+'set session key "early.registration" to "true"
+ mark signup as taken
 ')!
 
 Insert into KSSA_RULE (ID, NAME, RULE_TYPE_ID_FK, PRIORITY, HEADER, LHS, RHS) values (5003, 'FM Rule 3', 3, 10, null,
 '(signup operation is "DROP_WITHOUT_PENALTY,TRANSFER_OUT")',
-'mark preceding offerings as complete
+'mark preceding offerings as not taken
+ mark signup as not taken
+ on signup including preceding offerings remove rates ".*fee.*"
+ mark preceding offerings as complete
+ mark signup as not taken
 ')!
 
 Insert into KSSA_RULE (ID, NAME, RULE_TYPE_ID_FK, PRIORITY, HEADER, LHS, RHS) values (5004, 'FM Rule 4', 3, 10, null,
-'(signup date is after atp milestone "kuali.atp.milestone.lateRegistration" and signup date is before atp milestone "kuali.atp.milestone.lastDayForPenaltyAddDrop" and signup operation is "DROP")',
-'mark preceding offerings as not taken
- mark signup as not taken
- on signup remove rates ".*fee.*"
- charge incidental rate "late.registration", "1" using id "late.registration"
+'(signup operation is "WITHDRAW" and signup date is after atp milestone "kuali.atp.milestone.withdraw80")',
+'set session key "withdrawn" to "yes"
+ set session key "80percent" to "yes"
 ')!
 
 Insert into KSSA_RULE (ID, NAME, RULE_TYPE_ID_FK, PRIORITY, HEADER, LHS, RHS) values (5005, 'FM Rule 5', 3, 10, null,
-'(signup date is after atp milestone "kuali.atp.milestone.lastDayForPenaltyAddDrop" and signup operation is "DROP")',
-'mark preceding offerings as not taken
- mark signup as not taken
- mark signup as complete
+'(signup operation is "WITHDRAW" and signup date is after atp milestone "kuali.atp.milestone.withdraw60")',
+'set session key "withdrawn" to "yes"
+ set session key "60percent" to "yes"
 ')!
 
 Insert into KSSA_RULE (ID, NAME, RULE_TYPE_ID_FK, PRIORITY, HEADER, LHS, RHS) values (5006, 'FM Rule 6', 3, 10, null,
-'(signup operation is "WITHDRAW")',
-'mark all signups as not taken
- set session key "withdrawn" to "true"
- on signup including preceding offerings remove rates ".*fee.*"
+'(signup operation is "WITHDRAW" and signup date is after atp milestone "kuali.atp.milestone.withdraw40")',
+'set session key "withdrawn" to "yes"
+ set session key "40percent" to "yes"
 ')!
 
-Insert into KSSA_RULE (ID, NAME, RULE_TYPE_ID_FK, PRIORITY, HEADER, LHS, RHS) values (5007, 'FM Rule 7', 3, 9, null,
-'(Context is initialized)',
-'set session key "chargeable.credits" to number of units where signup operation is "ADD,ADD_WITHOUT_PENALTY,TRANSFER_IN" minus "DROP,DROP_WITHOUT_PENALTY,TRANSFER_OUT"
- set session key "taken.credits" to number of units where signup is taken
+Insert into KSSA_RULE (ID, NAME, RULE_TYPE_ID_FK, PRIORITY, HEADER, LHS, RHS) values (5007, 'FM Rule 7', 3, 10, null,
+'(signup operation is "WITHDRAW" and signup date is after atp milestone "kuali.atp.milestone.withdraw20")',
+'set session key "withdrawn" to "yes"
+ set session key "20percent" to "yes"
 ')!
 
-Insert into KSSA_RULE (ID, NAME, RULE_TYPE_ID_FK, PRIORITY, HEADER, LHS, RHS) values (5008, 'FM Rule TEST', 3, 8, null,
+Insert into KSSA_RULE (ID, NAME, RULE_TYPE_ID_FK, PRIORITY, HEADER, LHS, RHS) values (5008, 'FM Rule 8', 3, 10, null,
+'(signup operation is "WITHDRAW" and signup date is after atp milestone "kuali.atp.milestone.withdraw0")',
+'set session key "withdrawn" to "yes"
+ set session key "0percent" to "yes"
+')!
+
+Insert into KSSA_RULE (ID, NAME, RULE_TYPE_ID_FK, PRIORITY, HEADER, LHS, RHS) values (5009, 'FM Rule TEST', 3, 8, null,
 '(Context is initialized)',
 'set session key "chargeable.credits" to number of units where signup operation is "ADD,ADD_WITHOUT_PENALTY,TRANSFER_IN" minus "DROP,DROP_WITHOUT_PENALTY,TRANSFER_OUT"
  set session key "taken.credits" to number of units where signup is taken
@@ -117,6 +124,7 @@ Insert into KSSA_RULE_SET_RULE ( RULE_SET_ID_FK, RULE_ID_FK ) values (101, 5005)
 Insert into KSSA_RULE_SET_RULE ( RULE_SET_ID_FK, RULE_ID_FK ) values (101, 5006)!
 Insert into KSSA_RULE_SET_RULE ( RULE_SET_ID_FK, RULE_ID_FK ) values (101, 5007)!
 Insert into KSSA_RULE_SET_RULE ( RULE_SET_ID_FK, RULE_ID_FK ) values (101, 5008)!
+Insert into KSSA_RULE_SET_RULE ( RULE_SET_ID_FK, RULE_ID_FK ) values (101, 5009)!
 
 
 -----------------------------------------------------------------------------------------------------------------------
