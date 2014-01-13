9f1d0447c5fff074df92c6ed6bf94c6909a4af0b
mivanov <mivanov@kuali.org>
2014-01-13 14:49:45 -0500

    Re-written the first FM signup set of rules and added new DSL definitions, changed ksa.dsl and ksa_load_fm_rules_dml.sql files.
    git-svn-id: https://svn.kuali.org/repos/student/contrib/ksa/trunk@63360 0ac02d80-fb3f-11dc-a574-c57244a850eb

--- sql/ksa_load_fm_rules_dml.sql
+++ sql/ksa_load_fm_rules_dml.sql
@@ -26,6 +26,8 @@ Insert into KSSA_RULE (ID, NAME, RULE_TYPE_ID_FK, PRIORITY, HEADER, LHS, RHS) va
 'on each session signup fire "FM Signup 1" rule set
 ')!
 
+-- FM Signup 1 rule set --
+
 Insert into KSSA_RULE_SET (ID, NAME, RULE_TYPE_ID_FK, HEADER) values (101, 'FM Signup 1', 3,
 'import java.util.*;
 import java.math.*;
@@ -61,10 +63,10 @@ Insert into KSSA_RULE (ID, NAME, RULE_TYPE_ID_FK, PRIORITY, HEADER, LHS, RHS) va
 '(signup operation is "DROP_WITHOUT_PENALTY,TRANSFER_OUT")',
 'mark preceding offerings as not taken
  mark signup as not taken
- remove signup rates ".*fee.*"
- remove rates ".*fee.*" from preceding offerings
  mark preceding offerings as complete
  mark signup as complete
+ remove signup rates ".*fee.*"
+ remove rates ".*fee.*" from preceding offerings
 ')!
 
 Insert into KSSA_RULE (ID, NAME, RULE_TYPE_ID_FK, PRIORITY, HEADER, LHS, RHS) values (5004, 'FM Rule 4', 3, 10, null,
@@ -97,7 +99,55 @@ Insert into KSSA_RULE (ID, NAME, RULE_TYPE_ID_FK, PRIORITY, HEADER, LHS, RHS) va
  set session key "0percent" to "yes"
 ')!
 
-Insert into KSSA_RULE (ID, NAME, RULE_TYPE_ID_FK, PRIORITY, HEADER, LHS, RHS) values (5009, 'FM Rule TEST', 3, 8, null,
+Insert into KSSA_RULE (ID, NAME, RULE_TYPE_ID_FK, PRIORITY, HEADER, LHS, RHS) values (5009, 'FM Rule 9', 3, 10, null,
+'(signup operation is "DROP" and signup date is before atp milestone "kuali.atp.milestone.firstDayOfClass")',
+'mark preceding offerings as not taken
+ mark signup as not taken
+ mark preceding offerings as complete
+ mark signup as complete
+ remove signup rates ".*fee.*"
+ remove rates ".*fee.*" from preceding offerings
+')!
+
+Insert into KSSA_RULE (ID, NAME, RULE_TYPE_ID_FK, PRIORITY, HEADER, LHS, RHS) values (5010, 'FM Rule 10', 3, 10, null,
+'(signup operation is "DROP" and signup date is on or after atp milestone "kuali.atp.milestone.firstDayOfClass" and signup date is on or before atp milestone "kuali.atp.milestone.lastDayForPenaltyDrop")',
+'mark preceding offerings as not taken
+ mark signup as not taken
+ set signup key "late.penalty" to "yes"
+ set session key "late.penalty" to "yes"
+')!
+
+Insert into KSSA_RULE (ID, NAME, RULE_TYPE_ID_FK, PRIORITY, HEADER, LHS, RHS) values (5011, 'FM Rule 11', 3, 10, null,
+'(signup operation is "DROP" and signup date is after atp milestone "kuali.atp.milestone.lastDayForPenaltyDrop")',
+'mark signup as complete
+')!
+
+Insert into KSSA_RULE (ID, NAME, RULE_TYPE_ID_FK, PRIORITY, HEADER, LHS, RHS) values (5012, 'FM Rule 12', 3, 10, null,
+'(signup operation is "WITHDRAW")',
+'set session key "withdrawn" to "yes"
+')!
+
+Insert into KSSA_RULE (ID, NAME, RULE_TYPE_ID_FK, PRIORITY, HEADER, LHS, RHS) values (5013, 'FM Rule 13', 3, 10, null,
+'(signup has rates "mba.cohort.flag, regular" and session key "cohort.code" is "EM.." and session key "major.code" is ".*MBA")',
+'remove signup rates "regular"
+')!
+
+Insert into KSSA_RULE (ID, NAME, RULE_TYPE_ID_FK, PRIORITY, HEADER, LHS, RHS) values (5014, 'FM Rule 14', 3, 10, null,
+'(signup has rates "mba.cohort.flag, regular" and session key "cohort.code" is not "EM..")',
+'remove signup rates "mba.cohort.flag"
+')!
+
+Insert into KSSA_RULE (ID, NAME, RULE_TYPE_ID_FK, PRIORITY, HEADER, LHS, RHS) values (5015, 'FM Rule 15', 3, 10, null,
+'(session key "major.code" is not ".*MBA")',
+'remove signup rates "mba.cohort.flag"
+')!
+
+Insert into KSSA_RULE (ID, NAME, RULE_TYPE_ID_FK, PRIORITY, HEADER, LHS, RHS) values (5016, 'FM Rule 16', 3, 10, null,
+'(session key "major.code" is not "z077")',
+'remove signup rates "cybersecurity.leadership"
+')!
+
+Insert into KSSA_RULE (ID, NAME, RULE_TYPE_ID_FK, PRIORITY, HEADER, LHS, RHS) values (5099, 'FM Rule TEST', 3, 8, null,
 '(Context is initialized)',
 'set session key "chargeable.credits" to number of units where signup operation is "ADD,ADD_WITHOUT_PENALTY,TRANSFER_IN" minus "DROP,DROP_WITHOUT_PENALTY,TRANSFER_OUT"
  set session key "taken.credits" to number of units where signup is taken
@@ -116,6 +166,8 @@ Insert into KSSA_RULE (ID, NAME, RULE_TYPE_ID_FK, PRIORITY, HEADER, LHS, RHS) va
  add signup rate "late.registration", "1"
 ')!
 
+-- FM Signup 2 rule set --
+
 -- FM rule associations
 -- Main rule set
 Insert into KSSA_RULE_SET_RULE ( RULE_SET_ID_FK, RULE_ID_FK ) values (100, 5000)!
@@ -129,6 +181,14 @@ Insert into KSSA_RULE_SET_RULE ( RULE_SET_ID_FK, RULE_ID_FK ) values (101, 5006)
 Insert into KSSA_RULE_SET_RULE ( RULE_SET_ID_FK, RULE_ID_FK ) values (101, 5007)!
 Insert into KSSA_RULE_SET_RULE ( RULE_SET_ID_FK, RULE_ID_FK ) values (101, 5008)!
 Insert into KSSA_RULE_SET_RULE ( RULE_SET_ID_FK, RULE_ID_FK ) values (101, 5009)!
+Insert into KSSA_RULE_SET_RULE ( RULE_SET_ID_FK, RULE_ID_FK ) values (101, 5010)!
+Insert into KSSA_RULE_SET_RULE ( RULE_SET_ID_FK, RULE_ID_FK ) values (101, 5011)!
+Insert into KSSA_RULE_SET_RULE ( RULE_SET_ID_FK, RULE_ID_FK ) values (101, 5012)!
+Insert into KSSA_RULE_SET_RULE ( RULE_SET_ID_FK, RULE_ID_FK ) values (101, 5013)!
+Insert into KSSA_RULE_SET_RULE ( RULE_SET_ID_FK, RULE_ID_FK ) values (101, 5014)!
+Insert into KSSA_RULE_SET_RULE ( RULE_SET_ID_FK, RULE_ID_FK ) values (101, 5015)!
+Insert into KSSA_RULE_SET_RULE ( RULE_SET_ID_FK, RULE_ID_FK ) values (101, 5016)!
+Insert into KSSA_RULE_SET_RULE ( RULE_SET_ID_FK, RULE_ID_FK ) values (101, 5099)!
 
 
 -----------------------------------------------------------------------------------------------------------------------
