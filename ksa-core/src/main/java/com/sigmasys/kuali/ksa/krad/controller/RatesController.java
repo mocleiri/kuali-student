104be3481c79170309e0ae3db036361886d1a1a2
tbornholtz <tbornholtz@kuali.org>
2014-01-14 13:26:26 -0500

    KSA-294 - First very rough cut at some functionality on the rate screen
    git-svn-id: https://svn.kuali.org/repos/student/contrib/ksa/trunk@63414 0ac02d80-fb3f-11dc-a574-c57244a850eb

--- ksa-core/src/main/java/com/sigmasys/kuali/ksa/krad/controller/RatesController.java
+++ ksa-core/src/main/java/com/sigmasys/kuali/ksa/krad/controller/RatesController.java
@@ -1,9 +1,13 @@
 package com.sigmasys.kuali.ksa.krad.controller;
 
 import com.sigmasys.kuali.ksa.krad.form.RatesForm;
+import com.sigmasys.kuali.ksa.model.fm.Rate;
+import com.sigmasys.kuali.ksa.service.atp.AtpService;
 import com.sigmasys.kuali.ksa.service.fm.RateService;
 import org.apache.commons.logging.Log;
 import org.apache.commons.logging.LogFactory;
+import org.kuali.rice.core.api.util.RiceKeyConstants;
+import org.kuali.rice.krad.util.GlobalVariables;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.ModelAttribute;
@@ -12,6 +16,10 @@ import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.servlet.ModelAndView;
 
 import javax.servlet.http.HttpServletRequest;
+import java.util.ArrayList;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Set;
 
 /**
  * Controller to serve requests from the "Rates" page.
@@ -28,6 +36,9 @@ public class RatesController extends GenericSearchController {
     @Autowired
     private RateService rateService;
 
+    @Autowired
+    private AtpService atpService;
+
 
     /**
      * Creates the page's form.
@@ -41,6 +52,55 @@ public class RatesController extends GenericSearchController {
 
     @RequestMapping(method = {RequestMethod.POST, RequestMethod.GET}, params = "methodToCall=displayInitialPage")
     public ModelAndView displayInitialPage(@ModelAttribute("KualiForm") RatesForm form) {
+
+        return getUIFModelAndView(form);
+    }
+
+    @RequestMapping(method = RequestMethod.POST, params = "methodToCall=getRates")
+    public ModelAndView getRates(@ModelAttribute("KualiForm") RatesForm form) {
+
+        Set<Rate> rates = new HashSet<Rate>();
+
+        String atp = form.getAcademicTimePeriod();
+        if(!atpService.atpExists(atp)) {
+            GlobalVariables.getMessageMap().putError(form.getViewId(), RiceKeyConstants.ERROR_CUSTOM, "Academic Time Period '" + atp + "' not found");
+            return getUIFModelAndView(form);
+        }
+
+        List<Rate> atpRates = rateService.getRatesByAtpId(atp);
+        rates.addAll(atpRates);
+
+        String catalogCode = form.getRateCatalogCode();
+
+        Long rateCatalogId;
+        try {
+            rateCatalogId = Long.valueOf(catalogCode);
+        } catch(NumberFormatException e) {
+            logger.error("Error converting '" + catalogCode + "' to a rate catalog id");
+            rateCatalogId = null;
+        }
+
+        if(rateCatalogId != null) {
+            List<Rate> catalogCodeRates = rateService.getRatesByCatalogId(rateCatalogId);
+            rates.retainAll(catalogCodeRates);
+        }
+
+        String rateCode = form.getRateCode();
+        String rateSubcode = form.getRateSubcode();
+
+        if(rateCode != null && !"".equals(rateCode) && (rateSubcode == null || "".equals(rateSubcode))) {
+            List<Rate> codeRates = rateService.getRatesByCode(rateCode);
+            rates.retainAll(codeRates);
+        } else if(rateCode != null && !"".equals(rateCode) && rateSubcode != null && !"".equals(rateSubcode)) {
+            List<Rate> codeSubcodeRates = rateService.getRatesByCodeAndSubCode(rateCode, rateSubcode);
+            rates.retainAll(codeSubcodeRates);
+        }
+
+        List<Rate> rateList = new ArrayList<Rate>();
+        rateList.addAll(rates);
+        rateList.add(0, new Rate());
+
+        form.setRates(rateList);
 
         return getUIFModelAndView(form);
     }
--- ksa-core/src/main/java/com/sigmasys/kuali/ksa/krad/form/RatesForm.java
+++ ksa-core/src/main/java/com/sigmasys/kuali/ksa/krad/form/RatesForm.java
@@ -1,5 +1,10 @@
 package com.sigmasys.kuali.ksa.krad.form;
 
+import com.sigmasys.kuali.ksa.model.fm.Rate;
+
+import java.util.ArrayList;
+import java.util.List;
+
 /**
  * The form object behind the "Rates" page.
  * User: Sergey
@@ -7,4 +12,56 @@ package com.sigmasys.kuali.ksa.krad.form;
  * Time: 10:44 PM
  */
 public class RatesForm extends AbstractViewModel {
+
+    // Filter fields
+    private String academicTimePeriod;
+    private String rateCatalogCode;
+    private String rateCode;
+    private String rateSubcode;
+
+
+    private List<Rate> rates;
+
+    public String getAcademicTimePeriod() {
+        return academicTimePeriod;
+    }
+
+    public void setAcademicTimePeriod(String academicTimePeriod) {
+        this.academicTimePeriod = academicTimePeriod;
+    }
+
+    public String getRateCatalogCode() {
+        return rateCatalogCode;
+    }
+
+    public void setRateCatalogCode(String rateCatalogCode) {
+        this.rateCatalogCode = rateCatalogCode;
+    }
+
+    public String getRateCode() {
+        return rateCode;
+    }
+
+    public void setRateCode(String rateCode) {
+        this.rateCode = rateCode;
+    }
+
+    public String getRateSubcode() {
+        return rateSubcode;
+    }
+
+    public void setRateSubcode(String rateSubcode) {
+        this.rateSubcode = rateSubcode;
+    }
+
+    public List<Rate> getRates() {
+        if(rates == null) {
+            rates = new ArrayList<Rate>();
+        }
+        return rates;
+    }
+
+    public void setRates(List<Rate> rates) {
+        this.rates = rates;
+    }
 }
