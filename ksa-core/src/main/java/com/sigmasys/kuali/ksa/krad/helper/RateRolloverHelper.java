9e7bbdfde97ea36b8db4506c248faa08b5481e8d
godunovs <godunovs@kuali.org>
2014-01-15 01:50:55 -0500

    Added ATP lookup.
    git-svn-id: https://svn.kuali.org/repos/student/contrib/ksa/trunk@63467 0ac02d80-fb3f-11dc-a574-c57244a850eb

--- ksa-core/src/main/java/com/sigmasys/kuali/ksa/krad/helper/RateRolloverHelper.java
+++ ksa-core/src/main/java/com/sigmasys/kuali/ksa/krad/helper/RateRolloverHelper.java
@@ -1,8 +1,19 @@
 package com.sigmasys.kuali.ksa.krad.helper;
 
 import com.sigmasys.kuali.ksa.krad.model.AtpModel;
+import com.sigmasys.kuali.ksa.service.atp.AtpService;
+import com.sigmasys.kuali.ksa.util.ContextUtils;
+import org.apache.commons.logging.Log;
+import org.apache.commons.logging.LogFactory;
+import org.kuali.rice.core.api.criteria.LikePredicate;
+import org.kuali.rice.core.api.criteria.Predicate;
+import org.kuali.rice.core.api.criteria.PredicateFactory;
+import org.kuali.rice.core.api.criteria.QueryByCriteria;
 import org.kuali.rice.krad.uif.service.impl.ViewHelperServiceImpl;
+import org.kuali.student.r2.common.dto.ContextInfo;
+import org.kuali.student.r2.core.atp.dto.AtpInfo;
 
+import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 
@@ -17,6 +28,12 @@ import java.util.List;
  */
 public class RateRolloverHelper extends ViewHelperServiceImpl {
 
+    private static final Log logger = LogFactory.getLog(RateRolloverHelper.class);
+
+    /**
+     * AtpService to perform lookup.
+     */
+    private AtpService atpService;
 
     /**
      * Returns suggested values for the "Rollover From" ATP lookup field.
@@ -25,9 +42,7 @@ public class RateRolloverHelper extends ViewHelperServiceImpl {
      * @return List of suggested values.
      */
     public List<AtpModel> getAtpRolloverFromForSuggest(String suggest) {
-        AtpModel atp = new AtpModel();
-        atp.setAtpId("test");
-        return Arrays.asList(atp);
+        return searchForAtps(suggest);
     }
 
     /**
@@ -37,9 +52,61 @@ public class RateRolloverHelper extends ViewHelperServiceImpl {
      * @return List of suggested values.
      */
     public List<AtpModel> getAtpRolloverToForSuggest(String suggest) {
-        AtpModel atp = new AtpModel();
-        atp.setAtpId("test");
-        return Arrays.asList(atp);
+        return searchForAtps(suggest);
+    }
+
+    /**
+     * Performs ATP lookup by ATP name.
+     *
+     * @param suggest   Suggestion string.
+     * @return List of matching ATPs by name.
+     */
+    private List<AtpModel> searchForAtps(String suggest) {
+
+        List<AtpModel> result = new ArrayList<AtpModel>();
+
+        // Perform search for ATP:
+        try {
+            ContextInfo contextInfo = getAtpService().getAtpContextInfo();
+            Predicate like = PredicateFactory.like("name", String.format("*%s*", suggest));
+            QueryByCriteria.Builder builder = QueryByCriteria.Builder.create();
+
+            builder.setPredicates(like);
+            builder.setMaxResults(10);
+            builder.setStartAtIndex(0);
+
+            QueryByCriteria query = builder.build();
+            List<AtpInfo> atpInfoList = getAtpService().searchForAtps(query, contextInfo);
+
+            for (AtpInfo atpInfo : atpInfoList) {
+
+                // Create a new AtpModel and add to the result:
+                AtpModel atp = new AtpModel();
+
+                atp.setAtpId(atpInfo.getName());
+                result.add(atp);
+            }
+
+            return result;
+        } catch (Exception e) {
+            // Just log an error:
+            logger.error("Cannot search for ATPs for suggest");
+        }
+
+        return result;
+    }
+
+    /**
+     * Returns the local instance of the AtpService. Creates one if needed.
+     *
+     * @return The local instance of the AtpService.
+     */
+    private AtpService getAtpService() {
+        if (atpService == null) {
+            atpService = ContextUtils.getBean(AtpService.class);
+        }
+
+        return atpService;
     }
 
 }
