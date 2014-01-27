2148424902a81d0a2c7206c2867502da0a2ad014
jbergste <jbergste@kuali.org>
2014-01-27 17:27:10 -0500

    KSENROLL-11636  Define more page elements.
    git-svn-id: https://svn.kuali.org/repos/student/test/functional-automation/sambal/trunk@64067 0ac02d80-fb3f-11dc-a574-c57244a850eb

--- features/step_definitions/registration_proof_of_concept_steps.rb
+++ features/step_definitions/registration_proof_of_concept_steps.rb
@@ -6,10 +6,11 @@ end
 
 When /^the course search results are displayed in mobile format$/ do
   on RegisterForCourseResultsMobile do |page|
-    # this is the message about seeing course details
-    page.details_message_element.present?.should == false
-    # basic course info should be there
-    page.course_name_element.present?.should == true
+    # this is the message about seeing course details - should not be there
+    page.details_message.present?.should == false
+    # basic course info - should be there
+    page.results_course_code(0).present?.should == true
+    page.results_course_title(0).present?.should == true
   end
 end
 
--- lib/sambal-kuali/pages/registration/register_for_course_results_page_mobile.rb
+++ lib/sambal-kuali/pages/registration/register_for_course_results_page_mobile.rb
@@ -1,34 +1,43 @@
 class RegisterForCourseResultsMobile < RegisterForCourseSearchBase
   page_url "#{$test_site}/kscr-poc/index.jsp"
 
+  # all resolutions
   element(:results_count_section) { |b| b.frm.section(class: "kscr-Results") }
   element(:results_count) { |b| b.results_count_section.p(class: "kscr-Results-Count").text }
 
   element(:results_list) { |b| b.frm.ol(class: "kscr-COList") }
   element(:results_list_item) { |b, index| b.results_list.li[index] }
   element(:results_item_section)  { |b, index| b.results_list_item(index).section(class: "kscr-COItem") }
+  #element(:results_item_course_code)  { |b, index| b.results_item_section(index).link(class: "kscr-COItem-code ng-binding") }
+  #element(:results_item_course_title)  { |b, index| b.results_item_section(index).link(class: "kscr-COItem-title ng-binding") }
+  #element(:results_item_course_credits)  { |b, index| b.results_item_section(index).link(class: "kscr-COItem-credits ng-binding") }
 
-  def target_list_item_by_index(index)
-    result_list_item index
+  # desktop resolution only
+  element(:results_message_section)  { |b| b.frm.section(class: "kscr-Results medium-5 column") }
+  element(:results_message_div)  { |b| b.frm.div(class: "medium-7 column hide-for-small-only") }
+  element(:details_message) { |b| b.results_message_div.h2}
+
+  def target_list_item_by_index(ind)
+    results_list_item(ind)
   end
 
   def results_link(index)
-    item = target_list_item_by_index index
+    item = target_list_item_by_index(index)
     item.link(class: "kscr-COItem-code")
   end
 
   def results_course_code(index)
-    item = target_list_item_by_index index
+    item = target_list_item_by_index (index)
     item.link(class: "kscr-COItem-code").text
   end
 
-  def results_course_title(index)
-    item = target_list_item_by_index index
+  def results_course_title index
+    item = target_list_item_by_index (index)
     item.span(class: "kscr-COItem-title").text
   end
 
   def results_course_credits(index)
-    item = target_list_item_by_index index
+    item = target_list_item_by_index(index)
     item.span(class: "kscr-COItem-credits").text
   end
 end
