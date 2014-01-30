4fa9a3b76972c6a1f4a26f4608d9807b5efd1525
arun.ramaiah <arun.ramaiah@kuali.org>
2014-01-30 10:56:37 -0500

    KSAP-706 - Fix nightly failures on Course Search scenarios.
    git-svn-id: https://svn.kuali.org/repos/student/test/functional-automation/ksap-aft/trunk@64266 0ac02d80-fb3f-11dc-a574-c57244a850eb

--- lib/sambal-kuali/pages/login_page.rb
+++ lib/sambal-kuali/pages/login_page.rb
@@ -2,6 +2,8 @@ class Login < PageFactory
 
   page_url "#{$test_site}/portal.do?selectedTab=main"
 
+  expected_element :username_field
+
   element(:username_field) { |b| b.text_field(:name=>"j_username") }
   element(:password_field) { |b| b.text_field(:name=>"j_password") }
   element(:login_button) { |b| b.button(:value=>"Login") }
