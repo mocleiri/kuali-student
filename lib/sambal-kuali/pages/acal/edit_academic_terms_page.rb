59ea7d351b9bb3ccfbcd7c6ea3fd09f7d06e2b70
mike.wyers <mike.wyers@kuali.org>
2014-01-29 21:30:59 -0500

    KSENROLL-11652 AFTs related to previous refactoring, fix row nos in key dates view
    git-svn-id: https://svn.kuali.org/repos/student/test/functional-automation/sambal/trunk@64236 0ac02d80-fb3f-11dc-a574-c57244a850eb

--- lib/sambal-kuali/pages/acal/edit_academic_terms_page.rb
+++ lib/sambal-kuali/pages/acal/edit_academic_terms_page.rb
@@ -148,7 +148,7 @@ class EditAcademicTerms < BasePage
 
   def key_date_target_row(term_type, key_date_group_type, key_date_type)
     if key_date_group_div(term_type, key_date_group_type).table.exists?
-      key_date_group_div(term_type, key_date_group_type).table.rows[2..-2].each do | row |
+      key_date_group_div(term_type, key_date_group_type).table.rows[1..-2].each do | row |
         if row.cells[0].text == key_date_type
           return row
         end
