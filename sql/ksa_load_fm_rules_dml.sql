9dc95a8ec8bad87ba7e6b454d41d6621fa4d3496
paulheald <paulheald@kuali.org>
2014-02-11 17:09:46 -0500

    Error in comparion for college park fees. Corrected. Test against scenario 34.
    git-svn-id: https://svn.kuali.org/repos/student/contrib/ksa/trunk@64916 0ac02d80-fb3f-11dc-a574-c57244a850eb

--- sql/ksa_load_fm_rules_dml.sql
+++ sql/ksa_load_fm_rules_dml.sql
@@ -405,24 +405,24 @@ Insert into KSSA_RULE (DESCRIPTION, ID, NAME, RULE_TYPE_ID_FK, PRIORITY, HEADER,
 ')!
 
 Insert into KSSA_RULE (DESCRIPTION, ID, NAME, RULE_TYPE_ID_FK, PRIORITY, HEADER, LHS, RHS) values ('Frostburg student receives discount major 0909F', 7006, 'FM Signup 2_6', 3, 10, null,
-'(session key "major.code" is "0909F" and signup has rates "cp.undergrad.resident.ft")',
+'(session key "major.code" is "0909f" and signup has rates "cp.undergrad.resident.ft")',
 'add signup rate "cp.undergrad.resident.ft.frostburg.discount", "default"')!
 
 Insert into KSSA_RULE (DESCRIPTION, ID, NAME, RULE_TYPE_ID_FK, PRIORITY, HEADER, LHS, RHS) values ('Frostburg student receives discount major 0909F', 7007, 'FM Signup 2_7', 3, 10, null,
-'(session key "major.code" is "0909F" and signup has rates "cp.undergrad.resident.pt")',
+'(session key "major.code" is "0909f" and signup has rates "cp.undergrad.resident.pt")',
 'add signup rate "cp.undergrad.resident.pt.frostburg.discount", "default"')!
 
 Insert into KSSA_RULE (DESCRIPTION, ID, NAME, RULE_TYPE_ID_FK, PRIORITY, HEADER, LHS, RHS) values ('Frostburg student receives discount major 0909F', 7008, 'FM Signup 2_8', 3, 10, null,
-'(session key "major.code" is "0909F" and signup has rates "cp.undergrad.nonresident.ft")',
+'(session key "major.code" is "0909f" and signup has rates "cp.undergrad.nonresident.ft")',
 'add signup rate "cp.undergrad.nonresident.ft.frostburg.discount", "default"')!
 
 Insert into KSSA_RULE (DESCRIPTION, ID, NAME, RULE_TYPE_ID_FK, PRIORITY, HEADER, LHS, RHS) values ('Frostburg student receives discount major 0909F', 7009, 'FM Signup 2_9', 3, 10, null,
-'(session key "major.code" is "0909F" and signup has rates "cp.undergrad.nonresident.pt")',
+'(session key "major.code" is "0909f" and signup has rates "cp.undergrad.nonresident.pt")',
 'add signup rate "cp.undergrad.nonresident.pt.frostburg.discount", "default"')!
 
 Insert into KSSA_RULE (DESCRIPTION, ID, NAME, RULE_TYPE_ID_FK, PRIORITY, HEADER, LHS, RHS) values ('Student already has mandatory fees so incurred tech fees removed', 7010, 'FM Signup 2_10', 3, 10, null,
-'(signup has rates "tech.fee.[..]" and signup has rates "cp.[.*graduate].[..]")',
-'remove signup rates "tech.fee.[..]"')!
+'(signup has rates "tech.fee.[..]" and signup has rates "cp.(resident|nonresident|graduate).[f|p]t")',
+'remove signup rates "tech.fee.[f|p]t"')!
 
 
 -- FM Signup 3 rule set --
