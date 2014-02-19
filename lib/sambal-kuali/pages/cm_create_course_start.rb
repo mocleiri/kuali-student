cd4a2c250d8ea31735585e64eaea8104af7373b7
arun.ramaiah <arun.ramaiah@kuali.org>
2014-02-19 04:36:24 -0500

    Update filename from cm_initialpage_admin.rb to cm_create_course_start.rb
    git-svn-id: https://svn.kuali.org/repos/student/test/functional-automation/cm-aft/trunk@65254 0ac02d80-fb3f-11dc-a574-c57244a850eb

---
+++ lib/sambal-kuali/pages/cm_create_course_start.rb
@@ -0,0 +1,12 @@
+class CmCreateCourseStart < BasePage
+
+  wrapper_elements
+  cm_elements
+
+  element(:blank_proposal) {|b| b.div(id:'KS-createCourseInitialPage-CMStartOptions').input(id:'KS-createCourseInitialPage-CMStartOptions_control_0') }
+  element(:curriculum_review_process) {|b| b.div(id:'createCourseInitial_is_reviewProcess').input(id:'createCourseInitial_is_reviewProcess_control') }
+  action(:continue) {|b| b.button(id: 'createCourseInitial-continueCreate').click }
+  action(:cancel) {|b| b.button(id: 'cancel').click }
+
+
+end #class
