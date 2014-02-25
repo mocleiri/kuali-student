7924ded90f7dee80da187f5f89677ec4de8efe82
mguilla <mguilla@kuali.org>
2014-02-25 17:19:49 -0500

    work in progress
    git-svn-id: https://svn.kuali.org/repos/student/enrollment/ks-ap/branches/KSAP-0.8-iu@65640 0ac02d80-fb3f-11dc-a574-c57244a850eb

--- ks-ap-framework/src/main/java/org/kuali/student/ap/framework/context/support/DefaultPlanHelper.java
+++ ks-ap-framework/src/main/java/org/kuali/student/ap/framework/context/support/DefaultPlanHelper.java
@@ -5,17 +5,26 @@ import java.util.Collections;
 import java.util.Comparator;
 import java.util.List;
 
+import javax.jws.WebParam;
+
 import org.kuali.rice.core.api.config.property.ConfigContext;
 import org.kuali.student.ap.framework.config.KsapFrameworkServiceLocator;
 import org.kuali.student.ap.framework.context.PlanConstants;
 import org.kuali.student.ap.framework.context.PlanHelper;
 import org.kuali.student.ap.academicplan.dto.LearningPlanInfo;
+import org.kuali.student.ap.academicplan.dto.PlaceholderInfo;
 import org.kuali.student.ap.academicplan.infc.DegreeMapRequirement;
 import org.kuali.student.ap.academicplan.infc.Placeholder;
 import org.kuali.student.ap.academicplan.infc.PlaceholderInstance;
 import org.kuali.student.ap.academicplan.infc.TypedObjectReference;
 import org.kuali.student.enrollment.courseoffering.infc.ActivityOffering;
+import org.kuali.student.r2.common.dto.ContextInfo;
+import org.kuali.student.r2.common.exceptions.DoesNotExistException;
+import org.kuali.student.r2.common.exceptions.InvalidParameterException;
+import org.kuali.student.r2.common.exceptions.MissingParameterException;
+import org.kuali.student.r2.common.exceptions.OperationFailedException;
 import org.kuali.student.r2.core.acal.infc.Term;
+import org.kuali.student.r2.lum.course.dto.CourseInfo;
 import org.kuali.student.r2.lum.course.infc.Course;
 
 /**
@@ -115,44 +124,107 @@ public class DefaultPlanHelper implements PlanHelper {
         return calendarTerms;
     }
 
+    
+    
 	@Override
 	public Course getCourse(TypedObjectReference ref) {
 		if (ref.getRefObjectType().equals(PlanConstants.REF_TYPE_COURSE)){
 			// need to load a course with the course id
 			
-			return null;
+			CourseInfo course = KsapFrameworkServiceLocator.getCourseHelper().getCourseInfo(ref.getRefObjectId());
+			return course;
 		}
 		return null;
 	}
 
 	@Override
 	public ActivityOffering getActivityOffering(TypedObjectReference ref) {
-		// TODO Auto-generated method stub
+		// TODO: MARK will write a helper method that can be used to get the ActivityOffering by id.
+		
 		return null;
 	}
 
 	@Override
 	public Placeholder getPlaceHolder(TypedObjectReference ref) {
-		// TODO Auto-generated method stub
+
+		if (ref.getRefObjectType().equals(PlanConstants.REF_TYPE_PLACEHOLDER)) {
+		    
+			String placeholderId = ref.getRefObjectId();
+			Placeholder placeholder;
+			try {
+				placeholder = KsapFrameworkServiceLocator
+						.getDegreeMapService().getPlaceholder(
+								placeholderId, KsapFrameworkServiceLocator.getContext().getContextInfo());
+				return placeholder;
+			} catch (DoesNotExistException e) {
+				return null;
+			} catch (InvalidParameterException e) {
+				throw new RuntimeException(String.format("Could not fetch placeholder with id [%s].", placeholderId), e);
+			} catch (MissingParameterException e) {
+				throw new RuntimeException(String.format("Could not fetch placeholder with id [%s].", placeholderId), e);
+			} catch (OperationFailedException e) {
+				throw new RuntimeException(String.format("Could not fetch placeholder with id [%s].", placeholderId), e);
+			}
+
+		}
+
 		return null;
 	}
 
 	
 	@Override
 	public PlaceholderInstance getPlaceHolderInstance(TypedObjectReference ref) {
-		// TODO Auto-generated method stub
+		if (ref.getRefObjectType().equals(PlanConstants.REF_TYPE_PLACEHOLDER_INSTANCE)) {
+		    
+			String pid = ref.getRefObjectId();
+			PlaceholderInstance pi;
+			try {
+				pi = KsapFrameworkServiceLocator
+						.getDegreeMapService().getPlaceholderInstance(
+								pid, KsapFrameworkServiceLocator.getContext().getContextInfo());
+				return pi;
+			} catch (DoesNotExistException e) {
+				return null;
+			} catch (InvalidParameterException e) {
+				throw new RuntimeException(String.format("Could not fetch placeholder instance with id [%s].", pid), e);
+			} catch (MissingParameterException e) {
+				throw new RuntimeException(String.format("Could not fetch placeholder instance with id [%s].", pid), e);
+			} catch (OperationFailedException e) {
+				throw new RuntimeException(String.format("Could not fetch placeholder instance with id [%s].", pid), e);
+			}
+
+		}
+
 		return null;
 	}
 	
 	
 	@Override
 	public DegreeMapRequirement getRequirement(TypedObjectReference ref) {
-		// TODO Auto-generated method stub
+		if (ref.getRefObjectType().equals(PlanConstants.REF_TYPE_DEGREE_MAP_REQUIREMENT)) {
+		    
+			String rid = ref.getRefObjectId();
+			DegreeMapRequirement dmr;
+			try {
+				dmr = KsapFrameworkServiceLocator
+						.getDegreeMapService().getRequirement(
+								rid, KsapFrameworkServiceLocator.getContext().getContextInfo());
+				return dmr;
+			} catch (DoesNotExistException e) {
+				return null;
+			} catch (InvalidParameterException e) {
+				throw new RuntimeException(String.format("Could not fetch degree map requirement with id [%s].", rid), e);
+			} catch (MissingParameterException e) {
+				throw new RuntimeException(String.format("Could not fetch degree map requirement with id [%s].", rid), e);
+			} catch (OperationFailedException e) {
+				throw new RuntimeException(String.format("Could not fetch degree map requirement with id [%s].", rid), e);
+			}
+
+		}
+
 		return null;
 	}
 
-
-
-    
+ 
     
 }
