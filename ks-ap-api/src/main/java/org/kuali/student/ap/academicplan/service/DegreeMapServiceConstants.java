7b330f8eb194a0c5fb392289c1c5a4e0ee552945
mwfyffe <mwfyffe@kuali.org>
2014-03-05 10:48:44 -0500

    SCT-6649 cleaned up duplicate services after merge
    git-svn-id: https://svn.kuali.org/repos/student/enrollment/ks-api/branches/KSAP-0.7-iu@65991 0ac02d80-fb3f-11dc-a574-c57244a850eb

--- ks-ap-api/src/main/java/org/kuali/student/ap/academicplan/service/DegreeMapServiceConstants.java
+++ ks-ap-api/src/main/java/org/kuali/student/ap/academicplan/service/DegreeMapServiceConstants.java
@@ -11,6 +11,4 @@ public class DegreeMapServiceConstants {
     public static final String NAMESPACE = CommonServiceConstants.REF_OBJECT_URI_GLOBAL_PREFIX + "degreeMap";
     public static final String SERVICE_NAME = "DegreeMapService";
 
-  
-
 }
