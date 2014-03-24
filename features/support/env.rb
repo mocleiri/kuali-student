7401a6635c6dc9f9ce745eb192ccda56b2322a1f
rani.baskaran <rani.baskaran@kuali.org>
2014-03-24 05:46:17 -0400

    git-svn-id: https://svn.kuali.org/repos/student/test/functional-automation/cm-aft/trunk@66855 0ac02d80-fb3f-11dc-a574-c57244a850eb

--- features/support/env.rb
+++ features/support/env.rb
@@ -55,7 +55,7 @@ Before do
   @browser = browser
 end
 
-# at_exit { browser.close unless browser == nil }
+at_exit { browser.close unless browser == nil }
 
 if ENV['HEADLESS']
   # commented out to allow parallel execution
