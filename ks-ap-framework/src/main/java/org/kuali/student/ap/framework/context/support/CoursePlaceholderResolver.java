7b5acecb0f4205328fd7ba7259a751640e7cbb45
mwfyffe <mwfyffe@kuali.org>
2014-04-09 16:24:15 -0400

    SCT-6666 update placeholder interaction
    git-svn-id: https://svn.kuali.org/repos/student/enrollment/ks-ap/branches/ks-ap-0.7-iu-dev@67825 0ac02d80-fb3f-11dc-a574-c57244a850eb

--- ks-ap-framework/src/main/java/org/kuali/student/ap/framework/context/support/CoursePlaceholderResolver.java
+++ ks-ap-framework/src/main/java/org/kuali/student/ap/framework/context/support/CoursePlaceholderResolver.java
@@ -3,21 +3,27 @@ package org.kuali.student.ap.framework.context.support;
 import java.util.Collections;
 import java.util.Set;
 
+import org.apache.log4j.Logger;
 import org.kuali.student.ap.academicplan.infc.Placeholder;
 import org.kuali.student.ap.framework.context.PlaceholderResolver;
+import org.kuali.student.r2.common.exceptions.InvalidParameterException;
+import org.kuali.student.r2.common.exceptions.MissingParameterException;
 
 public class CoursePlaceholderResolver implements PlaceholderResolver {
+	
+	private static final Logger LOG = Logger.getLogger(CoursePlaceholderResolver.class);
 
 	@Override
-	public Set<String> resolve(Placeholder ph) throws IllegalStateException, IllegalArgumentException {
+	public Set<String> resolve(Placeholder ph) throws MissingParameterException {
 		//If any of parms 2-4 are not null, throw an exception
 		if(ph.getParm2() != null || ph.getParm3() != null || ph.getParm4() != null){
-			throw new IllegalStateException(this.getClass() + " called but Placeholder doesn't represent a course.");
+			InvalidParameterException e = new InvalidParameterException(this.getClass() + " called but Placeholder doesn't represent a course.");
+			LOG.warn(e,e);
 		}
 		
 		//Make sure we have a non-null parm1
 		if(ph.getParm1() == null){
-			throw new IllegalArgumentException("parm1 for course Placeholder can't be null");
+			throw new MissingParameterException("parm1 for course Placeholder can't be null");
 		}
 		
 		//parm1 is already a KS course ID, so we can pass it along
