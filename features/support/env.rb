ee938359a5cf68773129c84b5fcea0b96c2e2525
mike.wyers <mike.wyers@kuali.org>
2014-04-25 14:54:27 -0400

    KSENROLL-10435 AFTs update environment - add screenshot on failure to headless env config
    git-svn-id: https://svn.kuali.org/repos/student/test/functional-automation/sambal/trunk@68639 0ac02d80-fb3f-11dc-a574-c57244a850eb

--- features/support/env.rb
+++ features/support/env.rb
@@ -75,6 +75,8 @@ if ENV['HEADLESS']
   #re-start browser after each failed scenario
   After do | scenario |
     if scenario.failed?
+      encoded_img = @browser.driver.screenshot_as(:base64)
+      embed("data:image/png;base64,#{encoded_img}",'image/png')
       @browser.close unless @browser == nil
       browser = nil
     end
