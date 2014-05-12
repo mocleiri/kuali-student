49f27784aa244c2f63075add53e2c239506bcc06
mike.wyers <mike.wyers@kuali.org>
2014-05-12 14:54:23 -0400

    KSENROLL-12714 AFTs fix page objects for view population details
    git-svn-id: https://svn.kuali.org/repos/student/test/functional-automation/sambal/trunk@69346 0ac02d80-fb3f-11dc-a574-c57244a850eb

--- lib/sambal-kuali/pages/population/view_population_diag_page.rb
+++ lib/sambal-kuali/pages/population/view_population_diag_page.rb
@@ -8,10 +8,10 @@ class ViewPopulationDiag < PopulationsBase
 
   include PopulationsSearch
 
-  value(:name) { |b| b.frm.div(data_label: "Name").span(index: 2).text }
-  value(:description) { |b| b.frm.div(data_label: "Description").span(index: 2).text }
-  value(:state) { |b| b.frm.div(data_label: "State").span(index: 1).text }
-  value(:rule) { |b| b.frm.div(data_label: "Rule").span(index: 2).text }
+  value(:name) { |b| b.frm.div(id: 'name_inquiry').pre.text }
+  value(:description) { |b| b.frm.div(id: 'desc_inquiry').text[/(?<=\n).*/] }
+  value(:state) { |b| b.frm.div(id: 'state_inquiry').text[/(?<=\n).*/] }
+  value(:rule) { |b| b.frm.div(id: 'rule_inquiry').text[/(?<=\n).*/] }
   element(:close_button_element) { |b| b.frm.button(text: "Close")}
   action(:close) { |b| b.close_button_element.click;b.loading.wait_while_present}
 
