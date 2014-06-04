567e9f8631044a5fb4da724c4e0b37a6965cc9e5
mike.wyers <mike.wyers@kuali.org>
2014-06-04 11:19:20 -0400

    KSENROLL-10435 AFT Upgrade script for firefox v 27.0.1
    git-svn-id: https://svn.kuali.org/repos/student/test/functional-automation/sambal/trunk@70281 0ac02d80-fb3f-11dc-a574-c57244a850eb

--- upgrade_firefox.sh
+++ upgrade_firefox.sh
@@ -9,8 +9,8 @@ unlink /usr/bin/firefox
 rm -rf /usr/lib/firefox
 
 cd $workpath
-wget -r --no-parent --reject "index.html*" -nH --cut-dirs=7 https://ftp.mozilla.org/pub/mozilla.org/firefox/releases/14.0.1/linux-x86_64/en-US/
-tar xvf firefox-14.0.1.tar.bz2
+wget -r --no-parent --reject "index.html*" -nH --cut-dirs=7 https://ftp.mozilla.org/pub/mozilla.org/firefox/releases/27.0.1/linux-x86_64/en-US/
+tar xvf firefox-27.0.1.tar.bz2
 cd bin
 ln -s ../firefox/firefox
 ldconfig
