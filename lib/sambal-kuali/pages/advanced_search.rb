007819819de4210707053481cf85040022ee1d99
arun.ramaiah <arun.ramaiah@kuali.org>
2014-06-05 00:19:06 -0400

    KSCM-2161- Fix for tests failing due to sync issue
    git-svn-id: https://svn.kuali.org/repos/student/test/functional-automation/cm-aft/trunk@70325 0ac02d80-fb3f-11dc-a574-c57244a850eb

--- lib/sambal-kuali/pages/advanced_search.rb
+++ lib/sambal-kuali/pages/advanced_search.rb
@@ -8,7 +8,7 @@ class CmAdvancedSearchPage < BasePage
 
 
   element(:jointly_offered_search_by) { |b| b.frm.select(id: "joints_search.searchBy_control") }
-  element(:jointly_offered_given_name) { |b| b.frm.text_field(id: "joints_search.courseTitle_control") }
+  element(:jointly_offered_given_name) { |b| b.frm.text_field(id: "joints_search.courseTitle_control"); b.wait_until_present(120) }
   element(:jointly_offered_course_code) { |b| b.frm.text_field(id: "joints_search.courseCode_control") }
   element(:jointly_offered_text) { |b| b.frm.text_field(id: "joints_search.descr.plain_control") }
   action(:jointly_offered_search) { |b| b.frm.button(id: "button_search").click}
