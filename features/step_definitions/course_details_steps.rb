e39166e8815c79fb06f3a966c210772f9a364875
sanujkumar <sanujkumar@kuali.org>
2014-06-16 15:07:32 -0400

    KSAP-1332
    git-svn-id: https://svn.kuali.org/repos/student/test/functional-automation/ksap-aft/trunk@70787 0ac02d80-fb3f-11dc-a574-c57244a850eb

--- features/step_definitions/course_details_steps.rb
+++ features/step_definitions/course_details_steps.rb
@@ -1,5 +1,6 @@
 When /^I search for a course on course search page$/ do
  @course_search_result = make CourseSearchResults, :course_code => "ENGL201" ,:description=>"historical",:requisite=>"None",:scheduled_terms=>"SP 14",:projected_terms=>"Check",:gened_requirements=>"General",:subject=>"English"
+ @course_section_object=make CourseSectionObject
  @course_search_result.course_search
 end
 
@@ -20,3 +21,25 @@ Then /^I should be able to view all the required information on the Course Detai
     page.subject(course_code).text.should match /#{@course_search_result.subject}/
   end
 end
+
+
+And(/^I navigate to the Course Section Details page$/) do
+  @course_search_result.navigate_course_detail_page
+  @course_search_result.navigate_course_section_page
+end
+
+Then(/^I should be able to view the section details about the course$/) do
+  on CourseSectionPage do |page|
+
+    page.course_detail_header.text.should match /#{@course_section_object.course_detail_header}/
+    page.term_and_course_offering.should == @course_section_object.term_and_course_offering
+    page.course_description.text.should match /#{@course_section_object.course_description}/
+    page.activity_offering_code(0,0,0,0).should match /#{@course_section_object.activity_offering_code}/
+    page.activity_offering_instructor(0,0,0,0).should match /#{@course_section_object.activity_offering_instructor}/
+    page.activity_offering_time(0,0,0,0).should match /#{@course_section_object.activity_offering_time}/
+    page.activity_offering_location(0,0,0,0).should match /#{@course_section_object.activity_offering_location}/
+    page.activity_offering_seats(0,0,0,0).should match /#{@course_section_object.activity_offering_seats}/
+
+  end
+end
+
