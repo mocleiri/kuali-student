a44a03c00a567c1fe67f4a897e1aa70e6dc12e20
mike.wyers <mike.wyers@kuali.org>
2014-06-18 13:53:57 -0400

    KSENROLL-13179 AFTs fix FE3-6,7 failures
    git-svn-id: https://svn.kuali.org/repos/student/test/functional-automation/sambal/trunk@70902 0ac02d80-fb3f-11dc-a574-c57244a850eb

--- features/step_definitions/final_exam_steps.rb
+++ features/step_definitions/final_exam_steps.rb
@@ -624,7 +624,37 @@ When /^I view the Exam Offerings for a CO with a standard final exam driven by C
 end
 
 When /^I view the Exam Offerings for a CO in an Open SOC with a standard final exam driven by Activity Offering$/ do
-  @course_offering = create CourseOffering, :create_by_copy=>(make CourseOffering, :term => "201301", :course => "ENGL304")
+  @original_co = make CourseOffering, :term => Rollover::OPEN_EO_CREATE_TERM, :course => "ENGL304"
+
+  unless @original_co.exists?
+    course_offering = make CourseOffering, :term=> @original_co.term,
+                           :course => @original_co.course,
+                           :suffix => ' ',
+                           :final_exam_driver => "Final Exam Per Activity Offering"
+    course_offering.delivery_format_list[0].format = "Lecture/Discussion"
+    course_offering.delivery_format_list[0].grade_format = "Discussion"
+    course_offering.delivery_format_list[0].final_exam_activity = "Lecture"
+    course_offering.create
+
+    activity_offering = create ActivityOfferingObject, :parent_course_offering => course_offering,
+                               :format => "Lecture/Discussion", :activity_type => "Lecture"
+    si_obj =  make SchedulingInformationObject, :days => "TH",
+                   :start_time => "11:00", :start_time_ampm => "am",
+                   :end_time => "11:50", :end_time_ampm => "am",
+                   :facility => 'TWS', :room => '1100'
+    activity_offering.add_req_sched_info :rsi_obj => si_obj
+
+    #TODO: KSENROLL-13157 problems creating 2nd AO
+    # activity_offering = create ActivityOfferingObject, :parent_course_offering => course_offering,
+    #                            :format => "Lecture/Discussion", :activity_type => "Discussion"
+    # si_obj =  make SchedulingInformationObject, :days => "W",
+    #                :start_time => "09:00", :start_time_ampm => "am",
+    #                :end_time => "09:50", :end_time_ampm => "am",
+    #                :facility => 'KEY', :room => '0117'
+    # activity_offering.add_req_sched_info :rsi_obj => si_obj
+  end
+
+  @course_offering = create CourseOffering, :create_by_copy=> @original_co
   @course_offering.delivery_format_list[0].format = "Lecture/Discussion"
 
   @course_offering.edit :final_exam_type => "Standard Final Exam", :final_exam_driver => "Final Exam Per Activity Offering",
@@ -795,8 +825,6 @@ When /^I view the Exam Offerings for a CO where the Activity Offering Standard F
     #                :facility => 'KEY', :room => '0117'
     # activity_offering.add_req_sched_info :rsi_obj => si_obj
   end
-
-
 
   @course_offering = create CourseOffering, :create_by_copy=> @original_co
   @course_offering.delivery_format_list[0].format = "Lecture/Discussion"
@@ -2076,7 +2104,37 @@ Then /^the Exam Offering table for the canceled AO should also be in the same st
 end
 
 Given /^that a CO allows for multiple Format Offerings and has one existing format offering and a standard exam driven by Course Offering$/ do
-  @course_offering = create CourseOffering, :create_by_copy=>(make CourseOffering, :term=> "201208", :course => "ENGL304")
+  @original_co = make CourseOffering, :term => Rollover::PUBLISHED_EO_CREATE_TERM, :course => "ENGL304"
+
+  unless @original_co.exists?
+    course_offering = make CourseOffering, :term=> @original_co.term,
+                           :course => @original_co.course,
+                           :suffix => ' ',
+                           :final_exam_driver => "Final Exam Per Activity Offering"
+    course_offering.delivery_format_list[0].format = "Lecture/Discussion"
+    course_offering.delivery_format_list[0].grade_format = "Discussion"
+    course_offering.delivery_format_list[0].final_exam_activity = "Lecture"
+    course_offering.create
+
+    activity_offering = create ActivityOfferingObject, :parent_course_offering => course_offering,
+                               :format => "Lecture/Discussion", :activity_type => "Lecture"
+    si_obj =  make SchedulingInformationObject, :days => "TH",
+                   :start_time => "11:00", :start_time_ampm => "am",
+                   :end_time => "11:50", :end_time_ampm => "am",
+                   :facility => 'TWS', :room => '1100'
+    activity_offering.add_req_sched_info :rsi_obj => si_obj
+
+    #TODO: KSENROLL-13157 problems creating 2nd AO
+    # activity_offering = create ActivityOfferingObject, :parent_course_offering => course_offering,
+    #                            :format => "Lecture/Discussion", :activity_type => "Discussion"
+    # si_obj =  make SchedulingInformationObject, :days => "W",
+    #                :start_time => "09:00", :start_time_ampm => "am",
+    #                :end_time => "09:50", :end_time_ampm => "am",
+    #                :facility => 'KEY', :room => '0117'
+    # activity_offering.add_req_sched_info :rsi_obj => si_obj
+  end
+
+  @course_offering = create CourseOffering, :create_by_copy=> @original_co
 end
 
 When /^I edit the CO to add a second Format Offering$/ do
