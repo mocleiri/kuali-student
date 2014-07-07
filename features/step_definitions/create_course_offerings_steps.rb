b540e169ff95e1b4fec1e4f211246e685d698765
mike.wyers <mike.wyers@kuali.org>
2014-07-07 12:26:17 -0400

    KSENROLL-13482 Fix failing AFT AZ 5.1C_open. Make sure COs created have atleast 1 AO
    git-svn-id: https://svn.kuali.org/repos/student/test/functional-automation/sambal/trunk@71598 0ac02d80-fb3f-11dc-a574-c57244a850eb

--- features/step_definitions/create_course_offerings_steps.rb
+++ features/step_definitions/create_course_offerings_steps.rb
@@ -4,6 +4,8 @@ When /^I create a Course Offering with a lecture Delivery Format$/ do
   @course_offering.delivery_format_list[0].grade_format = "Course Offering"
   @course_offering.delivery_format_list[0].final_exam_activity = "Lecture"
   @course_offering.create
+
+  @course_offering.create_ao :navagate_to_page => false
 end
 
 When /^I create a Course Offering with two Delivery Formats$/ do
@@ -18,6 +20,7 @@ When /^I create a Course Offering with two Delivery Formats$/ do
                                                  :final_exam_activity => "Lecture",
                                                  :parent_co => @course_offering)
   @course_offering.create
+  @course_offering.create_ao :navagate_to_page => false
 end
 
 And /^I create a Course Offering with random Delivery Formats$/ do
