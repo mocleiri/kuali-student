7e5fc1bf89d8b2e144e35a88239c948047b85db3
chongzhu <chongzhu@kuali.org>
2014-07-11 19:10:30 -0400

    KSCM-2202: AFT Scenarios - Add/Edit/Delete Requisites to Course Proposal (CC13)
    git-svn-id: https://svn.kuali.org/repos/student/test/functional-automation/cm-aft/trunk@71945 0ac02d80-fb3f-11dc-a574-c57244a850eb

--- lib/sambal-kuali/data_objects/course_requisites_object.rb
+++ lib/sambal-kuali/data_objects/course_requisites_object.rb
@@ -46,6 +46,60 @@ class CmRequisitesRuleData < DataFactory
     end
   end
 
+  def navigate_to_requisite
+    on CmCourseRequisites do |page|
+      page.course_requisites unless page.current_page('Course Requisites').exists?
+    end
+  end
+
+  def advanced_search(field, code)
+    on CmRequisiteRules do |page|
+      page.edit_loading.wait_while_present
+      if field == "course title"
+        page.loading.wait_while_present
+        click_search_link( Regexp.new(".*editTree.+proposition\.cluSet\.clus"))
+        page.lookup_course_title.when_present.set code
+      elsif field == "course set"
+        page.loading.wait_while_present
+        page.search_link
+        page.lookup_set_name.when_present.set code
+      elsif field == "program code"
+        page.loading.wait_while_present
+        click_search_link( Regexp.new(".*editTree.+proposition\.progCluSet\.clus"))
+        page.lookup_course_code.when_present.set code
+      elsif field == "class standing"
+        page.loading.wait_while_present
+        click_search_link( Regexp.new(".*editTree.+proposition\.classStanding"))
+        page.lookup_class_standing.when_present.set code
+      elsif field == "org"
+        page.loading.wait_while_present
+        click_search_link( Regexp.new(".*editTree.+proposition\.orgInfo\.id"))
+        page.lookup_abrev_org.when_present.set code
+      elsif field == "population"
+        page.loading.wait_while_present
+        click_search_link( Regexp.new(".*editTree.+proposition\.orgInfo\.population\.id"))
+        page.lookup_population.when_present.set code
+      end
+      page.lookup_search_button
+      page.loading.wait_while_present
+      page.return_course_code(/.*#{Regexp.escape(code)}.*/i).a( text: /Select/).click
+      page.loading.wait_while_present
+    end
+  end
+
+  def click_search_link( regex)
+    on CmRequisiteRules do |page|
+      elements = page.edit_tree_section.elements( :tag_name, 'a')
+      elements.each do |elem|
+        if elem.text == "Advanced Search" && page.edit_tree_section.a( id: elem.id).attribute_value('data-submit_data') =~ regex
+          page.edit_tree_section.a(id: elem.id).click
+          break
+        end
+      end
+      raise "co requisites click_search_link: link not found for: #{regex}"
+    end
+  end
+
 end
 
 class CmRequisiteRule < CmRequisitesRuleData
