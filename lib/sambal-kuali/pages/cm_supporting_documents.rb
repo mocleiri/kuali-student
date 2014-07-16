d2189d8dbc48b2c0abece5590fc0d4b9181fe0dc
arun.ramaiah <arun.ramaiah@kuali.org>
2014-07-16 02:55:18 -0400

    KSCM-1883 fix to update AFTs with new ID definitions.
    git-svn-id: https://svn.kuali.org/repos/student/test/functional-automation/cm-aft/trunk@72168 0ac02d80-fb3f-11dc-a574-c57244a850eb

--- lib/sambal-kuali/pages/cm_supporting_documents.rb
+++ lib/sambal-kuali/pages/cm_supporting_documents.rb
@@ -5,7 +5,7 @@ class CmSupportingDocuments < BasePage
 
   element(:supporting_doc_section) { |b| b.div(id: "KS-CourseView-SupportingDocumentsSubSection") }
   action(:add_supporting_doc) { |b| b.button(id: "KS-CourseView-SupportingDocument-Section_add").click; b.loading_extended_wait }
-  element(:document_select) { |document_level,b| b.supporting_doc_section.file_field(name: "document.newMaintainableObject.dataObject.documentsToAdd[#{document_level-1}].documentUpload") }
-  element(:document_description) { |document_level,b| b.supporting_doc_section.textarea(name: "document.newMaintainableObject.dataObject.documentsToAdd[#{document_level-1}].description") }
+  element(:document_select) { |document_level,b| b.supporting_doc_section.file_field(name: "document.newMaintainableObject.dataObject.supportingDocs[#{document_level-1}].documentUpload") }
+  element(:document_description) { |document_level,b| b.supporting_doc_section.textarea(name: "document.newMaintainableObject.dataObject.supportingDocs[#{document_level-1}].description") }
   action(:delete) { |document_level,b| b.supporting_doc_section.a(id: /line#{document_level-1}/, title: "Delete").click; b.loading_extended_wait }
 end
