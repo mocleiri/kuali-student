dd647f1dc454ab1bff00c5d9ac453770cda83eaa
arun.ramaiah <arun.ramaiah@kuali.org>
2014-08-04 05:06:11 -0400

    KSCM-2526 Change to subject code either being ENGL or CHEM
    git-svn-id: https://svn.kuali.org/repos/student/test/functional-automation/cm-aft/trunk@73315 0ac02d80-fb3f-11dc-a574-c57244a850eb

--- features/step_definitions/test_data_creation.rb
+++ features/step_definitions/test_data_creation.rb
@@ -1,5 +1,7 @@
 When(/^I create a proposal with all fields populated$/) do
-  @course_proposal1 = create CmCourseProposalObject, :create_new_proposal => true, :proposal_title => random_alphanums(10,'approve and submit test data'),
+  @course_proposal1 = create CmCourseProposalObject, :create_new_proposal => true,
+                            :proposal_title => random_alphanums(10,'approve and submit test data'),
+                            :subject_code => ["ENGL","CHEM"].to_a.sample,
                             :submit_fields => [(make CmSubmitFieldsObject)],
                             :approve_fields => [(make CmApproveFieldsObject)]
 end
