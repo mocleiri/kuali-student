39858803aaa3039ffeba235aa3903ea43f4a3bc7
arun.ramaiah <arun.ramaiah@kuali.org>
2014-08-07 11:48:49 -0400

    KSCM-2417 Incremental changes related to Approve a course proposal
    git-svn-id: https://svn.kuali.org/repos/student/test/functional-automation/cm-aft/trunk@73562 0ac02d80-fb3f-11dc-a574-c57244a850eb

--- features/approve_credit_course_proposal.feature
+++ features/approve_credit_course_proposal.feature
@@ -1,34 +1,45 @@
 @draft
 Feature: Approve Course Proposal
 
-  Scenario Outline: RP2.1 Partial create a course proposal as Faculty and verify that Reviewer cannot Approve
+ Scenario Outline: RP2.1 Partial create a course proposal as Faculty and verify that Reviewer cannot Approve
     Given I have a course proposal with approve fields partially completed created as <author>
-    When I am logged in as a <reviewer>
-    And I attempt to approve the course proposal
+    When I attempt to approve the course proposal as <department_level_chair>
     Then I cannot approve the incomplete proposal
-    Examples:
-      |author|reviewer|
-      |fred  |martha  |
-      |alice |martha  |
+    And I attempt to approve or acknowledge the course proposal as <department_level_committee>
+    Then I cannot approve or acknowledge the incomplete proposal
+  Examples:
+    |author|department_level_chair|department_level_committee|
+    |fred  |martha                |doug                      |
+    |alice |martha                |doug                      |
 
 
-  Scenario Outline: RP2.2 Create a course proposal with approve fields as Faculty and verify reviewer can approve
+ Scenario Outline: RP2.2 Create a course proposal with approve fields as Faculty and verify reviewer can approve
     Given I have a course proposal with approve fields created as <author>
-    When I am logged in as a <reviewer>
-    And I approve the course proposal
-    Then I can successfully approve the course proposal
-    Examples:
-      |author|reviewer|
-      |fred  |martha  |
-      |alice |martha  |
+    When I approve the course proposal as <department_approver>
+    And I approve the course proposal as <division_approver>
+    And I approve the course proposal as <college_approver>
+    And I approve the course proposal as <senate_committee_approver>
+    And I approve the course proposal as <publication_office_approver>
+    Then the course proposal is successfully approved
+    And the new course is Active
+  Examples:
+    |author|department_approver|division_approver|college_approver|senate_committee_approver|publication_office_approver|
+    |fred  |carl               |edna             |erin            |martha                   |alice                      |
+    |alice |carl               |edna             |erin            |martha                   |alice                      |
+
+
 
   Scenario Outline: RP2.3 Partial create a course proposal as Faculty, complete proposal and approve as reviewer.
     Given I have a partial completed course proposal created as <author>
-    When I am logged in as a <reviewer>
+    When I review the proposal as <department_approver>
+    Then missing fields are highlighted and proposal cannot be approved
     And I complete the missing fields on the proposal
-    And I approve the completed course proposal
-    Then I can successfully approve the course proposal
-    Examples:
-      |author|reviewer|
-      |fred  |martha  |
-      |alice |martha  |
+    And I approve the course proposal as <college_approver>
+    And I approve the course proposal as <senate_committee_approver>
+    And I approve the course proposal as <publication_office_approver>
+    Then the course proposal is successfully approved
+    And the new course is Active
+  Examples:
+    |author|department_approver|college_approver|senate_committee_approver|publication_office_approver|
+    |fred  |carol              |earl            |martha                   |alice                      |
+    |alice |carol              |earl            |martha                   |alice                      |
--- features/step_definitions/approve_credit_course_proposal.rb
+++ features/step_definitions/approve_credit_course_proposal.rb
@@ -21,13 +21,28 @@ When(/^I am logged in as a (.*?)$/) do |reviewer|
 end
 
 
-And(/^I attempt to approve the course proposal$/) do
+And(/^I attempt to approve the course proposal as (.*?)$/) do |reviewer|
+  log_in reviewer,reviewer
   navigate_rice_to_cm_home
   @course_proposal.search(@course_proposal.proposal_title)
 
 end
 
-And(/^I approve the course proposal$/) do
+And(/^I attempt to approve or acknowledge the course proposal as (.*?)$/) do
+  log_in reviewer,reviewer
+  navigate_rice_to_cm_home
+  @course_proposal.search(@course_proposal.proposal_title)
+end
+
+
+Then(/^I cannot approve or acknowledge the incomplete proposal$/) do
+  @course_proposal.review_proposal_action
+  #TODO Add validation to check that Approve is disabled.
+end
+
+
+And(/^I approve the course proposal as (.*?)$/) do |reviewer|
+  log_in reviewer, reviewer
   navigate_rice_to_cm_home
   @course_proposal.search(@course_proposal.proposal_title)
   @course_proposal.review_proposal_action
@@ -37,7 +52,9 @@ end
 
 Then(/^I cannot approve the incomplete proposal$/) do
   @course_proposal.review_proposal_action
-  #TODO Add validation to check that Approve is disabled.
+    on CmReviewProposal do |proposal|
+      proposal.approval_review.exists?.should be_false
+    end
 end
 
 Given(/^I have a course proposal with approve fields created as (.*?)$/) do |proposal_author|
@@ -46,13 +63,13 @@ Given(/^I have a course proposal with approve fields created as (.*?)$/) do |pro
   if proposal_author == "fred"
 
   @course_proposal = create CmCourseProposalObject, :create_new_proposal => true,
-                                                    :submit_fields => [(make CmSubmitFieldsObject, :subject_code => ["ENGL","CHEM"].sample)],
+                                                    :submit_fields => [(make CmSubmitFieldsObject, :subject_code => "CHEM")],
                                                     :approve_fields => [(make CmApproveFieldsObject)]
 
   elsif proposal_author == "alice"
     @course_proposal = create CmCourseProposalObject, :create_new_proposal => true,
                                                       :curriculum_review_process => "Yes",
-                                                      :submit_fields => [(make CmSubmitFieldsObject, :subject_code => ["ENGL","CHEM"].sample)],
+                                                      :submit_fields => [(make CmSubmitFieldsObject, :subject_code => "CHEM")],
                                                       :approve_fields => [(make CmApproveFieldsObject)]
   end
 
@@ -61,17 +78,6 @@ Given(/^I have a course proposal with approve fields created as (.*?)$/) do |pro
 end
 
 
-Then(/^I can successfully approve the course proposal$/) do
-  steps %{Given I am logged in as Curriculum Specialist}
-  navigate_rice_to_cm_home
-  @course_proposal.search(@course_proposal.proposal_title)
-  @course_proposal.review_proposal_action
-  on CmReviewProposal do |review|
-    review.proposal_status.should == "Enroute"
-    review.review_approve.exists?.should be_true #need to improve this to validate that is active rather than just visible.
-  end
-  
-end
 
 Given(/^I have a partial completed course proposal created as (.*?)$/) do |proposal_author|
   log_in proposal_author,proposal_author
@@ -79,13 +85,13 @@ Given(/^I have a partial completed course proposal created as (.*?)$/) do |propo
   if proposal_author == "fred"
 
     @course_proposal = create CmCourseProposalObject, :create_new_proposal => true,
-                              :submit_fields => [(make CmSubmitFieldsObject, :subject_code => ["ENGL","CHEM"].sample)],
+                              :submit_fields => [(make CmSubmitFieldsObject, :subject_code => "ENGL")],
                               :approve_fields => [(make CmApproveFieldsObject, :transcript_course_title => nil, :course_number => nil)]
 
   elsif proposal_author == "alice"
     @course_proposal = create CmCourseProposalObject, :create_new_proposal => true,
                               :curriculum_review_process => "Yes",
-                              :submit_fields => [(make CmSubmitFieldsObject, :subject_code => ["ENGL","CHEM"].sample)],
+                              :submit_fields => [(make CmSubmitFieldsObject, :subject_code => "ENGL")],
                               :approve_fields => [(make CmApproveFieldsObject, :transcript_course_title => nil, :course_number => nil)]
   end
 
@@ -103,9 +109,37 @@ And(/^I complete the missing fields on the proposal$/) do
   @course_proposal.approve_fields[0].edit :transcript_course_title => random_alphanums(5,'edited transcript'),
                                           :course_number => "#{(1..999).to_a.sample}",
                                           :defer_save => false
+
+  on(CmCourseInformation).review_proposal
+  @course_proposal.approve_proposal
+
 end
 
 And(/^I approve the completed course proposal$/) do
-  on(CmCourseInformation).review_proposal
-  @course_proposal.approve_proposal
+
+end
+
+
+Then(/^the course proposal is successfully approved$/) do
+  steps %{Given I am logged in as Curriculum Specialist}
+  navigate_rice_to_cm_home
+  @course_proposal.search(@course_proposal.proposal_title)
+  @course_proposal.review_proposal_action
+  on CmReviewProposal do |review|
+    review.proposal_status.should == "Approved"
+  end
+end
+
+And(/^the new course is Active$/) do
+  steps %{Given I am logged in as Faculty}
+  @course = make CmCourseObject, :search_term => "#{@course_proposal.submit_fields[0].subject_code}#{@course_proposal.approve_fields[0].course_number}",
+                                 :course_state => "Active"
+  @course.search_for_course
+  on CmReviewProposal do |course_review|
+    course_review.course_state_review.capitalize.should include @course.course_state
+    #COURSE INFORMATION
+    course_review.course_title_review.should include @course.course_title
+    "#{course_review.subject_code_review}""#{course_review.course_number_review}".should include @course.course_code
+    course_review.description_review.should include @course.description
+  end
 end
